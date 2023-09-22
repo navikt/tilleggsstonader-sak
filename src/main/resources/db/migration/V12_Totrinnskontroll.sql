e862442e371a6ca1359acf16cc258f564ed53247
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2023-09-22 15:26:22 +0200

    Endring av typer og revertering av navn i db-schema

--- src/main/resources/db/migration/V12_Totrinnskontroll.sql
+++ src/main/resources/db/migration/V12_Totrinnskontroll.sql
@@ -2,15 +2,15 @@ CREATE table totrinnsstatus
 (
     id             UUID PRIMARY KEY,
     behandlings_id UUID NOT NULL REFERENCES behandling (id),
-    opprettet_av   VARCHAR NOT NULL,
+    opprettet_av   VARCHAR      NOT NULL,
     opprettet_tid  TIMESTAMP(3) NOT NULL,
-    endret_av      VARCHAR NOT NULL,
-    sporbar        TIMESTAMP(3) NOT NULL,
-    saksbehandler  VARCHAR NOT NULL,
+    endret_av      VARCHAR      NOT NULL,
+    endretAv       TIMESTAMP(3) NOT NULL,
+    saksbehandler  VARCHAR      NOT NULL,
     beslutter      VARCHAR,
     begrunnelse    VARCHAR,
     årsak          JSON,
-    status         VARCHAR NOT NULL
+    status         VARCHAR      NOT NULL
 
 );
 CREATE INDEX ON totrinnsstatus (behandlings_Id);
