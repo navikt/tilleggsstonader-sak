863dc54d6d20d6d8c72a401cab9ce2d08eb6d4e1
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2023-09-22 12:43:27 +0200

    Oppdatering av Db-schema for totrinnskontroll

--- src/main/resources/db/migration/V12_Totrinnskontroll.sql
+++ src/main/resources/db/migration/V12_Totrinnskontroll.sql
@@ -1,18 +1,16 @@
 CREATE table totrinnsstatus
 (
-    id                                 UUID PRIMARY KEY,
-    behandlings_id                      UUID NOT NULL REFERENCES behandling (id),
-    opprettet_av                        VARCHAR      NOT NULL,
-    opprettet_tid                       TIMESTAMP(3) NOT NULL,
-    endret_av                           VARCHAR      NOT NULL,
-    endret_tid                          TIMESTAMP(3) NOT NULL,
-    saksbehandler                       VARCHAR,
-    beslutter                           VARCHAR,
-    begrunnelse                         VARCHAR,
-    årsakerUnderkjent                   VARCHAR,
-    status                              VARCHAR
+    id             UUID PRIMARY KEY,
+    behandlings_id UUID NOT NULL REFERENCES behandling (id),
+    opprettet_av   VARCHAR NOT NULL,
+    opprettet_tid  TIMESTAMP(3) NOT NULL,
+    endret_av      VARCHAR NOT NULL,
+    sporbar        TIMESTAMP(3) NOT NULL,
+    saksbehandler  JSON,
+    beslutter      JSON,
+    begrunnelse    JSON,
+    årsak          JSON,
+    status         JSON
 
 );
-CREATE INDEX ON totrinnsstatus(behandlings_Id);
-
-CREATE INDEX ON totrinnsstatus(ID);
+CREATE INDEX ON totrinnsstatus (behandlings_Id);
