9ccd2833a9db78d9c1becac0140bb7e7f1417705
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2023-09-25 09:12:33 +0200

    Endring av navn på felt for homogen navgiving i dben

--- src/main/resources/db/migration/V12_Totrinnskontroll.sql
+++ src/main/resources/db/migration/V12_Totrinnskontroll.sql
@@ -1,11 +1,11 @@
 CREATE table totrinnsstatus
 (
     id             UUID PRIMARY KEY,
-    behandlings_id UUID NOT NULL REFERENCES behandling (id),
+    behandlingId UUID NOT NULL REFERENCES behandling (id),
     opprettet_av   VARCHAR      NOT NULL,
     opprettet_tid  TIMESTAMP(3) NOT NULL,
     endret_av      VARCHAR      NOT NULL,
-    endretAv       TIMESTAMP(3) NOT NULL,
+    endret_tid     TIMESTAMP(3) NOT NULL,
     saksbehandler  VARCHAR      NOT NULL,
     beslutter      VARCHAR,
     begrunnelse    VARCHAR,
@@ -13,4 +13,4 @@ CREATE table totrinnsstatus
     status         VARCHAR      NOT NULL
 
 );
-CREATE INDEX ON totrinnsstatus (behandlings_Id);
+CREATE INDEX ON totrinnsstatus (behandlingId);
