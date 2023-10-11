f2eb4964373b055d7b59504e6614b7273387b374
Johan Blomgren <blommish@users.noreply.github.com>
2023-10-11 08:30:28 +0200

    Utvider søknadsdomene med jsonkolonne for å kunne lagre ned søknaden å hente opp den som grunnlag til vilkår

--- src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/søknad/domain/SøknadBarn.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/søknad/domain/SøknadBarn.kt
@@ -1,5 +1,8 @@
 package no.nav.tilleggsstonader.sak.opplysninger.søknad.domain
 
+import no.nav.tilleggsstonader.kontrakter.søknad.JaNei
+import no.nav.tilleggsstonader.kontrakter.søknad.barnetilsyn.TypeBarnepass
+import no.nav.tilleggsstonader.kontrakter.søknad.barnetilsyn.ÅrsakBarnepass
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.util.UUID
@@ -9,4 +12,11 @@ data class SøknadBarn(
     @Id
     val id: UUID = UUID.randomUUID(),
     val ident: String,
+    val data: BarnMedBarnepass,
+)
+
+data class BarnMedBarnepass(
+    val type: TypeBarnepass,
+    val startetIFemte: JaNei?,
+    val årsak: ÅrsakBarnepass?,
 )
