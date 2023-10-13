7a50967638c04fde6bb6436a1f0d1778d60b9c97
Johan Blomgren <blommish@users.noreply.github.com>
2023-10-13 11:37:51 +0200

    Beregning trenger kun en controller?

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/beregning/BeregningController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/beregning/BeregningController.kt
@@ -1,21 +1,22 @@
-package no.nav.tilleggsstonader.sak.vedtak.beregning.barnetilsyn
+package no.nav.tilleggsstonader.sak.vedtak.beregning
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.tilleggsstonader.sak.vedtak.beregning.barnetilsyn.BeregningsresultatTilsynBarnDto
+import no.nav.tilleggsstonader.sak.vedtak.beregning.barnetilsyn.InnvilgelseTilsynBarnDto
+import no.nav.tilleggsstonader.sak.vedtak.beregning.barnetilsyn.TilsynBarnBeregningService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/beregning/tilsyn-barn")
+@RequestMapping("/api/beregning")
 @ProtectedWithClaims(issuer = "azuread")
-class TilsynBarnBeregningController(
+class BeregningController(
     private val tilsynBarnBeregningService: TilsynBarnBeregningService,
 ) {
 
-    @PostMapping
+    @PostMapping("tilsyn-barn")
     fun beregn(vedtak: InnvilgelseTilsynBarnDto): BeregningsresultatTilsynBarnDto {
         return tilsynBarnBeregningService.beregn(vedtak)
     }
-
-    // GET
 }
