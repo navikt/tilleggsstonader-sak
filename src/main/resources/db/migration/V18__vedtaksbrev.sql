dd473f24826a51e5c6e0ed75cea3e0ed44720260
eirarset <eirik.arseth@nav.no>
2023-10-17 13:33:47 +0200

    Formatering

--- src/main/resources/db/migration/V18__vedtaksbrev.sql
+++ src/main/resources/db/migration/V18__vedtaksbrev.sql
@@ -1,11 +1,11 @@
 CREATE TABLE vedtaksbrev
 (
     behandling_id         UUID PRIMARY KEY REFERENCES behandling (id),
-    saksbehandler_html    VARCHAR NOT NULL,
-    saksbehandlersignatur VARCHAR NOT NULL ,
+    saksbehandler_html    VARCHAR      NOT NULL,
+    saksbehandlersignatur VARCHAR      NOT NULL,
     besluttersignatur     VARCHAR,
     beslutter_pdf         BYTEA,
-    saksbehandler_ident   VARCHAR NOT NULL,
+    saksbehandler_ident   VARCHAR      NOT NULL,
     beslutter_ident       VARCHAR,
     opprettet_tid         TIMESTAMP(3) NOT NULL,
     besluttet_tid         TIMESTAMP(3)
