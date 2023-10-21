9c4ef9fb1ea49b6951b98c24f073bf37307c0cc1
Johan Blomgren <blommish@users.noreply.github.com>
2023-10-21 09:40:01 +0200

    Mocker oppgaver og mapper

--- src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/mocks/OppgaveClientConfig.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/mocks/OppgaveClientConfig.kt
@@ -1,6 +1,9 @@
 package no.nav.tilleggsstonader.sak.infrastruktur.mocks
 
+import io.mockk.every
 import io.mockk.mockk
+import no.nav.tilleggsstonader.kontrakter.oppgave.FinnMappeResponseDto
+import no.nav.tilleggsstonader.kontrakter.oppgave.FinnOppgaveResponseDto
 import no.nav.tilleggsstonader.sak.opplysninger.oppgave.OppgaveClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,6 +18,8 @@ class OppgaveClientConfig {
     @Primary
     fun oppgaveClient(): OppgaveClient {
         val oppgaveClient = mockk<OppgaveClient>()
+        every { oppgaveClient.hentOppgaver(any()) } returns FinnOppgaveResponseDto(0, emptyList())
+        every { oppgaveClient.finnMapper(any(), any()) } returns FinnMappeResponseDto(0, emptyList())
         return oppgaveClient
     }
 }
