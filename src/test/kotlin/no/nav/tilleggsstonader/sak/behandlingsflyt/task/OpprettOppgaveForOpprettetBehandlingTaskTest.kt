19dc599727b1f4ecfbcf48ded48de113097a2b8e
eirarset <eirik.arseth@nav.no>
2023-10-24 13:41:33 +0200

    Task for opprettelse av behandle sak oppgave

---
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/behandlingsflyt/task/OpprettOppgaveForOpprettetBehandlingTaskTest.kt
@@ -0,0 +1,83 @@
+package no.nav.tilleggsstonader.sak.behandlingsflyt.task
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.internal.TaskService
+import no.nav.tilleggsstonader.sak.behandling.BehandlingService
+import no.nav.tilleggsstonader.sak.behandling.domain.Behandling
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
+import no.nav.tilleggsstonader.sak.opplysninger.oppgave.OppgaveService
+import no.nav.tilleggsstonader.sak.util.behandling
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+
+internal class OpprettOppgaveForOpprettetBehandlingTaskTest {
+
+    val behandlingService = mockk<BehandlingService>()
+    val oppgaveService = mockk<OppgaveService>()
+    val taskService = mockk<TaskService>()
+    val opprettOppgaveForOpprettetBehandlingTask =
+        OpprettOppgaveForOpprettetBehandlingTask(behandlingService, oppgaveService)
+
+    val oppgaveId = 1L
+
+    val opprettTaskSlot = slot<Task>()
+
+    @BeforeEach
+    internal fun setUp() {
+        every { oppgaveService.opprettOppgave(any(), any(), any(), any(), any()) } returns oppgaveId
+        every { taskService.save(capture(opprettTaskSlot)) } answers { firstArg() }
+    }
+
+    @EnumSource(
+        value = BehandlingStatus::class,
+        names = ["OPPRETTET", "UTREDES"],
+        mode = EnumSource.Mode.INCLUDE,
+    )
+    @ParameterizedTest
+    internal fun `Skal opprette oppgave hvis behandlingen har status opprettet eller utredes`(behandlingStatus: BehandlingStatus) {
+        val behandling = mockBehandling(behandlingStatus)
+
+        opprettOppgaveForOpprettetBehandlingTask.doTask(
+            OpprettOppgaveForOpprettetBehandlingTask.opprettTask(
+                OpprettOppgaveForOpprettetBehandlingTask.OpprettOppgaveTaskData(
+                    behandling.id,
+                    "",
+                ),
+            ),
+        )
+
+        verify(exactly = 1) { oppgaveService.opprettOppgave(any(), any(), any(), any(), any()) }
+    }
+
+    @EnumSource(
+        value = BehandlingStatus::class,
+        names = ["OPPRETTET", "UTREDES"],
+        mode = EnumSource.Mode.EXCLUDE,
+    )
+    @ParameterizedTest
+    internal fun `Skal ikke opprette oppgave hvis behandlingen ikke har status opprettet eller utredes`(behandlingStatus: BehandlingStatus) {
+        val behandling = mockBehandling(behandlingStatus)
+
+        opprettOppgaveForOpprettetBehandlingTask.doTask(
+            OpprettOppgaveForOpprettetBehandlingTask.opprettTask(
+                OpprettOppgaveForOpprettetBehandlingTask.OpprettOppgaveTaskData(
+                    behandling.id,
+                    "",
+                ),
+            ),
+        )
+
+        verify(exactly = 0) { oppgaveService.opprettOppgave(any(), any(), any(), any(), any()) }
+    }
+
+    private fun mockBehandling(status: BehandlingStatus): Behandling {
+        val behandling = behandling(status = status)
+        every { behandlingService.hentBehandling(behandling.id) } returns behandling
+        return behandling
+    }
+}
