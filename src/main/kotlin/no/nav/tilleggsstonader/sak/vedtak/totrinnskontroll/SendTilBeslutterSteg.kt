fabfd0e530fd207d8f64a293470711c2306ca74e
Johan Blomgren <blommish@users.noreply.github.com>
2023-11-02 09:48:44 +0100

    Update src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/totrinnskontroll/SendTilBeslutterSteg.kt
    Co-authored-by: Eirik Emil Neesgaard Årseth <eirik.arseth@nav.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/totrinnskontroll/SendTilBeslutterSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/totrinnskontroll/SendTilBeslutterSteg.kt
@@ -34,7 +34,7 @@ class SendTilBeslutterSteg(
     private val oppgaveService: OppgaveService,
 ) : BehandlingSteg<Void?> {
 
-    // TODO valider at man har opprettet vedtaks?
+    // TODO valider at man har opprettet vedtaksbrev?
     override fun validerSteg(saksbehandling: Saksbehandling) {
         brukerfeilHvis(saksbehandling.steg != stegType()) {
             "Behandling er i feil steg=${saksbehandling.steg}"
