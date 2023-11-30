ddc3ffeea03242ed8c8fceefa75698d43384a764
eirarset <eirik.arseth@nav.no>
2023-11-30 08:54:40 +0100

    Distribuer vedtaksbrev til alle brevmottakere

--- src/main/kotlin/no/nav/tilleggsstonader/sak/brev/DistribuerVedtaksbrevTask.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/brev/DistribuerVedtaksbrevTask.kt
@@ -3,6 +3,14 @@ package no.nav.tilleggsstonader.sak.brev
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.tilleggsstonader.kontrakter.dokdist.DistribuerJournalpostRequest
+import no.nav.tilleggsstonader.kontrakter.dokdist.Distribusjonstype
+import no.nav.tilleggsstonader.kontrakter.felles.Fagsystem
+import no.nav.tilleggsstonader.sak.behandlingsflyt.StegService
+import no.nav.tilleggsstonader.sak.brev.brevmottaker.Brevmottaker
+import no.nav.tilleggsstonader.sak.brev.brevmottaker.BrevmottakerRepository
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
+import no.nav.tilleggsstonader.sak.journalføring.JournalpostClient
 import org.springframework.stereotype.Service
 import java.util.Properties
 import java.util.UUID
@@ -15,11 +23,45 @@ import java.util.UUID
     triggerTidVedFeilISekunder = 31L,
     beskrivelse = "Distribuerer vedtaksbrev etter journalføring",
 )
-class DistribuerVedtaksbrevTask : AsyncTaskStep {
+class DistribuerVedtaksbrevTask(
+    private val brevmottakerRepository: BrevmottakerRepository,
+    private val journalpostClient: JournalpostClient,
+    private val stegService: StegService,
+    private val brevSteg: BrevSteg,
+) : AsyncTaskStep {
 
     override fun doTask(task: Task) {
         val behandlingId = UUID.fromString(task.payload)
-        throw NotImplementedError() // TODO
+
+        val brevmottakere = brevmottakerRepository.findByBehandlingId(behandlingId)
+
+        validerHarBrevmottakere(brevmottakere)
+
+        brevmottakere
+            .filter { it.bestillingId == null }
+            .forEach { brevmottaker ->
+                val bestillingId = distribuerTilBrevmottaker(brevmottaker)
+
+                brevmottakerRepository.update(brevmottaker.copy(bestillingId = bestillingId))
+            }
+
+        stegService.håndterSteg(behandlingId, brevSteg)
+    }
+
+    private fun distribuerTilBrevmottaker(it: Brevmottaker) = journalpostClient.distribuerJournalpost(
+        DistribuerJournalpostRequest(
+            journalpostId = it.journalpostId
+                ?: error("Ugyldig tilstand. Mangler journalpostId for brev som skal distribueres"),
+            bestillendeFagsystem = Fagsystem.TILLEGGSSTONADER,
+            dokumentProdApp = "TILLEGGSSTONADER-SAK",
+            distribusjonstype = Distribusjonstype.VEDTAK,
+        ),
+    )
+
+    private fun validerHarBrevmottakere(brevmottakere: List<Brevmottaker>) {
+        feilHvis(brevmottakere.isEmpty()) {
+            "Ingen brevmottakere"
+        }
     }
 
     companion object {
