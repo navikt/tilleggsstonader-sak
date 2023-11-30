51a0271c26ae631fb68425e13792f6deba53a5e5
eirarset <eirik.arseth@nav.no>
2023-11-30 08:53:58 +0100

    Brevsteg skal opprette task for ferdigstilling av behandling

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/brev/BrevSteg.kt
@@ -0,0 +1,20 @@
+package no.nav.tilleggsstonader.sak.brev
+
+import no.nav.familie.prosessering.internal.TaskService
+import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
+import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
+import no.nav.tilleggsstonader.sak.behandlingsflyt.FerdigstillBehandlingTask
+import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import org.springframework.stereotype.Service
+
+@Service
+class BrevSteg(
+    val taskService: TaskService,
+) : BehandlingSteg<Void?> {
+
+    override fun utførSteg(saksbehandling: Saksbehandling, data: Void?) {
+        taskService.save(FerdigstillBehandlingTask.opprettTask(saksbehandling))
+    }
+
+    override fun stegType(): StegType = StegType.JOURNALFØR_OG_DISTRIBUER_VEDTAKSBREV
+}
