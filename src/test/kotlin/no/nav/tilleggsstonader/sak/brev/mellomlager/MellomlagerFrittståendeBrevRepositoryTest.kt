3773fc7c61798f34124d532c3575c1311e437b42
Eirik Emil Neesgaard Årseth <eirik.arseth@nav.no>
2023-12-05 13:24:18 +0100

    ✨ Mellomlagring av brev (#115)
    * Mellomlagring av brev.
    
    Gjenbrukt fra Enslig forsørger. Legger til endepunkter for å hente og lagre mellomlagrede brev. Frontend har ansvar for å parse/seriaøisere json til en streng som lagres i db.
    
    * Legg på index på fagsak_id
    
    * Refaktor. Skal hente mellomlager basert på hvem som opprettet det, og ikke sistEndret for bedre lesbarhet. Disse to vil alltid være den samme.
    
    * Refaktor. Importer assertThat på toppnivå.

---
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/brev/mellomlager/MellomlagerFrittståendeBrevRepositoryTest.kt
@@ -0,0 +1,61 @@
+package no.nav.tilleggsstonader.sak.brev.mellomlager
+
+import no.nav.tilleggsstonader.sak.IntegrationTest
+import no.nav.tilleggsstonader.sak.util.BrukerContextUtil.testWithBrukerContext
+import no.nav.tilleggsstonader.sak.util.fagsak
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.within
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.temporal.ChronoUnit
+
+internal class MellomlagerFrittståendeBrevRepositoryTest : IntegrationTest() {
+
+    @Autowired
+    private lateinit var mellomlagerFrittståendeBrevRepository: MellomlagerFrittståendeBrevRepository
+
+    @Test
+    internal fun `skal lagre mellomlagret brev`() {
+        val fagsak = testoppsettService.lagreFagsak(fagsak())
+        val mellomlagretBrev = MellomlagretFrittståendeBrev(
+            fagsakId = fagsak.id,
+            brevverdier = "{}",
+            brevmal = "",
+        )
+
+        mellomlagerFrittståendeBrevRepository.insert(mellomlagretBrev)
+
+        val mellomlagretBrevFraDb = mellomlagerFrittståendeBrevRepository.findById(mellomlagretBrev.id)
+        assertThat(mellomlagretBrevFraDb).get()
+            .usingRecursiveComparison()
+            .ignoringFields("sporbar.endret.endretTid")
+            .isEqualTo(mellomlagretBrev)
+        assertThat(mellomlagretBrevFraDb.get().sporbar.endret.endretTid)
+            .isCloseTo(mellomlagretBrev.sporbar.endret.endretTid, within(1, ChronoUnit.SECONDS))
+    }
+
+    @Test
+    internal fun `skal finne igjen mellomlagret brev fra fagsakId og saksbehandlers ident`() {
+        val saksbehandlerIdent = "12345678910"
+        val fagsak = testoppsettService.lagreFagsak(fagsak())
+
+        testWithBrukerContext(saksbehandlerIdent) {
+            val mellomlagretBrev = MellomlagretFrittståendeBrev(
+                fagsakId = fagsak.id,
+                brevverdier = "{}",
+                brevmal = "",
+            )
+
+            mellomlagerFrittståendeBrevRepository.insert(mellomlagretBrev)
+            val mellomlagretBrevFraDb =
+                mellomlagerFrittståendeBrevRepository.findByFagsakIdAndSporbarOpprettetAv(fagsak.id, saksbehandlerIdent)
+            assertThat(mellomlagretBrevFraDb)
+                .usingRecursiveComparison()
+                .ignoringFields("sporbar.endret.endretTid")
+                .isEqualTo(mellomlagretBrev)
+
+            assertThat(mellomlagretBrevFraDb!!.sporbar.endret.endretTid)
+                .isCloseTo(mellomlagretBrev.sporbar.endret.endretTid, within(1, ChronoUnit.SECONDS))
+        }
+    }
+}
