c8967f6835bdf702f7003ff5cf6dcae9bea91162
Sara Hjelle <46678893+sarahjelle@users.noreply.github.com>
2023-12-07 09:37:28 +0100

    🚧 Lag egen tabell i databasen for å lagre stønadsperioder (#117)

---
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/domain/StønadsperiodeRepositoryTest.kt
@@ -0,0 +1,58 @@
+package no.nav.tilleggsstonader.sak.vilkår.domain
+
+import no.nav.tilleggsstonader.sak.IntegrationTest
+import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
+import no.nav.tilleggsstonader.sak.util.behandling
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDate
+
+internal class StønadsperiodeRepositoryTest : IntegrationTest() {
+    @Autowired
+    lateinit var stønadsperiodeRepository: StønadsperiodeRepository
+
+    @Test
+    internal fun `skal kunne lagre og hente stønadsperiode`() {
+        val behandling = testoppsettService.opprettBehandlingMedFagsak(behandling = behandling())
+        val stønadsperiode = stønadsperiodeRepository.insert(
+            Stønadsperiode(
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(5),
+                behandlingId = behandling.id,
+                målgruppe = MålgruppeType.AAP,
+                aktivitet = AktivitetType.TILTAK,
+            ),
+        )
+
+        assertThat(stønadsperiodeRepository.findByIdOrThrow(stønadsperiode.id)).isEqualTo(stønadsperiode)
+    }
+
+    @Test
+    internal fun `skal finne alle stønadsperioder for behandling`() {
+        val behandling = testoppsettService.opprettBehandlingMedFagsak(behandling = behandling())
+
+        val stønadsperiode1 = stønadsperiodeRepository.insert(
+            Stønadsperiode(
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(5),
+                behandlingId = behandling.id,
+                målgruppe = MålgruppeType.AAP,
+                aktivitet = AktivitetType.TILTAK,
+            ),
+        )
+
+        val stønadsperiode2 = stønadsperiodeRepository.insert(
+            Stønadsperiode(
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(5),
+                behandlingId = behandling.id,
+                målgruppe = MålgruppeType.AAP,
+                aktivitet = AktivitetType.TILTAK,
+            ),
+        )
+
+        assertThat(stønadsperiodeRepository.findAllByBehandlingId(behandling.id))
+            .containsExactlyInAnyOrder(stønadsperiode1, stønadsperiode2)
+    }
+}
