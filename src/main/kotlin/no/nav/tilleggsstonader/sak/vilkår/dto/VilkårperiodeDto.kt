e17ba88c4426e93cd7366b873699cea3ab3cd272
Johan Blomgren <blommish@users.noreply.github.com>
2023-12-07 10:14:26 +0100

    Lagt til endepunkt for å hente å lagre stønadsperioder

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/dto/VilkårperiodeDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/dto/VilkårperiodeDto.kt
@@ -1,11 +1,19 @@
 package no.nav.tilleggsstonader.sak.vilkår.dto
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.tilleggsstonader.kontrakter.felles.ObjectMapperProvider.objectMapper
 import no.nav.tilleggsstonader.kontrakter.felles.Periode
 import no.nav.tilleggsstonader.sak.vilkår.domain.Vilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.domain.VilkårperiodeType
+import no.nav.tilleggsstonader.sak.vilkår.domain.vilkårperiodetyper
 import java.time.LocalDate
 
 data class VilkårperiodeDto(
+    @JsonDeserialize(using = VilkårperiodeTypeDeserializer::class)
     val type: VilkårperiodeType,
     override val fom: LocalDate,
     override val tom: LocalDate,
@@ -21,6 +29,7 @@ fun Vilkårperiode.tilDto(vilkår: VilkårDto) =
     )
 
 data class OpprettVilkårperiode(
+    @JsonDeserialize(using = VilkårperiodeTypeDeserializer::class)
     val type: VilkårperiodeType,
     override val fom: LocalDate,
     override val tom: LocalDate,
@@ -30,3 +39,11 @@ data class Vilkårperioder(
     val målgrupper: List<VilkårperiodeDto>,
     val aktiviteter: List<VilkårperiodeDto>,
 )
+
+// TODO test controller sånn at vi får testet denne
+class VilkårperiodeTypeDeserializer : JsonDeserializer<VilkårperiodeType>() {
+    override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): VilkårperiodeType {
+        return vilkårperiodetyper[p!!.text] ?: error("Finner ikke mapping for ${p.text}")
+    }
+
+}
