cf350b51cfb537c4584cc4b428ab9a02bb4335a2
sarahjelle <sara.hjelle@bekk.no>
2024-01-11 13:47:46 +0100

    Flytt logikk til service

--- src/main/kotlin/no/nav/tilleggsstonader/sak/journalføring/JournalpostController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/journalføring/JournalpostController.kt
@@ -1,12 +1,6 @@
 package no.nav.tilleggsstonader.sak.journalføring
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.tilleggsstonader.kontrakter.felles.BrukerIdType
-import no.nav.tilleggsstonader.kontrakter.journalpost.Dokumentvariantformat
-import no.nav.tilleggsstonader.kontrakter.journalpost.Journalpost
-import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
-import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
-import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
 import no.nav.tilleggsstonader.sak.tilgang.AuditLoggerEvent
 import no.nav.tilleggsstonader.sak.tilgang.TilgangService
 import org.springframework.http.MediaType
@@ -22,43 +16,14 @@ import org.springframework.web.bind.annotation.RestController
 @Validated
 class JournalpostController(
     private val journalpostService: JournalpostService,
-    private val personService: PersonService,
     private val tilgangService: TilgangService,
 ) {
     @GetMapping("/{journalpostId}/dokument-pdf/{dokumentInfoId}", produces = [MediaType.APPLICATION_PDF_VALUE])
     fun hentDokumentSomPdf(@PathVariable journalpostId: String, @PathVariable dokumentInfoId: String): ByteArray {
-        val (journalpost, personIdent) = finnJournalpostOgPersonIdent(journalpostId)
+        val (journalpost, personIdent) = journalpostService.finnJournalpostOgPersonIdent(journalpostId)
+
         tilgangService.validerTilgangTilPersonMedBarn(personIdent, AuditLoggerEvent.ACCESS)
-        validerDokumentKanHentes(journalpost, dokumentInfoId, journalpostId)
-        return journalpostService.hentDokument(journalpostId, dokumentInfoId)
-    }
 
-    private fun validerDokumentKanHentes(
-        journalpost: Journalpost,
-        dokumentInfoId: String,
-        journalpostId: String,
-    ) {
-        val dokument = journalpost.dokumenter?.find { it.dokumentInfoId == dokumentInfoId }
-        feilHvis(dokument == null) {
-            "Finner ikke dokument med $dokumentInfoId for journalpost=$journalpostId"
-        }
-        brukerfeilHvisIkke(
-            dokument.dokumentvarianter?.any { it.variantformat == Dokumentvariantformat.ARKIV }
-                ?: false,
-        ) {
-            "Vedlegget er sannsynligvis under arbeid, må åpnes i gosys"
-        }
-    }
-
-    private fun finnJournalpostOgPersonIdent(journalpostId: String): Pair<Journalpost, String> {
-        val journalpost = journalpostService.hentJournalpost(journalpostId)
-        val personIdent = journalpost.bruker?.let {
-            when (it.type) {
-                BrukerIdType.FNR -> it.id
-                BrukerIdType.AKTOERID -> personService.hentPersonIdenter(it.id).gjeldende().ident
-                BrukerIdType.ORGNR -> error("Kan ikke hente journalpost=$journalpostId for orgnr")
-            }
-        } ?: error("Kan ikke hente journalpost=$journalpostId uten bruker")
-        return Pair(journalpost, personIdent)
+        return journalpostService.hentDokument(journalpost, dokumentInfoId)
     }
 }
--- src/main/kotlin/no/nav/tilleggsstonader/sak/journalføring/JournalpostService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/journalføring/JournalpostService.kt
@@ -12,11 +12,14 @@ import no.nav.tilleggsstonader.kontrakter.sak.DokumentBrevkode
 import no.nav.tilleggsstonader.kontrakter.søknad.Søknadsskjema
 import no.nav.tilleggsstonader.kontrakter.søknad.barnetilsyn.SøknadsskjemaBarnetilsyn
 import no.nav.tilleggsstonader.sak.fagsak.domain.Fagsak
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
+import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
 import no.nav.tilleggsstonader.sak.vedlegg.VedleggRequest
 import org.springframework.stereotype.Service
 
 @Service
-class JournalpostService(private val journalpostClient: JournalpostClient) {
+class JournalpostService(private val journalpostClient: JournalpostClient, private val personService: PersonService) {
     fun finnJournalposterForBruker(personIdent: String, vedleggRequest: VedleggRequest): List<Journalpost> {
         return journalpostClient.finnJournalposterForBruker(
             JournalposterForBrukerRequest(
@@ -33,7 +36,10 @@ class JournalpostService(private val journalpostClient: JournalpostClient) {
         return journalpostClient.hentJournalpost(journalpostId)
     }
 
-    fun opprettJournalpost(arkiverDokumentRequest: ArkiverDokumentRequest, saksbehandler: String? = null): ArkiverDokumentResponse {
+    fun opprettJournalpost(
+        arkiverDokumentRequest: ArkiverDokumentRequest,
+        saksbehandler: String? = null,
+    ): ArkiverDokumentResponse {
         return journalpostClient.opprettJournalpost(arkiverDokumentRequest, saksbehandler)
     }
 
@@ -71,7 +77,11 @@ class JournalpostService(private val journalpostClient: JournalpostClient) {
         }
     }
 
-    private fun ferdigstillJournalføring(journalpostId: String, journalførendeEnhet: String, saksbehandler: String? = null) {
+    private fun ferdigstillJournalføring(
+        journalpostId: String,
+        journalførendeEnhet: String,
+        saksbehandler: String? = null,
+    ) {
         journalpostClient.ferdigstillJournalpost(journalpostId, journalførendeEnhet, saksbehandler)
     }
 
@@ -81,7 +91,8 @@ class JournalpostService(private val journalpostClient: JournalpostClient) {
         eksternFagsakId: Long,
         saksbehandler: String? = null,
     ) {
-        val oppdatertJournalpost = JournalføringHelper.lagOppdaterJournalpostRequest(journalpost, eksternFagsakId, dokumenttitler)
+        val oppdatertJournalpost =
+            JournalføringHelper.lagOppdaterJournalpostRequest(journalpost, eksternFagsakId, dokumenttitler)
         journalpostClient.oppdaterJournalpost(oppdatertJournalpost, journalpost.journalpostId, saksbehandler)
     }
 
@@ -90,11 +101,40 @@ class JournalpostService(private val journalpostClient: JournalpostClient) {
         return journalpostClient.hentSøknadTilsynBarn(søknadJournalpost.journalpostId, dokumentinfo.dokumentInfoId)
     }
 
+    fun finnJournalpostOgPersonIdent(journalpostId: String): Pair<Journalpost, String> {
+        val journalpost = hentJournalpost(journalpostId)
+        val personIdent = journalpost.bruker?.let {
+            when (it.type) {
+                BrukerIdType.FNR -> it.id
+                BrukerIdType.AKTOERID -> personService.hentPersonIdenter(it.id).gjeldende().ident
+                BrukerIdType.ORGNR -> error("Kan ikke hente journalpost=$journalpostId for orgnr")
+            }
+        } ?: error("Kan ikke hente journalpost=$journalpostId uten bruker")
+        return Pair(journalpost, personIdent)
+    }
+
     fun hentDokument(
-        journalpostId: String,
+        journalpost: Journalpost,
         dokumentInfoId: String,
         dokumentVariantformat: Dokumentvariantformat = Dokumentvariantformat.ARKIV,
     ): ByteArray {
-        return journalpostClient.hentDokument(journalpostId, dokumentInfoId, dokumentVariantformat)
+        validerDokumentKanHentes(journalpost, dokumentInfoId)
+        return journalpostClient.hentDokument(journalpost.journalpostId, dokumentInfoId, dokumentVariantformat)
+    }
+
+    private fun validerDokumentKanHentes(
+        journalpost: Journalpost,
+        dokumentInfoId: String,
+    ) {
+        val dokument = journalpost.dokumenter?.find { it.dokumentInfoId == dokumentInfoId }
+        feilHvis(dokument == null) {
+            "Finner ikke dokument med id=$dokumentInfoId for journalpost med id=${journalpost.journalpostId}"
+        }
+        brukerfeilHvisIkke(
+            dokument.dokumentvarianter?.any { it.variantformat == Dokumentvariantformat.ARKIV }
+                ?: false,
+        ) {
+            "Vedlegget er sannsynligvis under arbeid, må åpnes i gosys"
+        }
     }
 }
