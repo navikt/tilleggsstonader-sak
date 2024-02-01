4e2605d703cf84e68da3bea0559143d1edb0f8e5
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-01 16:33:21 +0100

    Lagt til htmlify-klient og kontrakt for internt vedtak

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/interntVedtak/HtmlifyClient.kt
@@ -0,0 +1,24 @@
+package no.nav.tilleggsstonader.sak.interntVedtak
+
+import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@Component
+class HtmlifyClient(
+    @Value("\${clients.htmlify.uri}")
+    private val uri: URI,
+    @Qualifier("utenAuth") restTemplate: RestTemplate,
+) : AbstractRestClient(restTemplate) {
+
+    fun generateHtml(interntVedtak: InterntVedtak): String {
+        return postForEntity<String>(
+            UriComponentsBuilder.fromUri(uri).pathSegment("api", "internt-vedtak").toUriString(),
+            interntVedtak,
+        )
+    }
+}
---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/interntVedtak/InterntVedtak.kt
@@ -0,0 +1,68 @@
+package no.nav.tilleggsstonader.sak.interntVedtak
+
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingÅrsak
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.KildeVilkårsperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatDelvilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatVilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.VilkårperiodeType
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class InterntVedtak(
+    val behandling: Behandlinginfo,
+    val søknad: Søknadsinformasjon?,
+    val målgrupper: List<Vilkårperiode>,
+    val aktiviteter: List<Vilkårperiode>,
+    val stønadsperioder: List<Stønadsperiode>,
+)
+
+data class Behandlinginfo(
+    val behandlingId: UUID,
+    val eksternFagsakId: Long,
+    val stønadstype: Stønadstype,
+    val årsak: BehandlingÅrsak,
+    val ident: String,
+    val opprettetTidspunkt: LocalDateTime,
+    val resultat: BehandlingResultat,
+    val vedtakstidspunkt: LocalDateTime,
+    val saksbehandler: String,
+    val beslutter: String?,
+)
+
+data class Søknadsinformasjon(
+    val mottattTidspunkt: LocalDateTime,
+)
+
+data class Vilkårperiode(
+    val type: VilkårperiodeType,
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val delvilkår: DelvilkårVilkårperiode,
+    val kilde: KildeVilkårsperiode,
+    val resultat: ResultatVilkårperiode,
+    val begrunnelse: String?,
+)
+
+data class DelvilkårVilkårperiode(
+    val medlemskap: VurderingVilkårperiode?,
+    val lønnet: VurderingVilkårperiode?,
+    val mottarSykepenger: VurderingVilkårperiode?,
+)
+
+data class VurderingVilkårperiode(
+    val svar: String?,
+    val begrunnelse: String?,
+    val resultat: ResultatDelvilkårperiode,
+)
+
+data class Stønadsperiode(
+    val målgruppe: MålgruppeType,
+    val aktivitet: AktivitetType,
+    val fom: LocalDate,
+    val tom: LocalDate,
+)
