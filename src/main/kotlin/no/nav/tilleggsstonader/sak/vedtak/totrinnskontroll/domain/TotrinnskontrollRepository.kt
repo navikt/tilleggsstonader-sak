22e45de6d150c3c5bc3e6eaea273e78df1c19750
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-06 10:52:32 +0100

    Fikset tester og gjort en liten opprydding

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/totrinnskontroll/domain/TotrinnskontrollRepository.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/totrinnskontroll/domain/TotrinnskontrollRepository.kt
@@ -3,20 +3,12 @@ package no.nav.tilleggsstonader.sak.vedtak.totrinnskontroll.domain
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.InsertUpdateRepository
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.RepositoryInterface
 import org.springframework.stereotype.Repository
-import java.util.*
+import java.util.UUID
 
 @Repository
 interface TotrinnskontrollRepository :
     RepositoryInterface<Totrinnskontroll, UUID>,
     InsertUpdateRepository<Totrinnskontroll> {
 
-    fun findTopByBehandlingIdAndStatusOrderBySporbarEndretEndretTidDesc(
-        behandlingId: UUID,
-        status: TotrinnInternStatus,
-    ): Totrinnskontroll
-
     fun findTopByBehandlingIdOrderBySporbarEndretEndretTidDesc(behandlingId: UUID): Totrinnskontroll?
-
-    // TODO trengs denne?
-    fun findAllByBehandlingId(behandlingId: UUID): List<Totrinnskontroll>
 }
