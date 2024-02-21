82817db9f35300798cf8166292f97e5660cf3b79
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-21 08:36:42 +0100

    lint

--- src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/fakta/BehandlingFaktaDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/fakta/BehandlingFaktaDto.kt
@@ -20,7 +20,7 @@ data class FaktaHovedytelse(
 data class SøknadsgrunnlagHovedytelse(
     val hovedytelse: List<Hovedytelse>,
     val boddSammenhengende: JaNei?,
-    val planleggerBoINorgeNeste12mnd: JaNei?
+    val planleggerBoINorgeNeste12mnd: JaNei?,
 )
 
 data class FaktaAktivtet(
