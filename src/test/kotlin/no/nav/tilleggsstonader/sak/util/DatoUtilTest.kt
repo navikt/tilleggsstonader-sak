e7b656a3f63c5f0f1563c890f0fd9177ee173c98
Runar Skagestad <runska@gmail.com>
2024-03-04 15:29:52 +0100

    Skriver flere tester for funksjon som kalkulerer alder

--- src/test/kotlin/no/nav/tilleggsstonader/sak/util/DatoUtilTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/util/DatoUtilTest.kt
@@ -15,4 +15,17 @@ class DatoUtilTest {
     fun `antallÅrSiden skal returnere null hvis vi sender inn null`() {
         assertThat(antallÅrSiden(null)).isNull()
     }
+
+
+    @Test
+    fun `antallÅrSiden skal returnere 0 hvis vi sender inn dagens dato for ett år siden`() {
+        val dagensDatoIFjor = LocalDate.now().minusYears(1)
+        assertThat(antallÅrSiden(dagensDatoIFjor)).isEqualTo(0)
+    }
+
+    @Test
+    fun `antallÅrSiden skal returnere 1 hvis vi sender inn gårsdagens dato minus ett år`() {
+        val gårsdagensDatoIFjor = LocalDate.now().minusYears(1).minusDays(1)
+        assertThat(antallÅrSiden(gårsdagensDatoIFjor)).isEqualTo(1)
+    }
 }
