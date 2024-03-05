1bf52cd1952abc93a2ffc60d30f7e2dab306af3a
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-05 21:03:24 +0100

    Lagt til repository for å lagre ned årsaker til sett på vent

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/vent/SettPåVent.kt
@@ -0,0 +1,28 @@
+package no.nav.tilleggsstonader.sak.behandling.vent
+
+import no.nav.tilleggsstonader.sak.infrastruktur.database.Sporbar
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Embedded
+import org.springframework.data.relational.core.mapping.Table
+import java.util.UUID
+
+@Table("sett_pa_vent")
+data class SettPåVent(
+    @Id
+    val id: UUID = UUID.randomUUID(),
+    val behandlingId: UUID,
+    val oppgaveId: Long,
+    @Column("arsaker")
+    val årsaker: List<ÅrsakSettPåVent>,
+    val aktiv: Boolean = true,
+    @Embedded(onEmpty = Embedded.OnEmpty.USE_EMPTY)
+    val sporbar: Sporbar = Sporbar(),
+) {
+    init {
+        feilHvis(årsaker.isEmpty()) {
+            "SettPåVent må inneholde årsaker for behandling=$behandlingId"
+        }
+    }
+}
