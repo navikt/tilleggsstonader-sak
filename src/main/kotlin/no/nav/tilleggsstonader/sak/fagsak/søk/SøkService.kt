926363e77970f7ca411305bbac14f743fb1e30d1
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-15 16:11:43 +0100

    Søk etter person returnerer Søkeresultat med tom fagsakPersonId hvis personen har sak i Arena

--- src/main/kotlin/no/nav/tilleggsstonader/sak/fagsak/søk/SøkService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/fagsak/søk/SøkService.kt
@@ -5,7 +5,7 @@ import no.nav.tilleggsstonader.sak.fagsak.domain.FagsakPerson
 import no.nav.tilleggsstonader.sak.fagsak.domain.FagsakPersonService
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.ApiFeil
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
-import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
+import no.nav.tilleggsstonader.sak.opplysninger.arena.ArenaService
 import no.nav.tilleggsstonader.sak.opplysninger.dto.NavnDto
 import no.nav.tilleggsstonader.sak.opplysninger.mapper.KjønnMapper
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
@@ -25,6 +25,7 @@ class SøkService(
     private val fagsakPersonService: FagsakPersonService,
     private val personService: PersonService,
     private val fagsakService: FagsakService,
+    private val arenaService: ArenaService,
 ) {
 
     fun søkPersonForEksternFagsak(eksternFagsakId: Long): Søkeresultat {
@@ -39,13 +40,16 @@ class SøkService(
             "Finner ingen personer for valgt personident"
         }
         val gjeldendePersonIdent = personIdenter.gjeldende().ident
-        // TODO opprett fagsak hvis personen finnes i Arena? (finnFagsakEllerOpprettHvisPersonFinnesIInfotrygd)
-        brukerfeilHvisIkke(fagsakService.harFagsak(personIdenter.identer())) {
-            "Finner ikke fagsak for søkte personen"
-        }
-        val fagsakPerson = fagsakPersonService.finnPerson(personIdenter.identer())
 
-        return tilSøkeresultat(gjeldendePersonIdent, fagsakPerson)
+        val fagsakPerson = fagsakPersonService.finnPerson(personIdenter.identer())
+        if (fagsakPerson != null) {
+            return tilSøkeresultat(gjeldendePersonIdent, fagsakPerson)
+        }
+        if (arenaService.harSaker(gjeldendePersonIdent)) {
+            return tilSøkeresultat(gjeldendePersonIdent, null)
+        }
+
+        throw ApiFeil("Personen har ikke fagsak eller sak i arena", HttpStatus.BAD_REQUEST)
     }
 
     private fun tilSøkeresultat(
