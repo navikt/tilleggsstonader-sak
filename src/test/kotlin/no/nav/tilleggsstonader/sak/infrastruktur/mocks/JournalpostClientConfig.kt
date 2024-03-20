249322c73773ad4ec9e935cd7035b01fb78d1c80
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-03-20 10:49:10 +0100

    skal sende med dokumenter som pdf til frontend for visning i journalføringsbildet. Retter opp i skrivefeil i felt på DokumentInfoDto

--- src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/mocks/JournalpostClientConfig.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/mocks/JournalpostClientConfig.kt
@@ -5,7 +5,16 @@ import io.mockk.mockk
 import no.nav.tilleggsstonader.kontrakter.dokarkiv.ArkiverDokumentResponse
 import no.nav.tilleggsstonader.kontrakter.felles.BrukerIdType
 import no.nav.tilleggsstonader.kontrakter.felles.Tema
-import no.nav.tilleggsstonader.kontrakter.journalpost.*
+import no.nav.tilleggsstonader.kontrakter.journalpost.AvsenderMottaker
+import no.nav.tilleggsstonader.kontrakter.journalpost.AvsenderMottakerIdType
+import no.nav.tilleggsstonader.kontrakter.journalpost.Bruker
+import no.nav.tilleggsstonader.kontrakter.journalpost.DokumentInfo
+import no.nav.tilleggsstonader.kontrakter.journalpost.Dokumentvariant
+import no.nav.tilleggsstonader.kontrakter.journalpost.Dokumentvariantformat
+import no.nav.tilleggsstonader.kontrakter.journalpost.Journalpost
+import no.nav.tilleggsstonader.kontrakter.journalpost.Journalposttype
+import no.nav.tilleggsstonader.kontrakter.journalpost.Journalstatus
+import no.nav.tilleggsstonader.kontrakter.journalpost.LogiskVedlegg
 import no.nav.tilleggsstonader.sak.journalføring.JournalpostClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -70,15 +79,26 @@ class JournalpostClientConfig {
                         LogiskVedlegg("1", "Dokumentasjon på sykdom"),
                         LogiskVedlegg("2", "Inntektsendring"),
                         LogiskVedlegg("3", "Samværsmelding"),
-                    )
+                    ),
+                    dokumentvarianter = listOf(
+                        Dokumentvariant(
+                            variantformat = Dokumentvariantformat.ARKIV,
+                            saksbehandlerHarTilgang = true,
+                        ),
+                    ),
                 ),
                 DokumentInfo(
                     dokumentInfoId = "2",
                     tittel = "Dummy dokument 2",
-                )
-            )
+                    dokumentvarianter = listOf(
+                        Dokumentvariant(
+                            variantformat = Dokumentvariantformat.ARKIV,
+                            saksbehandlerHarTilgang = true,
+                        ),
+                    ),
+                ),
+            ),
         )
-
 
     private val dummyPdf = this::class.java.classLoader.getResource("interntVedtak/internt_vedtak.pdf")!!.readBytes()
 
@@ -87,7 +107,7 @@ class JournalpostClientConfig {
         type = AvsenderMottakerIdType.FNR,
         navn = "Ola Nordmann",
         land = "NOR",
-        erLikBruker = true
+        erLikBruker = true,
     )
 
     companion object {
