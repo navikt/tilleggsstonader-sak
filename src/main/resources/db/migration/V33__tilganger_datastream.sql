01e8639c27597d9ed9733a40d6aef8ac51462e54
Runar Skagestad <runska@gmail.com>
2024-04-09 13:17:00 +0200

    V33 skal ikke kjøre lokalt eller i tester
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>
    Co-authored-by: Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>

--- src/main/resources/db/migration/V33__tilganger_datastream.sql
+++ src/main/resources/db/migration/V33__tilganger_datastream.sql
@@ -1,7 +1,16 @@
-ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
-GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
+DO
+$$
+    BEGIN
+        IF EXISTS
+                (SELECT 1 from pg_roles where rolname = 'datastream')
+        THEN
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
+            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
 
-ALTER USER "tilleggsstonader-sak" WITH REPLICATION;
-ALTER USER "datastream" WITH REPLICATION;
-CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
-SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+            ALTER USER "tilleggsstonader-sak" WITH REPLICATION;
+            ALTER USER "datastream" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+            SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        END IF;
+    END
+$$;
