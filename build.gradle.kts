00c4fe2537b8657be594a974c208fa167b8efd9f
Runar Skagestad <runska@gmail.com>
2024-04-24 14:17:20 +0200

    Oppdaterer kontrakter
    Co-authored-by: Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.io.ByteArrayOutputStream
 val javaVersion = JavaLanguageVersion.of(21)
 val familieProsesseringVersion = "2.20240214140223_83c31de"
 val tilleggsstønaderLibsVersion = "2024.04.02-08.44.ef5242ac90bc"
-val tilleggsstønaderKontrakterVersion = "2024.04.24-12.46.24506d96746d"
+val tilleggsstønaderKontrakterVersion = "2024.04.24-14.08.6d447de5b4f0"
 val tokenSupportVersion = "4.1.4"
 val wiremockVersion = "3.5.2"
 val mockkVersion = "1.13.10"
