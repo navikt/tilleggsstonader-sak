003898a9058c068d88153e0aa291314dda2e763a
Runar Skagestad <runska@gmail.com>
2024-04-24 15:09:45 +0200

    Rename regel "HAR_ALDER_LAVERE_ENN_GRENSEVERDI" til "HAR_FULLFØRT_FJERDEKLASSE"

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/regler/vilkår/PassBarnRegelTestUtil.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/regler/vilkår/PassBarnRegelTestUtil.kt
@@ -9,7 +9,7 @@ import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.SvarId
 object PassBarnRegelTestUtil {
 
     fun oppfylteDelvilkårPassBarn() = listOf(
-        delvilkårDto(VurderingDto(RegelId.HAR_ALDER_LAVERE_ENN_GRENSEVERDI, SvarId.NEI, "en begrunnelse")),
+        delvilkårDto(VurderingDto(RegelId.HAR_FULLFØRT_FJERDEKLASSE, SvarId.NEI, "en begrunnelse")),
         delvilkårDto(VurderingDto(RegelId.ANNEN_FORELDER_MOTTAR_STØTTE, SvarId.NEI)),
         delvilkårDto(VurderingDto(RegelId.UTGIFTER_DOKUMENTERT, SvarId.JA)),
     )
