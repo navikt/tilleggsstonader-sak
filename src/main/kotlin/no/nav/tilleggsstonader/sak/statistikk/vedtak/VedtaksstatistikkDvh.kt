db8c0e29845b0bac33576b236a4f0710e268a3c0
Runar Skagestad <runska@gmail.com>
2024-05-03 11:05:34 +0200

    Map ut vilkårsvurderinger til vedtaksstatistikk
    Co-authored-by: Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/VedtaksstatistikkDvh.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/VedtaksstatistikkDvh.kt
@@ -4,6 +4,7 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingÅrsak
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.dto.AdressebeskyttelseGradering
 import no.nav.tilleggsstonader.sak.statistikk.vedtak.StønadstypeDvh.BARNETILSYN
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkårsresultat
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dto.DelvilkårDto
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.RegelId
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
@@ -11,7 +12,7 @@ import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatVilkår
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.Vilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.VilkårperiodeType
 import java.time.LocalDate
-import java.time.ZonedDateTime
+import java.time.LocalDateTime
 import java.util.UUID
 
 // TODO: Vurder om dette bør flyttes til kontrakter
@@ -22,10 +23,10 @@ class VedtaksstatistikkDvh(
     val eksternBehandlingId: Long,
     val relatertBehandlingId: Long?, // Ekstern behandlingsid på relatert behandling
     val adressebeskyttelse: AdressebeskyttelseDvh,
-    val tidspunktVedtak: ZonedDateTime,
+    val tidspunktVedtak: LocalDateTime,
     val målgruppe: List<MålgruppeDvh>,
     val aktivitet: List<AktivitetDvh>,
-    val vilkårsvureringer: List<VilkårsvurderingDvh>,
+    val vilkårsvurderinger: List<VilkårsvurderingDvh>,
     val person: String,
     val barn: List<BarnDvh>,
     val behandlingType: BehandlingTypeDvh,
@@ -38,7 +39,7 @@ class VedtaksstatistikkDvh(
     val årsakRevurdering: ÅrsakRevurderingDvh? = null,
     val avslagÅrsak: AvslagÅrsakDvh? = null,
 
-)
+    )
 
 enum class StønadstypeDvh {
     BARNETILSYN,
@@ -101,12 +102,25 @@ data class BarnDvh(
 
 data class AktivitetDvh(
     val type: AktivitetTypeDvh,
-    val resultat: ResultatDvh,
-)
+    val resultat: ResultatVilkårperiodeDvh,
+) {
+    companion object {
+        fun fraDomene(vilkårsperioder: List<Vilkårperiode>): List<AktivitetDvh> {
+            return vilkårsperioder
+                .filterNot { ResultatVilkårperiode.SLETTET == it.resultat }
+                .map {
+                    AktivitetDvh(
+                        type = AktivitetTypeDvh.fraDomene(it.type),
+                        resultat = ResultatVilkårperiodeDvh.fraDomene(it.resultat),
+                    )
+                }
+        }
+    }
+}
 
 data class MålgruppeDvh(
     val type: MålgruppeTypeDvh,
-    val resultat: ResultatDvh,
+    val resultat: ResultatVilkårperiodeDvh,
 ) {
     companion object {
         fun fraDomene(vilkårsperioder: List<Vilkårperiode>): List<MålgruppeDvh> {
@@ -115,20 +129,17 @@ data class MålgruppeDvh(
                 .map {
                     MålgruppeDvh(
                         type = MålgruppeTypeDvh.fraDomene(it.type),
-                        resultat = ResultatDvh.fraDomene(it.resultat),
+                        resultat = ResultatVilkårperiodeDvh.fraDomene(it.resultat),
                     )
                 }
         }
     }
 }
 
-enum class ResultatDvh {
+enum class ResultatVilkårperiodeDvh {
     OPPFYLT,
-    AUTOMATISK_OPPFYLT,
     IKKE_OPPFYLT,
-    IKKE_AKTUELL,
     IKKE_TATT_STILLING_TIL,
-    SKAL_IKKE_VURDERES,
     ;
 
     companion object {
@@ -138,6 +149,28 @@ enum class ResultatDvh {
             ResultatVilkårperiode.IKKE_VURDERT -> IKKE_TATT_STILLING_TIL
             ResultatVilkårperiode.SLETTET ->
                 throw IllegalArgumentException("Slettede vedtak er ikke relevant, og skal ha blitt filtrert bort.")
+        }
+    }
+}
+
+enum class VilkårsresultatDvh {
+    OPPFYLT,
+    AUTOMATISK_OPPFYLT,
+    IKKE_OPPFYLT,
+    IKKE_AKTUELL,
+    IKKE_TATT_STILLING_TIL,
+    SKAL_IKKE_VURDERES,
+    ;
+
+    companion object {
+        fun fraDomene(resultat: Vilkårsresultat) = when (resultat) {
+            Vilkårsresultat.OPPFYLT -> OPPFYLT
+            Vilkårsresultat.AUTOMATISK_OPPFYLT -> AUTOMATISK_OPPFYLT
+            Vilkårsresultat.IKKE_OPPFYLT -> IKKE_OPPFYLT
+            Vilkårsresultat.IKKE_AKTUELL -> IKKE_AKTUELL
+            Vilkårsresultat.IKKE_TATT_STILLING_TIL -> IKKE_TATT_STILLING_TIL
+            Vilkårsresultat.SKAL_IKKE_VURDERES -> SKAL_IKKE_VURDERES
+            // TODO: Vurder om vi trenger alle disse
         }
     }
 }
@@ -185,11 +218,31 @@ enum class MålgruppeTypeDvh {
 }
 
 data class VilkårsvurderingDvh(
-    val resultat: Vilkårsresultat,
-    val vilkårId: RegelId,
+    val resultat: VilkårsresultatDvh,
+    val vilkår: List<DelvilkårDvh>,
+) {
+    companion object {
+        fun fraDomene(delvilkår: List<DelvilkårDto>, resultat: Vilkårsresultat) = VilkårsvurderingDvh(
+            resultat = VilkårsresultatDvh.fraDomene(resultat),
+            vilkår = DelvilkårDvh.fraDomene(delvilkår)
+        )
+    }
+}
 
-    // TODO: Mapping fra VilkårDto
-)
+data class DelvilkårDvh(
+    val resultat: Vilkårsresultat,
+    val vurderinger: List<RegelId>, // Anti corruption layer
+) {
+    companion object {
+        fun fraDomene(delvilkår: List<DelvilkårDto>): List<DelvilkårDvh> {
+            return delvilkår.map {
+                DelvilkårDvh(
+                    resultat = it.resultat,
+                    vurderinger = it.vurderinger.map { vurdering -> vurdering.regelId })
+            }
+        }
+    }
+}
 
 enum class AdressebeskyttelseDvh {
     STRENGT_FORTROLIG,
--- src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/VedtaksstatistikkService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/VedtaksstatistikkService.kt
@@ -1,12 +1,68 @@
 package no.nav.tilleggsstonader.sak.statistikk.vedtak
 
+import no.nav.tilleggsstonader.sak.behandling.BehandlingService
+import no.nav.tilleggsstonader.sak.behandling.barn.BarnService
+import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
+import no.nav.tilleggsstonader.sak.opplysninger.pdl.dto.gradering
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.TilsynBarnVedtakService
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.VilkårService
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Service
-class VedtaksstatistikkService() {
+class VedtaksstatistikkService(
+    private val vedtaksstatistikkRepository: error.NonExistentClass,
+    private val behandlingService: BehandlingService,
+    private val personService: PersonService,
+    private val vedtakService: TilsynBarnVedtakService,
+    private val vilkårService: VilkårService,
+    private val vilkårperiodeService: VilkårperiodeService,
+    private val behandlingBarnService: BarnService,
+
+
+    ) {
     fun lagreVedtaksstatistikk(behandlingId: UUID, fagsakId: UUID, hendelseTidspunkt: LocalDateTime) {
-        TODO()
+
+        val personIdent = behandlingService.hentAktivIdent(behandlingId)
+        val vedtak = vedtakService.hentVedtak(behandlingId)
+
+        val vilkårsperioder = vilkårperiodeService.hentVilkårperioder(behandlingId)
+
+        val vilkårsvurderinger = vilkårService.hentVilkårsett(behandlingId)
+
+
+        vedtaksstatistikkRepository.lagreVedtaksstatistikk(
+            VedtaksstatistikkDvh(
+                fagsakId = fagsakId,
+                behandlingId = behandlingId,
+                eksternBehandlingId = behandlingService.hentEksternBehandlingId(behandlingId).id,
+                relatertBehandlingId = hentRelatertBehandlingId(behandlingId),
+                adressebeskyttelse = hentAdressebeskyttelse(personIdent),
+                tidspunktVedtak = hendelseTidspunkt,
+                målgruppe = MålgruppeDvh.fraDomene(vilkårsperioder.målgrupper),
+                aktivitet = AktivitetDvh.fraDomene(vilkårsperioder.aktiviteter),
+                vilkårsvurderinger = vilkårsvurderinger.map {
+                    VilkårsvurderingDvh.fraDomene(
+                        resultat = it.resultat,
+                        delvilkår = it.delvilkårsett,
+                    )
+                },
+                person =
+            )
+
     }
+
+    private fun hentAdressebeskyttelse(personIdent: String) = AdressebeskyttelseDvh.fraDomene(
+        personService.hentPersonKortBolk(
+            listOf(personIdent)
+        ).values.single().adressebeskyttelse.gradering()
+    )
+
+    private fun hentRelatertBehandlingId(behandlingId: UUID) =
+        behandlingService.hentSaksbehandling(behandlingId).forrigeBehandlingId?.let {
+            behandlingService.hentEksternBehandlingId(it)
+        }?.id
+
 }
