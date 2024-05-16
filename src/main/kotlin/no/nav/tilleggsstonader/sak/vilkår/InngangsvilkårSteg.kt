c234139abe2b80a0c5d814bb5796a92dc5346f86
Johan Blomgren <blommish@users.noreply.github.com>
2024-05-16 13:07:28 +0200

    Skal validere stønadsperioder i inngangsvilkår når man går til neste steg for å unngå at man går til neste steg med ugyldige stønadsperioder. Eks hvis man sletter en målgruppe og trykker neste steg (#315)

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/InngangsvilkårSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/InngangsvilkårSteg.kt
@@ -5,12 +5,18 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.StønadsperiodeService
 import org.springframework.stereotype.Service
 
 @Service
 class InngangsvilkårSteg(
     private val behandlingService: BehandlingService,
+    private val stønadsperiodeService: StønadsperiodeService,
 ) : BehandlingSteg<Void?> {
+
+    override fun validerSteg(saksbehandling: Saksbehandling) {
+        stønadsperiodeService.validerStønadsperioder(saksbehandling.id)
+    }
 
     override fun utførSteg(saksbehandling: Saksbehandling, data: Void?) {
         if (saksbehandling.status != BehandlingStatus.UTREDES) {
