7a6fc15f0fda2d75d94b0ede1a1c4502ace6b4bd
sarahjelle <sara.hjelle@bekk.no>
2024-05-22 15:36:03 +0200

    Flytt funksjon for å opprette innvilget vedtak til egen fil

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/TilsynBarnTestUtil.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/TilsynBarnTestUtil.kt
@@ -1,8 +1,16 @@
 package no.nav.tilleggsstonader.sak.vedtak.barnetilsyn
 
+import no.nav.tilleggsstonader.libs.utils.osloDateNow
+import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Beløpsperiode
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Beregningsgrunnlag
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Beregningsresultat
 import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.BeregningsresultatTilsynBarnDto
 import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.InnvilgelseTilsynBarnDto
 import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Utgift
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
+import java.math.BigDecimal
+import java.time.YearMonth
 import java.util.UUID
 
 object TilsynBarnTestUtil {
@@ -12,6 +20,46 @@ object TilsynBarnTestUtil {
         beregningsresultat: BeregningsresultatTilsynBarnDto? = null,
     ) = InnvilgelseTilsynBarnDto(
         utgifter = utgifter,
+        beregningsresultat = beregningsresultat,
+    )
+
+    val vedtakBeregningsresultat = VedtaksdataBeregningsresultat(
+        perioder = listOf(
+            Beregningsresultat(
+                dagsats = BigDecimal.TEN,
+                månedsbeløp = 1000,
+                grunnlag = Beregningsgrunnlag(
+                    måned = YearMonth.now(),
+                    makssats = 1000,
+                    stønadsperioderGrunnlag = emptyList(),
+                    utgifter = emptyList(),
+                    utgifterTotal = 2000,
+                    antallBarn = 1,
+                ),
+                beløpsperioder = listOf(
+                    Beløpsperiode(dato = osloDateNow(), beløp = 1000, målgruppe = MålgruppeType.AAP),
+                    Beløpsperiode(dato = osloDateNow().plusMonths(1), beløp = 2000, målgruppe = MålgruppeType.AAP),
+                ),
+            ),
+        ),
+    )
+
+    val vedtaksdata = VedtaksdataTilsynBarn(
+        utgifter = mapOf(
+            barn(
+                UUID.randomUUID(),
+                Utgift(YearMonth.of(2023, 1), YearMonth.of(2023, 1), 100),
+            ),
+        ),
+    )
+
+    fun innvilgetVedtak(
+        vedtak: VedtaksdataTilsynBarn? = vedtaksdata,
+        beregningsresultat: VedtaksdataBeregningsresultat? = vedtakBeregningsresultat,
+    ) = VedtakTilsynBarn(
+        behandlingId = UUID.randomUUID(),
+        type = TypeVedtak.INNVILGELSE,
+        vedtak = vedtak,
         beregningsresultat = beregningsresultat,
     )
 
--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/TilsynBarnVedtakServiceTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/TilsynBarnVedtakServiceTest.kt
@@ -1,19 +1,12 @@
 package no.nav.tilleggsstonader.sak.vedtak.barnetilsyn
 
 import io.mockk.mockk
-import no.nav.tilleggsstonader.libs.utils.osloDateNow
 import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.TilsynBarnTestUtil.innvilgetVedtak
 import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.AvslagTilsynBarnDto
-import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Beløpsperiode
-import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Beregningsgrunnlag
-import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Beregningsresultat
 import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.InnvilgelseTilsynBarnDto
-import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.dto.Utgift
-import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
-import java.time.YearMonth
 import java.util.*
 
 class TilsynBarnVedtakServiceTest {
@@ -22,7 +15,7 @@ class TilsynBarnVedtakServiceTest {
 
     @Test
     fun `skal mappe innvilget vedtak til dto`() {
-        val vedtak = vedtak()
+        val vedtak = innvilgetVedtak()
 
         val dto = tilsynBarnVedtakService.mapTilDto(vedtak) as InnvilgelseTilsynBarnDto
 
@@ -44,37 +37,4 @@ class TilsynBarnVedtakServiceTest {
         assertThat(dto.begrunnelse).isEqualTo(vedtak.avslagBegrunnelse)
         assertThat(dto.type).isEqualTo(vedtak.type)
     }
-
-    private fun vedtak() = VedtakTilsynBarn(
-        behandlingId = UUID.randomUUID(),
-        type = TypeVedtak.INNVILGELSE,
-        vedtak = VedtaksdataTilsynBarn(
-            utgifter = mapOf(
-                TilsynBarnTestUtil.barn(
-                    UUID.randomUUID(),
-                    Utgift(YearMonth.of(2023, 1), YearMonth.of(2023, 1), 100),
-                ),
-            ),
-        ),
-        beregningsresultat = VedtaksdataBeregningsresultat(
-            perioder = listOf(
-                Beregningsresultat(
-                    dagsats = BigDecimal.TEN,
-                    månedsbeløp = 1000,
-                    grunnlag = Beregningsgrunnlag(
-                        måned = YearMonth.now(),
-                        makssats = 1000,
-                        stønadsperioderGrunnlag = emptyList(),
-                        utgifter = emptyList(),
-                        utgifterTotal = 2000,
-                        antallBarn = 1,
-                    ),
-                    beløpsperioder = listOf(
-                        Beløpsperiode(dato = osloDateNow(), beløp = 1000, målgruppe = MålgruppeType.AAP),
-                        Beløpsperiode(dato = osloDateNow().plusMonths(1), beløp = 2000, målgruppe = MålgruppeType.AAP),
-                    ),
-                ),
-            ),
-        ),
-    )
 }
