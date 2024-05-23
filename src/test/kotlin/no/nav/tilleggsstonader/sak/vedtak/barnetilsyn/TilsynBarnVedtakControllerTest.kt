363cb5c3ade739b22792c7095f063c803fb502c9
sarahjelle <sara.hjelle@bekk.no>
2024-05-23 13:49:18 +0200

    Test skal sjekke om årsakene lagres ned og hentes opp igjen

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/TilsynBarnVedtakControllerTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/TilsynBarnVedtakControllerTest.kt
@@ -102,7 +102,8 @@ class TilsynBarnVedtakControllerTest(
 
         val lagretDto = hentVedtak(behandling.id).body!!
 
-        assertThat((lagretDto as AvslagTilsynBarnDto).begrunnelse).isEqualTo(vedtak.begrunnelse)
+        assertThat((lagretDto as AvslagTilsynBarnDto).årsakerAvslag).isEqualTo(vedtak.årsakerAvslag)
+        assertThat(lagretDto.begrunnelse).isEqualTo(vedtak.begrunnelse)
         assertThat(lagretDto.type).isEqualTo(TypeVedtak.AVSLAG)
     }
 
