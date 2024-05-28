c2ba25f34b7aaa856a3e967b95d5a330cd4a8beb
Eirik Emil Neesgaard Årseth <eirik.arseth@nav.no>
2024-05-28 09:57:22 +0200

    Json skal ikke kunne være null
    Co-authored-by: Johan Blomgren <blommish@users.noreply.github.com>

--- src/main/resources/db/migration/V40__vilkarperioder_grunnlag.sql
+++ src/main/resources/db/migration/V40__vilkarperioder_grunnlag.sql
@@ -1,7 +1,7 @@
 CREATE TABLE vilkarperioder_grunnlag
 (
     behandling_id UUID PRIMARY KEY REFERENCES behandling (id),
-    grunnlag      JSON,
+    grunnlag      JSON         NOT NULL,
     opprettet_av  VARCHAR      NOT NULL,
     opprettet_tid TIMESTAMP(3) NOT NULL,
     endret_av     VARCHAR      NOT NULL,
