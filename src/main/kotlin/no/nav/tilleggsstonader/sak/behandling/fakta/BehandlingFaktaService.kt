4b76fc0ccc0c8ac4fd39ddf00e6cb38999388cb3
Johan Blomgren <blommish@users.noreply.github.com>
2024-06-04 19:40:20 +0200

    Legger til fødselsdato på barn for å kunne utlede alder på barn vid tidspunkt for første stønadsperioden i Vilkårsoppsummering (#340)

--- src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/fakta/BehandlingFaktaService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/fakta/BehandlingFaktaService.kt
@@ -84,6 +84,7 @@ class BehandlingFaktaService(
                 barnId = behandlingBarn.id,
                 registergrunnlag = RegistergrunnlagBarn(
                     navn = barnGrunnlagsdata.navn.visningsnavn(),
+                    fødselsdato = barnGrunnlagsdata.fødselsdato,
                     alder = barnGrunnlagsdata.alder,
                     dødsdato = barnGrunnlagsdata.dødsdato,
                 ),
--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårsoppsummering/VilkårsoppsummeringService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårsoppsummering/VilkårsoppsummeringService.kt
@@ -3,10 +3,12 @@ package no.nav.tilleggsstonader.sak.vilkår.vilkårsoppsummering
 import no.nav.tilleggsstonader.sak.behandling.fakta.BehandlingFaktaService
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
 import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.StønadsperiodeService
+import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.dto.StønadsperiodeDto
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.VilkårService
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkårsresultat
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatVilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårsoppsummering.VilkårsoppsummeringUtil.utledAlderNårStønadsperiodeBegynner
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -21,33 +23,38 @@ class VilkårsoppsummeringService(
     fun hentVilkårsoppsummering(behandlingId: UUID): VilkårsoppsummeringDto {
         val vilkårperioder = vilkårperiodeService.hentVilkårperioder(behandlingId)
 
+        val stønadsperioder = stønadsperiodeService.hentStønadsperioder(behandlingId)
+
         return VilkårsoppsummeringDto(
             aktivitet = vilkårperioder.aktiviteter.any { it.resultat == ResultatVilkårperiode.OPPFYLT },
             målgruppe = vilkårperioder.målgrupper.any { it.resultat == ResultatVilkårperiode.OPPFYLT },
-            stønadsperiode = oppsummerStønadsperioder(behandlingId),
-            passBarn = oppsummeringPassBarnVilkår(behandlingId),
+            stønadsperiode = stønadsperioder.isNotEmpty(),
+            passBarn = oppsummeringPassBarnVilkår(behandlingId, stønadsperioder),
         )
     }
 
-    private fun oppsummerStønadsperioder(behandlingId: UUID): Boolean {
-        val stønadsperioder = stønadsperiodeService.hentStønadsperioder(behandlingId)
-        return stønadsperioder.isNotEmpty()
-    }
-
-    private fun oppsummeringPassBarnVilkår(behandlingId: UUID): List<BarnOppsummering> {
+    private fun oppsummeringPassBarnVilkår(
+        behandlingId: UUID,
+        stønadsperioder: List<StønadsperiodeDto>,
+    ): List<BarnOppsummering> {
         val passBarnVilkår = vilkårService.hentPassBarnVilkår(behandlingId)
         val fakta = behandlingFaktaService.hentFakta(behandlingId)
+
+        val datoFørsteStønadsperidoe = stønadsperioder.minOfOrNull { it.fom }
 
         return passBarnVilkår.map { vilkår ->
             val barn = fakta.barn.find { it.barnId == vilkår.barnId }
 
             feilHvis(barn == null) { "Fant ikke barn med id ${vilkår.barnId}" }
 
+            val alderNårStønadsperiodeBegynner =
+                utledAlderNårStønadsperiodeBegynner(barn.registergrunnlag.fødselsdato, datoFørsteStønadsperidoe)
             BarnOppsummering(
                 barnId = barn.barnId,
                 ident = barn.ident,
                 navn = barn.registergrunnlag.navn,
                 alder = barn.registergrunnlag.alder,
+                alderNårStønadsperiodeBegynner = alderNårStønadsperiodeBegynner,
                 oppfyllerAlleVilkår = vilkår.resultat == Vilkårsresultat.OPPFYLT,
             )
         }
