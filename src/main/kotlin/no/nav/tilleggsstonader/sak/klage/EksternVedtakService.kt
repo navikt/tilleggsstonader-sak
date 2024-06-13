6c92237f61862fa7ae0aac63b4de8bba2714c46a
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2024-06-13 13:23:46 +0200

    Legge til fagsystemtype og EØS/Nasjonal for sakstype i ekternt vedtak for klage

--- src/main/kotlin/no/nav/tilleggsstonader/sak/klage/EksternVedtakService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/klage/EksternVedtakService.kt
@@ -1,8 +1,11 @@
 package no.nav.tilleggsstonader.sak.klage
 
+import no.nav.tilleggsstonader.kontrakter.klage.FagsystemType
 import no.nav.tilleggsstonader.kontrakter.klage.FagsystemVedtak
+import no.nav.tilleggsstonader.kontrakter.klage.Regelverk
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandling.domain.Behandling
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingKategori
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
 import no.nav.tilleggsstonader.sak.fagsak.FagsakService
 import no.nav.tilleggsstonader.sak.fagsak.domain.Fagsak
@@ -25,11 +28,25 @@ class EksternVedtakService(
             .map { tilFagsystemVedtak(it) }
     }
 
-    private fun tilFagsystemVedtak(behandling: Behandling) = FagsystemVedtak(
-        eksternBehandlingId = behandlingService.hentEksternBehandlingId(behandling.id).id.toString(),
-        behandlingstype = behandling.type.visningsnavn,
-        resultat = behandling.resultat.displayName,
-        vedtakstidspunkt = behandling.vedtakstidspunkt
-            ?: error("Mangler vedtakstidspunkt for behandling=${behandling.id}"),
-    )
+    private fun tilFagsystemVedtak(behandling: Behandling): FagsystemVedtak {
+        val (resultat, fagsystemType) = utledReultatOgFagsystemType(behandling)
+
+        return FagsystemVedtak(
+            eksternBehandlingId = behandlingService.hentEksternBehandlingId(behandling.id).id.toString(),
+            behandlingstype = behandling.type.visningsnavn,
+            resultat = resultat,
+            vedtakstidspunkt = behandling.vedtakstidspunkt
+                ?: error("Mangler vedtakstidspunkt for behandling=${behandling.id}"),
+            fagsystemType = fagsystemType,
+            regelverk = mapTilRegelverk(behandling.kategori),
+        )
+    }
+    private fun utledReultatOgFagsystemType(behandling: Behandling): Pair<String, FagsystemType> {
+        // Når tilbakekrevning blir implementert så trenger denne å utvides med FagsystemType.TILBAKEKREVING
+        return Pair(behandling.resultat.displayName, FagsystemType.ORDNIÆR)
+    }
+    private fun mapTilRegelverk(kategori: BehandlingKategori) = when (kategori) {
+        BehandlingKategori.EØS -> Regelverk.EØS
+        BehandlingKategori.NASJONAL -> Regelverk.NASJONAL
+    }
 }
