0c5917f79d8401d18fc9f76b4933bc1ab1d07768
Johan Blomgren <blommish@users.noreply.github.com>
2024-06-13 18:11:27 +0200

    NAV-21535 Henter kun aktiviteter av typen TILTAK Andre typer er ikke aktuelle og man har kanskje ikke riktig tilgang for å få sett på de

--- src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/aktivitet/AktivitetService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/aktivitet/AktivitetService.kt
@@ -1,8 +1,12 @@
 package no.nav.tilleggsstonader.sak.opplysninger.aktivitet
 
 import no.nav.tilleggsstonader.kontrakter.aktivitet.AktivitetArenaDto
+import no.nav.tilleggsstonader.kontrakter.aktivitet.GruppeAktivitet
+import no.nav.tilleggsstonader.kontrakter.aktivitet.TypeAktivitet
+import no.nav.tilleggsstonader.libs.log.SecureLogger.secureLogger
 import no.nav.tilleggsstonader.libs.utils.osloDateNow
 import no.nav.tilleggsstonader.sak.fagsak.domain.FagsakPersonService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.util.UUID
@@ -12,13 +16,26 @@ class AktivitetService(
     private val fagsakPersonService: FagsakPersonService,
     private val aktivitetClient: AktivitetClient,
 ) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     fun hentAktiviteter(fagsakPersonId: UUID): List<AktivitetArenaDto> {
         val ident = fagsakPersonService.hentAktivIdent(fagsakPersonId)
         return aktivitetClient.hentAktiviteter(
             ident = ident,
             fom = osloDateNow().minusYears(3),
             tom = osloDateNow().plusYears(1),
-        ).sortedByDescending { it.fom }
+        )
+            .filter {
+                try {
+                    // Det er alltid gruppe=TILTAK når erStønadsberettiget = true (men alle av tiltak er ikke stønadsberettiget)
+                    it.erStønadsberettiget == true || TypeAktivitet.valueOf(it.type).gruppe == GruppeAktivitet.TLTAK
+                } catch (e: Exception) {
+                    logger.error("TypeAktivitet mangler mapping, se secure logs for detaljer.")
+                    secureLogger.error("TypeAktivitet=${it.type} mangler mapping. Vennligst oppdater TypeAktivitet med ny type.")
+                    false
+                }
+            }
+            .sortedByDescending { it.fom }
     }
 
     fun hentAktiviteterForGrunnlagsdata(ident: String, fom: LocalDate, tom: LocalDate): List<AktivitetArenaDto> {
@@ -26,6 +43,9 @@ class AktivitetService(
             ident = ident,
             fom = fom,
             tom = tom,
-        ).sortedByDescending { it.fom }
+        )
+            // Det er alltid gruppe=TILTAK når erStønadsberettiget = true (men alle av tiltak er ikke stønadsberettiget)
+            .filter { it.erStønadsberettiget == true }
+            .sortedByDescending { it.fom }
     }
 }
