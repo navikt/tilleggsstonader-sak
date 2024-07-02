5f41bcb5acd8eb583cce499ca7f230a3a542a558
eirikvilpponen <eirik.vilpponen@nav.no>
2024-07-02 15:06:40 +0200

    refaktorerer EregClient til å ligne mer på ArbeidsfordelingClient

--- src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/ereg/EregClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/ereg/EregClient.kt
@@ -1,29 +1,37 @@
 package no.nav.tilleggsstonader.sak.opplysninger.ereg
 
+import java.net.URI
 import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
-import java.net.URI
 
-@Service
+@Component
 class EregClient(
     @Value("\${clients.ereg.uri}")
-    private val eregUrl: URI,
-    @Qualifier("azureClientCredential") restTemplate: RestTemplate,
-) : AbstractRestClient(restTemplate) {
+    private val uri: URI,
+    @Qualifier("utenAuth") restTemplate: RestTemplate,
+) :
+    AbstractRestClient(restTemplate) {
 
     fun hentOrganisasjoner(organisasjonsnumre: List<String>): OrganisasjonsNavnDto? {
-        val uriBuilder = UriComponentsBuilder.fromUri(eregUrl)
-            .pathSegment("api/v1/organisasjon")
+        val uriBuilder = UriComponentsBuilder.fromUri(eregUri)
             .pathSegment(organisasjonsnumre.firstOrNull())
+            .build()
+            .toUriString()
+
         return try {
-            getForEntity<OrganisasjonsNavnDto>(uriBuilder.build().toUri().toString())
+            getForEntity<OrganisasjonsNavnDto>(uriBuilder)
         } catch (e: HttpClientErrorException.NotFound) {
             return null
         }
     }
+
+    private val eregUri = UriComponentsBuilder.fromUri(uri)
+        .pathSegment("api/v1/organisasjon")
+        .build()
+        .toUri()
 }
