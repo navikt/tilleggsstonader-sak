776f9b83ca481264b98c62caae68cf995c867b0e
Johan Blomgren <blommish@users.noreply.github.com>
2024-08-19 08:33:07 +0200

    Endepunkt for å kunne oppgaver fra Arena

--- src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/arena/ArenaService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/arena/ArenaService.kt
@@ -1,18 +1,22 @@
 package no.nav.tilleggsstonader.sak.opplysninger.arena
 
 import no.nav.tilleggsstonader.kontrakter.arena.ArenaStatusDto
+import no.nav.tilleggsstonader.kontrakter.arena.oppgave.ArenaOppgaveDto
 import no.nav.tilleggsstonader.kontrakter.felles.IdenterRequest
 import no.nav.tilleggsstonader.kontrakter.felles.IdenterStønadstype
 import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.sak.fagsak.domain.FagsakPersonService
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.dto.identer
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class ArenaService(
     private val arenaClient: ArenaClient,
     private val personService: PersonService,
+    private val fagsakPersonService: FagsakPersonService,
 ) {
 
     @Cacheable("arena-status-ident")
@@ -24,5 +28,12 @@ class ArenaService(
     fun harSaker(ident: String): Boolean {
         val identer = personService.hentPersonIdenter(ident).identer()
         return arenaClient.harSaker(IdenterRequest(identer)).harSaker
+    }
+
+    @Cacheable("arena-oppgaver", cacheManager = "5secCache")
+    fun hentOppgaver(fagsakPersonId: UUID): List<ArenaOppgaveDto> {
+        val aktivIdent = fagsakPersonService.hentAktivIdent(fagsakPersonId)
+        val identer = personService.hentPersonIdenter(aktivIdent).identer()
+        return arenaClient.hentOppgaver(IdenterRequest(identer))
     }
 }
--- src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/mocks/ArenaClientConfig.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/mocks/ArenaClientConfig.kt
@@ -7,12 +7,15 @@ import no.nav.tilleggsstonader.kontrakter.arena.ArenaStatusDto
 import no.nav.tilleggsstonader.kontrakter.arena.ArenaStatusHarSakerDto
 import no.nav.tilleggsstonader.kontrakter.arena.SakStatus
 import no.nav.tilleggsstonader.kontrakter.arena.VedtakStatus
+import no.nav.tilleggsstonader.kontrakter.arena.oppgave.ArenaOppgaveDto
+import no.nav.tilleggsstonader.kontrakter.arena.sak.Målgruppe
 import no.nav.tilleggsstonader.sak.opplysninger.arena.ArenaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Configuration
 @Profile("mock-arena")
@@ -39,6 +42,26 @@ class ArenaClientConfig {
                 ),
             )
             every { client.harSaker(any()) } returns ArenaStatusHarSakerDto(true)
+            every { client.hentOppgaver(any()) } returns listOf(
+                ArenaOppgaveDto(
+                    tittel = "Kontroller/registrer saksopplysninger - automatisk journalført",
+                    kommentar = "En kommentar\\n\\n med radbryte",
+                    fristFerdigstillelse = LocalDate.now(),
+                    benk = "Inn",
+                    tildelt = null,
+                    opprettetTidspunkt = LocalDateTime.now(),
+                    målgruppe = Målgruppe.NEDSATT__ARBEIDSEVNE,
+                ),
+                ArenaOppgaveDto(
+                    tittel = "Vurder dokument",
+                    kommentar = "En kommentar",
+                    fristFerdigstillelse = LocalDate.now(),
+                    benk = null,
+                    tildelt = "ABC1234",
+                    opprettetTidspunkt = LocalDateTime.now(),
+                    målgruppe = Målgruppe.NEDSATT__ARBEIDSEVNE,
+                ),
+            )
         }
     }
 }
