157ab6fb8dc9361c4ce372ccb1926b91697ca052
Eirik Årseth <eirik.arseth@nav.no>
2024-08-22 11:03:39 +0200

    Må hente vedtaksresultat, istedenfor å bruke det som ligger i behandlingsobkjektet fordi resultatet på behandlingen først settes ved ferdigstillelse

--- src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/simulering/SimuleringSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/simulering/SimuleringSteg.kt
@@ -1,37 +1,39 @@
 package no.nav.tilleggsstonader.sak.utbetaling.simulering
 
-import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksresultatService
 import org.springframework.stereotype.Service
 
 @Service
 class SimuleringSteg(
     val simuleringService: SimuleringService,
+    val vedtaksresultatService: VedtaksresultatService,
 ) : BehandlingSteg<Void?> {
 
     override fun utførSteg(saksbehandling: Saksbehandling, data: Void?) {
-        if (skalUtFøreSimulering(saksbehandling)) {
+        val resultat = vedtaksresultatService.hentVedtaksresultat(saksbehandling)
+
+        if (skalUtFøreSimulering(saksbehandling, resultat)) {
             simuleringService.hentOgLagreSimuleringsresultat(saksbehandling)
         }
     }
 
-    private fun skalUtFøreSimulering(saksbehandling: Saksbehandling): Boolean =
+    private fun skalUtFøreSimulering(saksbehandling: Saksbehandling, vedtak: TypeVedtak): Boolean =
         when (saksbehandling.type) {
-            BehandlingType.REVURDERING -> resultatSkalSimuleres(saksbehandling.resultat)
+            BehandlingType.REVURDERING -> resultatSkalSimuleres(vedtak)
             BehandlingType.FØRSTEGANGSBEHANDLING -> false
             else -> error("Behandlingstype ${saksbehandling.type} støttes ikke i simulering")
         }
 
-    private fun resultatSkalSimuleres(resultat: BehandlingResultat): Boolean {
-        return when (resultat) {
-            // TODO: Bruke resultat.skalIverksettes ?
-            BehandlingResultat.INNVILGET -> true
-            BehandlingResultat.OPPHØRT -> true
-            BehandlingResultat.AVSLÅTT -> false
-            else -> error("Simulering støtter ikke resultat $resultat")
+    private fun resultatSkalSimuleres(type: TypeVedtak): Boolean {
+        return when (type) {
+            TypeVedtak.INNVILGELSE -> true
+            TypeVedtak.AVSLAG -> false
+            else -> error("Simulering støtter ikke resultat $type")
         }
     }
 
--- src/test/kotlin/no/nav/tilleggsstonader/sak/utbetaling/simulering/SimuleringStegTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/utbetaling/simulering/SimuleringStegTest.kt
@@ -3,9 +3,10 @@ package no.nav.tilleggsstonader.sak.utbetaling.simulering
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
 import no.nav.tilleggsstonader.sak.util.saksbehandling
+import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksresultatService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -13,13 +14,17 @@ import org.junit.jupiter.api.Test
 class SimuleringStegTest {
 
     val simuleringService = mockk<SimuleringService>()
+    val vedtaksresultatService = mockk<VedtaksresultatService>()
 
-    val simuleringSteg = SimuleringSteg(simuleringService)
+    val simuleringSteg = SimuleringSteg(simuleringService, vedtaksresultatService)
 
     @BeforeEach
     fun setUp() {
         every { simuleringService.hentOgLagreSimuleringsresultat(any()) } returns mockk()
     }
+
+    private fun mockVedtakMedType(type: TypeVedtak) =
+        every { vedtaksresultatService.hentVedtaksresultat(any()) } returns type
 
     @Nested
     inner class Revurdering {
@@ -28,20 +33,8 @@ class SimuleringStegTest {
         fun `skal utføre simulering for innvilget revurdering`() {
             val saksbehandling = saksbehandling(
                 type = BehandlingType.REVURDERING,
-                resultat = BehandlingResultat.INNVILGET,
             )
-
-            simuleringSteg.utførSteg(saksbehandling, null)
-
-            verify { simuleringService.hentOgLagreSimuleringsresultat(saksbehandling) }
-        }
-
-        @Test
-        fun `skal utføre simulering for opphørt revurdering`() {
-            val saksbehandling = saksbehandling(
-                type = BehandlingType.REVURDERING,
-                resultat = BehandlingResultat.OPPHØRT,
-            )
+            mockVedtakMedType(TypeVedtak.INNVILGELSE)
 
             simuleringSteg.utførSteg(saksbehandling, null)
 
@@ -52,8 +45,8 @@ class SimuleringStegTest {
         fun `skal ikke utføre simulering for avslått revurdering`() {
             val saksbehandling = saksbehandling(
                 type = BehandlingType.REVURDERING,
-                resultat = BehandlingResultat.AVSLÅTT,
             )
+            mockVedtakMedType(TypeVedtak.AVSLAG)
 
             simuleringSteg.utførSteg(saksbehandling, null)
 
@@ -68,8 +61,8 @@ class SimuleringStegTest {
         fun `skal ikke utføre simulering for førstegangsbehandling`() {
             val saksbehandling = saksbehandling(
                 type = BehandlingType.FØRSTEGANGSBEHANDLING,
-                resultat = BehandlingResultat.INNVILGET,
             )
+            mockVedtakMedType(TypeVedtak.INNVILGELSE)
 
             simuleringSteg.utførSteg(saksbehandling, null)
 
