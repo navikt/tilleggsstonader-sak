75c3214dc0841f4839d2426cb114a25759375420
Eirik Årseth <eirik.arseth@nav.no>
2024-08-22 11:00:17 +0200

    Må opprette vedtak for å teste simulering

--- src/test/kotlin/no/nav/tilleggsstonader/sak/utbetaling/simulering/SimuleringControllerTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/utbetaling/simulering/SimuleringControllerTest.kt
@@ -6,12 +6,15 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
 import no.nav.tilleggsstonader.sak.behandling.dto.BehandlingDto
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.fagsak.domain.Fagsak
 import no.nav.tilleggsstonader.sak.fagsak.domain.PersonIdent
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.TilkjentYtelseUtil.tilkjentYtelse
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TilkjentYtelseRepository
 import no.nav.tilleggsstonader.sak.util.behandling
 import no.nav.tilleggsstonader.sak.util.fagsak
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.TilsynBarnTestUtil.innvilgetVedtak
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.TilsynBarnVedtakRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -31,6 +34,9 @@ internal class SimuleringControllerTest : IntegrationTest() {
     @Autowired
     private lateinit var simuleringsresultatRepository: SimuleringsresultatRepository
 
+    @Autowired
+    private lateinit var tilsynBarnVedtakRepository: TilsynBarnVedtakRepository
+
     @BeforeEach
     fun setUp() {
         headers.setBearerAuth(onBehalfOfToken())
@@ -39,16 +45,10 @@ internal class SimuleringControllerTest : IntegrationTest() {
     @Test
     internal fun `Skal returnere 200 OK for simulering av behandling`() {
         val personIdent = "12345678901"
-        val fagsak = testoppsettService.lagreFagsak(fagsak(identer = setOf(PersonIdent(personIdent))))
-        val behandling = testoppsettService.lagre(
-            behandling(
-                fagsak,
-                type = BehandlingType.REVURDERING,
-                resultat = BehandlingResultat.INNVILGET,
-                status = BehandlingStatus.UTREDES,
-                steg = StegType.SIMULERING,
-            ),
-        )
+        val fagsak = opprettFagsak(personIdent)
+        val behandling = opprettBehandling(fagsak)
+        opprettVedtak(behandling.id)
+
         tilkjentYtelseRepository.insert(tilkjentYtelse(behandlingId = behandling.id))
 
         val respons: ResponseEntity<SimuleringDto> = simulerForBehandling(behandling.id)
@@ -61,11 +61,30 @@ internal class SimuleringControllerTest : IntegrationTest() {
         assertThat(simuleringsresultat.data.detaljer.perioder).hasSize(16)
     }
 
+    private fun opprettFagsak(personIdent: String) =
+        testoppsettService.lagreFagsak(fagsak(identer = setOf(PersonIdent(personIdent))))
+
+    private fun opprettBehandling(fagsak: Fagsak) = testoppsettService.lagre(
+        behandling(
+            fagsak,
+            type = BehandlingType.REVURDERING,
+            resultat = BehandlingResultat.INNVILGET,
+            status = BehandlingStatus.UTREDES,
+            steg = StegType.SIMULERING,
+        ),
+    )
+
     private fun simulerForBehandling(behandlingId: UUID): ResponseEntity<SimuleringDto> {
         return restTemplate.exchange(
             localhost("/api/simulering/$behandlingId"),
             HttpMethod.GET,
             HttpEntity<BehandlingDto>(headers),
+        )
+    }
+
+    private fun opprettVedtak(behandlingId: UUID) {
+        tilsynBarnVedtakRepository.insert(
+            innvilgetVedtak(behandlingId = behandlingId),
         )
     }
 }
