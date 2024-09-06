5fd3f13d6b2367e45811359d00822f12749b9995
Johan Blomgren <blommish@users.noreply.github.com>
2024-09-06 13:44:55 +0200

    Validerer at vilkår har periodefelter når man går videre til neste steg - I tilfelle man fått et avslag og oppretter en ny behandling så kopieres vilkåren fra forrige behandling og då mangler periodefelter som må utfylles

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/VilkårSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/VilkårSteg.kt
@@ -1,12 +1,16 @@
 package no.nav.tilleggsstonader.sak.vilkår
 
+import no.nav.tilleggsstonader.libs.unleash.UnleashService
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
+import no.nav.tilleggsstonader.sak.infrastruktur.unleash.Toggle
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.VilkårService
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkårsresultat
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.evalutation.VilkårPeriodeValidering.validerIkkeOverlappendeVilkår
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.evalutation.VilkårsresultatUtil
 import org.springframework.stereotype.Service
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service
 class VilkårSteg(
     private val behandlingService: BehandlingService,
     private val vilkårService: VilkårService,
+    private val unleashService: UnleashService,
 ) : BehandlingSteg<Void?> {
     override fun utførSteg(saksbehandling: Saksbehandling, data: Void?) {
         if (saksbehandling.status != BehandlingStatus.UTREDES) {
@@ -30,6 +35,16 @@ class VilkårSteg(
             "Alle vilkår må være tatt stilling til"
         }
         validerIkkeOverlappendeVilkår(vilkår)
+
+        if (unleashService.isEnabled(Toggle.VILKÅR_PERIODISERING)) {
+            val manglerVerdierPåVilkårSomErTattStillingTil =
+                vilkår.filter { it.resultat == Vilkårsresultat.OPPFYLT || it.resultat == Vilkårsresultat.IKKE_OPPFYLT }
+                    .any { it.fom == null || it.tom == null || it.utgift == null }
+            brukerfeilHvis(manglerVerdierPåVilkårSomErTattStillingTil) {
+                "Mangler fom, tom eller utgift på et eller flere vilkår. " +
+                    "Vennligst ta stilling til hvilken periode vilkåret gjelder for"
+            }
+        }
     }
 
     override fun stegType(): StegType = StegType.VILKÅR
