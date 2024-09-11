73474c20db4c2b54b1ff2cd183b15d924e264afd
Johan Blomgren <blommish@users.noreply.github.com>
2024-09-11 08:33:26 +0200

    Fjerner toggle for vilkårperioder - skal alltid validere at fom tom og utgifter er tatt stilling til

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/VilkårSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/VilkårSteg.kt
@@ -1,6 +1,5 @@
 package no.nav.tilleggsstonader.sak.vilkår
 
-import no.nav.tilleggsstonader.libs.unleash.UnleashService
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
@@ -8,7 +7,6 @@ import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
-import no.nav.tilleggsstonader.sak.infrastruktur.unleash.Toggle
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.VilkårService
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkårsresultat
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.evalutation.VilkårPeriodeValidering.validerIkkeOverlappendeVilkår
@@ -19,7 +17,6 @@ import org.springframework.stereotype.Service
 class VilkårSteg(
     private val behandlingService: BehandlingService,
     private val vilkårService: VilkårService,
-    private val unleashService: UnleashService,
 ) : BehandlingSteg<Void?> {
     override fun utførSteg(saksbehandling: Saksbehandling, data: Void?) {
         if (saksbehandling.status != BehandlingStatus.UTREDES) {
@@ -36,21 +33,19 @@ class VilkårSteg(
         }
         validerIkkeOverlappendeVilkår(vilkår)
 
-        if (unleashService.isEnabled(Toggle.VILKÅR_PERIODISERING)) {
-            val manglerVerdierPåOppfylteVilkår =
-                vilkår.filter { it.resultat == Vilkårsresultat.OPPFYLT }
-                    .any { it.fom == null || it.tom == null || it.utgift == null }
-            brukerfeilHvis(manglerVerdierPåOppfylteVilkår) {
-                "Mangler fom, tom eller utgift på et eller flere vilkår. " +
+        val manglerVerdierPåOppfylteVilkår =
+            vilkår.filter { it.resultat == Vilkårsresultat.OPPFYLT }
+                .any { it.fom == null || it.tom == null || it.utgift == null }
+        brukerfeilHvis(manglerVerdierPåOppfylteVilkår) {
+            "Mangler fom, tom eller utgift på et eller flere vilkår. " +
                     "Vennligst ta stilling til hvilken periode vilkåret gjelder for."
-            }
-            val manglerVerdierPåIkkeOppfylteVilkår =
-                vilkår.filter { it.resultat == Vilkårsresultat.IKKE_OPPFYLT }
-                    .any { it.fom == null || it.tom == null }
-            brukerfeilHvis(manglerVerdierPåIkkeOppfylteVilkår) {
-                "Mangler fom eller tom på et eller flere vilkår. " +
+        }
+        val manglerVerdierPåIkkeOppfylteVilkår =
+            vilkår.filter { it.resultat == Vilkårsresultat.IKKE_OPPFYLT }
+                .any { it.fom == null || it.tom == null }
+        brukerfeilHvis(manglerVerdierPåIkkeOppfylteVilkår) {
+            "Mangler fom eller tom på et eller flere vilkår. " +
                     "Vennligst ta stilling til hvilken periode vilkåret gjelder for."
-            }
         }
     }
 
