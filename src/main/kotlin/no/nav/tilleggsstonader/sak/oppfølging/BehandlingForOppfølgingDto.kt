7c4323068d436acbf3d88d8aa5409f218f2b9944
Eirik Årseth <eirik.arseth@nav.no>
2024-09-13 11:34:54 +0200

    Rename. behandlingDto -> behandling

--- src/main/kotlin/no/nav/tilleggsstonader/sak/oppfølging/BehandlingForOppfølgingDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/oppfølging/BehandlingForOppfølgingDto.kt
@@ -3,4 +3,4 @@ package no.nav.tilleggsstonader.sak.oppfølging
 import no.nav.tilleggsstonader.sak.behandling.dto.BehandlingDto
 import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.dto.StønadsperiodeDto
 
-data class BehandlingForOppfølgingDto(val behandlingDto: BehandlingDto, val stønadsperioderForKontroll: List<StønadsperiodeDto>)
+data class BehandlingForOppfølgingDto(val behandling: BehandlingDto, val stønadsperioderForKontroll: List<StønadsperiodeDto>)
