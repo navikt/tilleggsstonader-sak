6ce4a40d713f8c3ac47c46b41f11f8d0edd7fc3b
Johan Blomgren <blommish@users.noreply.github.com>
2024-09-16 07:52:49 +0200

    Kopierer stønadsperioder og vilkår fra tidligere behandling i de behandlinger som ikke fikk kopert data fra forrige behandling. Skjedde før det var på plass

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/KopierDataFraTidligereBehandlingController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/KopierDataFraTidligereBehandlingController.kt
@@ -2,21 +2,24 @@ package no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
-import no.nav.tilleggsstonader.kontrakter.felles.overlapper
 import no.nav.tilleggsstonader.sak.behandling.domain.Behandling
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingRepository
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
+import no.nav.tilleggsstonader.sak.infrastruktur.database.Sporbar
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
-import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
 import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.StønadsperiodeService
+import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.domain.StønadsperiodeRepository
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatVilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.VilkårperiodeRepository
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.VilkårperiodeDto
+import org.slf4j.LoggerFactory
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -28,7 +31,11 @@ class KopierDataFraTidligereBehandlingController(
     private val behandlingRepository: BehandlingRepository,
     private val vilkårperiodeService: VilkårperiodeService,
     private val stønadsperiodeService: StønadsperiodeService,
+    private val stønadsperiodeRepository: StønadsperiodeRepository,
+    private val vilkårperiodeRepository: VilkårperiodeRepository,
 ) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     private fun utførEndringSomSystem() {
         SpringTokenValidationContextHolder().setTokenValidationContext(null)
@@ -44,48 +51,69 @@ class KopierDataFraTidligereBehandlingController(
     @Transactional
     fun inngangsvilkår(
         @PathVariable behandlingId: UUID,
-        @RequestParam(required = false) forrigeBehandlingId: UUID? = null,
     ) {
         utførEndringSomSystem()
 
         val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
-        validerHarLikFagsak(forrigeBehandlingId, behandling)
         validerRiktigStatus(behandling)
 
-        val gjenbrukFraId = forrigeBehandlingId ?: behandling.forrigeBehandlingId!!
+        val gjenbrukFraId = behandling.forrigeBehandlingId!!
         val perioder = vilkårperiodeService.hentVilkårperioderDto(behandling.id)
         val forrigePerioder = vilkårperiodeService.hentVilkårperioderDto(gjenbrukFraId)
 
-        brukerfeilHvis(perioder.målgrupper.any { v -> forrigePerioder.målgrupper.any { it.overlapper(v) } }) {
-            "Har målgrupper som overlapper"
-        }
+        kopierVilkårperiode(forrigePerioder.målgrupper, perioder.målgrupper, behandling)
+        kopierVilkårperiode(forrigePerioder.aktiviteter, perioder.aktiviteter, behandling)
+    }
 
-        brukerfeilHvis(perioder.aktiviteter.any { v -> forrigePerioder.aktiviteter.any { it.overlapper(v) } }) {
-            "Har aktiviteter som overlapper"
-        }
-        vilkårperiodeService.gjenbrukVilkårperioder(forrigeBehandlingId = gjenbrukFraId, nyBehandlingId = behandlingId)
+    private fun kopierVilkårperiode(
+        forrigePerioder: List<VilkårperiodeDto>,
+        nåværende: List<VilkårperiodeDto>,
+        behandling: Behandling,
+    ) {
+        forrigePerioder
+            .filter { it.resultat == ResultatVilkårperiode.OPPFYLT || it.resultat == ResultatVilkårperiode.IKKE_OPPFYLT }
+            .filter { forrige -> nåværende.none { m -> forrige.overlapper(m) && m.type == forrige.type } }
+            .forEach {
+                logger.info("Kopirerer vilkårperiode=${it.id} til behandling=${behandling.id}")
+                vilkårperiodeRepository.findByIdOrThrow(it.id)
+                    .copy(
+                        id = UUID.randomUUID(),
+                        behandlingId = behandling.id,
+                        sporbar = Sporbar(),
+                    )
+                    .let {
+                        vilkårperiodeRepository.insert(it)
+                    }
+            }
     }
 
     @PostMapping("{behandlingId}/stonadsperioder")
     @Transactional
     fun stønadsperioder(
         @PathVariable behandlingId: UUID,
-        @RequestParam(required = false) forrigeBehandlingId: UUID? = null,
     ) {
         utførEndringSomSystem()
 
         val behandling = behandlingRepository.findByIdOrThrow(behandlingId)
-        validerHarLikFagsak(forrigeBehandlingId, behandling)
         validerRiktigStatus(behandling)
 
-        val gjenbrukFra = forrigeBehandlingId ?: behandling.forrigeBehandlingId!!
+        val gjenbrukFra = behandling.forrigeBehandlingId!!
         val perioder = stønadsperiodeService.hentStønadsperioder(behandling.id)
         val forrigePerioder = stønadsperiodeService.hentStønadsperioder(gjenbrukFra)
 
-        brukerfeilHvis(perioder.any { v -> forrigePerioder.any { it.overlapper(v) } }) {
-            "Har stønadsperioder som overlapper"
-        }
-        stønadsperiodeService.gjenbrukStønadsperioder(forrigeBehandlingId = gjenbrukFra, nyBehandlingId = behandlingId)
+        forrigePerioder
+            .filter { forrigePeriode -> perioder.none { it.overlapper(forrigePeriode) } }
+            .forEach { forrigePeriode ->
+                logger.info("Kopirerer stønadsperiode=${forrigePeriode.id} til behandling=${behandling.id}")
+                stønadsperiodeRepository.findByIdOrThrow(forrigePeriode.id!!)
+                    .copy(
+                        id = UUID.randomUUID(),
+                        behandlingId = behandling.id,
+                        sporbar = Sporbar(),
+                    )
+                    .let { stønadsperiodeRepository.insert(it) }
+            }
+        stønadsperiodeService.validerStønadsperioder(behandlingId)
     }
 
     private fun validerRiktigStatus(behandling: Behandling) {
