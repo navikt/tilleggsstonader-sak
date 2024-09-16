0ecb1f3243943a0a56be3a2b05d683e04c4c4200
Eirik Emil Neesgaard Årseth <eirik.arseth@nav.no>
2024-09-16 10:54:17 +0200

    Skal kunne hente metric for antall løpende behandlinger pr. stønadstype (#424)
    * Skal kunne hente metric for antall løpende behandlinger pr. stønadstype
    
    * Rename
    
    * Rename

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/metrics/AntallBehandlingerMetricService.kt
@@ -0,0 +1,25 @@
+package no.nav.tilleggsstonader.sak.metrics
+
+import io.micrometer.core.instrument.Metrics
+import io.micrometer.core.instrument.MultiGauge
+import io.micrometer.core.instrument.Tag
+import io.micrometer.core.instrument.Tags
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingRepository
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+
+@Service
+class AntallBehandlingerMetricService(private val behandlingRepository: BehandlingRepository) {
+
+    private val antallGjeldendeIverksatteBehandlingerGauge = MultiGauge.builder("gjeldende_iverksatte_behandlinger").register(Metrics.globalRegistry)
+
+    @Scheduled(initialDelay = MetricUtil.FREKVENS_30_SEC, fixedDelay = MetricUtil.FREKVENS_30_MIN)
+    fun hentAntallGjeldendeIverksatteBehandlinger() {
+        val rows = Stønadstype.entries.map {
+            val antallGjeldendeIverksatteBehandlinger = behandlingRepository.antallGjeldendeIverksatteBehandlinger(stønadstype = it)
+            MultiGauge.Row.of(Tags.of(Tag.of("ytelse", it.name)), antallGjeldendeIverksatteBehandlinger)
+        }
+        antallGjeldendeIverksatteBehandlingerGauge.register(rows, true)
+    }
+}
