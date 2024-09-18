8ade0af9aef058fcbeafd9885eefb6775a0b28a9
sarahjelle <sara.hjelle@bekk.no>
2024-09-18 15:26:01 +0200

    Legg til tester på mapping av status

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/regler/evalutation/OppdaterVilkårTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/regler/evalutation/OppdaterVilkårTest.kt
@@ -2,13 +2,20 @@ package no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.evalutation
 
 import no.nav.tilleggsstonader.sak.felles.domain.BarnId
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import no.nav.tilleggsstonader.sak.util.behandling
 import no.nav.tilleggsstonader.sak.util.vilkår
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkår
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.VilkårStatus
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.VilkårType
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkårsresultat
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dto.OpprettVilkårDto
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dto.SvarPåVilkårDto
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.evalutation.OppdaterVilkår.oppdaterVilkår
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.evalutation.OppdaterVilkår.validerVilkårOgBeregnResultat
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.vilkår.PassBarnRegelTestUtil.ikkeOppfylteDelvilkårPassBarnDto
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.vilkår.PassBarnRegelTestUtil.oppfylteDelvilkårPassBarn
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.regler.vilkår.PassBarnRegelTestUtil.oppfylteDelvilkårPassBarnDto
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
@@ -67,5 +74,57 @@ internal class OppdaterVilkårTest {
                 validerVilkårOgBeregnResultat(vilkår, opprettVilkårDto.copy(vilkårType = VilkårType.EKSEMPEL))
             }.hasMessageContaining("Mangler beløp på vilkår")
         }
+    }
+
+    @Nested
+    inner class StatusPåVilkår {
+        val behandling = behandling()
+        val regelResultat = RegelResultat(
+            vilkårType = VilkårType.PASS_BARN,
+            vilkår = Vilkårsresultat.OPPFYLT,
+            delvilkår = emptyMap(),
+        )
+
+        @Test
+        fun `Nye vilkår skal ha status ny etter oppdatering`() {
+            val vilkår = vilkår(behandling.id, type = VilkårType.PASS_BARN, status = VilkårStatus.NY)
+            val innsendtOppdatering = innsendtOppdatering(vilkår)
+
+            val oppdaterVilkår =
+                oppdaterVilkår(vilkår = vilkår, oppdatering = innsendtOppdatering, vilkårsresultat = regelResultat)
+
+            assertThat(oppdaterVilkår.status).isEqualTo(VilkårStatus.NY)
+        }
+
+        @Test
+        fun `Vilkår med status endret skal ikke endre status ved oppdatering`() {
+            val vilkår = vilkår(behandling.id, type = VilkårType.PASS_BARN, status = VilkårStatus.ENDRET)
+            val innsendtOppdatering = innsendtOppdatering(vilkår)
+
+            val oppdaterVilkår =
+                oppdaterVilkår(vilkår = vilkår, oppdatering = innsendtOppdatering, vilkårsresultat = regelResultat)
+
+            assertThat(oppdaterVilkår.status).isEqualTo(VilkårStatus.ENDRET)
+        }
+
+        @Test
+        fun `Vilkår med status uendret skal få status endret etter oppdatering`() {
+            val vilkår = vilkår(behandling.id, type = VilkårType.PASS_BARN, status = VilkårStatus.UENDRET)
+            val innsendtOppdatering = innsendtOppdatering(vilkår)
+
+            val oppdaterVilkår =
+                oppdaterVilkår(vilkår = vilkår, oppdatering = innsendtOppdatering, vilkårsresultat = regelResultat)
+
+            assertThat(oppdaterVilkår.status).isEqualTo(VilkårStatus.ENDRET)
+        }
+
+        private fun innsendtOppdatering(originaltVilkår: Vilkår) = SvarPåVilkårDto(
+            id = originaltVilkår.id,
+            behandlingId = behandling.id,
+            delvilkårsett = oppfylteDelvilkårPassBarnDto(),
+            fom = originaltVilkår.fom,
+            tom = originaltVilkår.tom,
+            utgift = 100,
+        )
     }
 }
