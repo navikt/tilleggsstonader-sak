19540cc60bb94c22f11ab3aff5888955bfd422c1
Johan Blomgren <blommish@users.noreply.github.com>
2024-09-19 11:14:10 +0200

    Oppdaterer vilkårperiode og vilkår med riktig status

--- src/main/resources/db/migration/V49__perioder_status.sql
+++ src/main/resources/db/migration/V49__perioder_status.sql
@@ -1,2 +1,80 @@
-ALTER TABLE vilkar_periode ADD COLUMN status VARCHAR;
-ALTER TABLE vilkar ADD COLUMN status VARCHAR;
+ALTER TABLE vilkar_periode
+    ADD COLUMN status VARCHAR;
+ALTER TABLE vilkar
+    ADD COLUMN status VARCHAR;
+
+----------------
+-- Vilkårperiode
+----------------
+UPDATE vilkar_periode
+SET status = 'NY';
+
+-- Oppdaterer vilkårperiode til UENDRET, for å senere oppdatere de som har endringer til ENDRET
+UPDATE vilkar_periode vp
+SET status = 'UENDRET'
+FROM vilkar_periode vp_gammel
+WHERE vp.forrige_vilkarperiode_id = vp_gammel.id
+  AND vp.type = vp_gammel.type
+  AND vp.fom = vp_gammel.fom
+  AND vp.tom = vp_gammel.tom
+  AND vp.resultat = vp_gammel.resultat
+  AND vp.aktivitetsdager = vp_gammel.aktivitetsdager
+  AND vp.delvilkar::text = vp_gammel.delvilkar::text
+  AND vp.resultat <> 'SLETTET'
+;
+
+-- Oppdaterer vilkårperiode til ENDRET dersom det finnes endringer fra forrige vilkår
+UPDATE vilkar_periode vp
+SET status = 'ENDRET'
+FROM vilkar_periode vp_gammel
+WHERE vp.forrige_vilkarperiode_id = vp_gammel.id
+  AND NOT (
+    vp.type = vp_gammel.type
+        AND vp.fom = vp_gammel.fom
+        AND vp.tom = vp_gammel.tom
+        AND vp.resultat = vp_gammel.resultat
+        AND vp.aktivitetsdager = vp_gammel.aktivitetsdager
+        AND vp.delvilkar::text = vp_gammel.delvilkar::text
+    )
+  AND vp.resultat <> 'SLETTET'
+;
+
+UPDATE vilkar_periode
+SET status = 'SLETTET'
+WHERE resultat = 'SLETTET';
+ALTER TABLE vilkar_periode
+    ALTER COLUMN status SET NOT NULL;
+
+----------------
+-- Vilkår
+----------------
+UPDATE vilkar
+SET status = 'NY';
+
+-- Oppdaterer vilkår til UENDRET hvis de har opphavsvilkår, for å senere oppdatere de som er endret til endret
+UPDATE vilkar vp
+SET status = 'UENDRET'
+FROM behandling b
+WHERE vp.behandling_id = b.id
+  AND vp.opphavsvilkaar_behandling_id IS NOT NULL
+;
+
+-- Oppdaterer de vilkår som har opphavsvilkår men mangler match i forrige behandling, til status ENDRET
+UPDATE vilkar vp
+SET status = 'ENDRET'
+WHERE vp.opphavsvilkaar_behandling_id IS NOT NULL
+  AND NOT EXISTS (SELECT *
+                  FROM behandling b
+                           JOIN behandling_barn barn ON vp.barn_id::uuid = barn.id
+                           JOIN vilkar vilkar_forrige on vilkar_forrige.behandling_id = b.forrige_behandling_id
+                           JOIN behandling_barn barn_forrige
+                                ON vilkar_forrige.barn_id::uuid = barn_forrige.id AND barn.ident = barn_forrige.ident
+                  WHERE vilkar_forrige.behandling_id = b.forrige_behandling_id
+                    AND vilkar_forrige.fom = vp.fom
+                    AND vilkar_forrige.tom = vp.tom
+                    AND vilkar_forrige.utgift = vp.utgift
+                    AND vilkar_forrige.delvilkar::text = vp.delvilkar::text)
+;
+
+ALTER TABLE vilkar
+    ALTER COLUMN status SET NOT NULL;
