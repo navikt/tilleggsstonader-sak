77671d1bfbbf8dd9f90d13771ba7c46256e940f5
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-03 12:18:22 +0200

    Rydder opp i test før jeg begynner å legge til nytt

--- src/test/kotlin/no/nav/tilleggsstonader/sak/brev/frittstående/FrittståendeBrevServiceTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/brev/frittstående/FrittståendeBrevServiceTest.kt
@@ -1,16 +1,15 @@
 package no.nav.tilleggsstonader.sak.brev.frittstående
 
 import no.nav.familie.prosessering.internal.TaskService
-import no.nav.tilleggsstonader.libs.log.mdc.MDCConstants
 import no.nav.tilleggsstonader.sak.IntegrationTest
 import no.nav.tilleggsstonader.sak.fagsak.domain.PersonIdent
+import no.nav.tilleggsstonader.sak.util.BrukerContextUtil.clearBrukerContext
 import no.nav.tilleggsstonader.sak.util.BrukerContextUtil.mockBrukerContext
 import no.nav.tilleggsstonader.sak.util.fagsak
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 
 internal class FrittståendeBrevServiceTest : IntegrationTest() {
@@ -25,25 +24,24 @@ internal class FrittståendeBrevServiceTest : IntegrationTest() {
     @BeforeEach
     fun setUp() {
         mockBrukerContext("456")
-        MDC.put(MDCConstants.MDC_CALL_ID, "")
         testoppsettService.lagreFagsak(fagsakTilknyttetPesonIdent123)
     }
 
     @AfterEach
     override fun tearDown() {
         super.tearDown()
-        MDC.remove(MDCConstants.MDC_CALL_ID)
+        clearBrukerContext()
     }
 
     @Test
-    fun `skal opprette task for distribuering etter journalføring`() {
+    fun `skal opprette task for journalføring av frittstående brev`() {
         frittståendeBrevService.sendFrittståendeBrev(
             fagsakId = fagsakTilknyttetPesonIdent123.id,
             request = FrittståendeBrevDto(pdf = "brev".toByteArray(), tittel = "Tittel"),
         )
 
-        val res = taskService.findAll().single()
+        val task = taskService.findAll().single()
 
-        Assertions.assertThat(res.type).isEqualTo(JournalførFrittståendeBrevTask.TYPE)
+        assertThat(task.type).isEqualTo(JournalførFrittståendeBrevTask.TYPE)
     }
 }
