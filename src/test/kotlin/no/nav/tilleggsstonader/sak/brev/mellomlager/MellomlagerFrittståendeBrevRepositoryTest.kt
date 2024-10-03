51f291c0b0bfc75912290ff59291f9fd01d736c6
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-03 07:32:24 +0200

    Tester at mellomlagring er unik per saksbehandler

--- src/test/kotlin/no/nav/tilleggsstonader/sak/brev/mellomlager/MellomlagerFrittståendeBrevRepositoryTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/brev/mellomlager/MellomlagerFrittståendeBrevRepositoryTest.kt
@@ -1,10 +1,12 @@
 package no.nav.tilleggsstonader.sak.brev.mellomlager
 
 import no.nav.tilleggsstonader.sak.IntegrationTest
+import no.nav.tilleggsstonader.sak.felles.domain.FagsakId
 import no.nav.tilleggsstonader.sak.util.BrukerContextUtil.testWithBrukerContext
 import no.nav.tilleggsstonader.sak.util.fagsak
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.within
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.temporal.ChronoUnit
@@ -12,20 +14,20 @@ import java.time.temporal.ChronoUnit
 internal class MellomlagerFrittståendeBrevRepositoryTest : IntegrationTest() {
 
     @Autowired
-    private lateinit var mellomlagerFrittståendeBrevRepository: MellomlagerFrittståendeBrevRepository
+    private lateinit var repository: MellomlagerFrittståendeBrevRepository
+
+    private val fagsak = fagsak()
+
+    @BeforeEach
+    fun setUp() {
+        testoppsettService.lagreFagsak(fagsak)
+    }
 
     @Test
     internal fun `skal lagre mellomlagret brev`() {
-        val fagsak = testoppsettService.lagreFagsak(fagsak())
-        val mellomlagretBrev = MellomlagretFrittståendeBrev(
-            fagsakId = fagsak.id,
-            brevverdier = "{}",
-            brevmal = "",
-        )
+        val mellomlagretBrev = repository.insert(mellomlagretFrittståendeBrev())
 
-        mellomlagerFrittståendeBrevRepository.insert(mellomlagretBrev)
-
-        val mellomlagretBrevFraDb = mellomlagerFrittståendeBrevRepository.findById(mellomlagretBrev.id)
+        val mellomlagretBrevFraDb = repository.findById(mellomlagretBrev.id)
         assertThat(mellomlagretBrevFraDb).get()
             .usingRecursiveComparison()
             .ignoringFields("sporbar.endret.endretTid")
@@ -40,15 +42,9 @@ internal class MellomlagerFrittståendeBrevRepositoryTest : IntegrationTest() {
         val fagsak = testoppsettService.lagreFagsak(fagsak())
 
         testWithBrukerContext(saksbehandlerIdent) {
-            val mellomlagretBrev = MellomlagretFrittståendeBrev(
-                fagsakId = fagsak.id,
-                brevverdier = "{}",
-                brevmal = "",
-            )
-
-            mellomlagerFrittståendeBrevRepository.insert(mellomlagretBrev)
+            val mellomlagretBrev = repository.insert(mellomlagretFrittståendeBrev())
             val mellomlagretBrevFraDb =
-                mellomlagerFrittståendeBrevRepository.findByFagsakIdAndSporbarOpprettetAv(fagsak.id, saksbehandlerIdent)
+                repository.findByFagsakIdAndSporbarOpprettetAv(fagsak.id, saksbehandlerIdent)
             assertThat(mellomlagretBrevFraDb)
                 .usingRecursiveComparison()
                 .ignoringFields("sporbar.endret.endretTid")
@@ -58,4 +54,25 @@ internal class MellomlagerFrittståendeBrevRepositoryTest : IntegrationTest() {
                 .isCloseTo(mellomlagretBrev.sporbar.endret.endretTid, within(1, ChronoUnit.SECONDS))
         }
     }
+
+    @Test
+    fun `mellomlagring er unik per saksbehandler og fagsakId og saksbehandler A skal ikke finne brevet til saksbehandler B`() {
+        val saksbehandlerA = "A"
+        val saksbehandlerB = "B"
+
+        testWithBrukerContext(saksbehandlerB) {
+            repository.insert(mellomlagretFrittståendeBrev())
+        }
+
+        assertThat(repository.findByFagsakIdAndSporbarOpprettetAv(fagsak.id, saksbehandlerB)).isNotNull
+        assertThat(repository.findByFagsakIdAndSporbarOpprettetAv(FagsakId.random(), saksbehandlerB)).isNull()
+
+        assertThat(repository.findByFagsakIdAndSporbarOpprettetAv(fagsak.id, saksbehandlerA)).isNull()
+    }
+
+    private fun mellomlagretFrittståendeBrev() = MellomlagretFrittståendeBrev(
+        fagsakId = fagsak.id,
+        brevverdier = "{}",
+        brevmal = "",
+    )
 }
