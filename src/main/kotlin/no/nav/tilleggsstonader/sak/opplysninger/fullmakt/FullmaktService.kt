655a30226d273ce8213b372f69ec091c89271058
Runar Skagestad <runar.skagestad@nav.no>
2024-10-15 11:28:10 +0200

    Midlertidig fang opp og logg alle alle feil som oppstår når vi forsøker å hente fullmakter

--- src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/fullmakt/FullmaktService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/fullmakt/FullmaktService.kt
@@ -1,15 +1,22 @@
 package no.nav.tilleggsstonader.sak.opplysninger.fullmakt
 
 import no.nav.tilleggsstonader.kontrakter.felles.Tema
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class FullmaktService(
     private val fullmaktClient: FullmaktClient,
 ) {
+    val log = LoggerFactory.getLogger(FullmaktService::class.java)
     fun hentFullmektige(fullmaktsgiversIdent: String): List<FullmektigDto> {
-        return fullmaktClient.hentFullmektige(fullmaktsgiversIdent)
-        // .filtrerPåTilleggsstønadTemaer()
+        return try {
+            fullmaktClient.hentFullmektige(fullmaktsgiversIdent)
+            // .filtrerPåTilleggsstønadTemaer()
+        } catch (ex: Exception) {
+            log.error("Klarte ikke hente fullmakter: {} \n {}", ex.message, ex.stackTraceToString())
+            emptyList()
+        }
     }
 }
 
