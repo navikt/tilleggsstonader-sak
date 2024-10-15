80b1275dc40a5d4dbdcf092ceceecfd631285c83
Runar Skagestad <runar.skagestad@nav.no>
2024-10-15 11:38:33 +0200

    Fiks kompileringsfeil i test

--- src/test/kotlin/no/nav/tilleggsstonader/sak/opplysninger/PersonopplysningerServiceTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/opplysninger/PersonopplysningerServiceTest.kt
@@ -6,6 +6,8 @@ import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.fagsak.domain.FagsakPersonService
 import no.nav.tilleggsstonader.sak.felles.domain.FagsakPersonId
 import no.nav.tilleggsstonader.sak.opplysninger.dto.Adressebeskyttelse
+import no.nav.tilleggsstonader.sak.opplysninger.fullmakt.FullmaktService
+import no.nav.tilleggsstonader.sak.opplysninger.fullmakt.FullmektigDto
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.dto.AdressebeskyttelseGradering
 import no.nav.tilleggsstonader.sak.util.PdlTestdataHelper.metadataGjeldende
@@ -15,6 +17,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.dto.Adressebeskyttelse as AdressebeskyttelsePdl
 
 class PersonopplysningerServiceTest {
@@ -22,11 +25,13 @@ class PersonopplysningerServiceTest {
     private val fagsakPersonService = mockk<FagsakPersonService>()
     private val behandlingService = mockk<BehandlingService>()
     private val personService = mockk<PersonService>()
+    private val fullmaktService = mockk<FullmaktService>()
 
     private val service = PersonopplysningerService(
         fagsakPersonService = fagsakPersonService,
         behandlingService = behandlingService,
         personService = personService,
+        fullmaktService = fullmaktService,
     )
 
     @BeforeEach
@@ -34,7 +39,16 @@ class PersonopplysningerServiceTest {
         every { fagsakPersonService.hentAktivIdent(any()) } returns "0"
         every { behandlingService.hentAktivIdent(any()) } returns "1"
         every { personService.hentSøker(any()) } returns pdlSøker()
+        every { fullmaktService.hentFullmektige(any()) } returns listOf(fullmektigStub)
     }
+
+    private val fullmektigStub = FullmektigDto(
+        fullmektigIdent = "99999999999",
+        fullmektigNavn = "Gulliver",
+        gyldigFraOgMed = LocalDate.parse("2023-01-01"),
+        gyldigTilOgMed = LocalDate.parse("2024-01-01"),
+        temaer = listOf("TSO")
+    )
 
     @Nested
     inner class HarVerge {
