4c8e56b3bfd139376922d73385d92054855c0096
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-16 12:34:59 +0200

    Lagt til endepunkt for å kunne hente vedtak fra Arena

--- src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/arena/ArenaClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/opplysninger/arena/ArenaClient.kt
@@ -3,6 +3,7 @@ package no.nav.tilleggsstonader.sak.opplysninger.arena
 import no.nav.tilleggsstonader.kontrakter.arena.ArenaStatusDto
 import no.nav.tilleggsstonader.kontrakter.arena.ArenaStatusHarSakerDto
 import no.nav.tilleggsstonader.kontrakter.arena.oppgave.ArenaOppgaveDto
+import no.nav.tilleggsstonader.kontrakter.arena.vedtak.ArenaSakOgVedtakDto
 import no.nav.tilleggsstonader.kontrakter.felles.IdenterRequest
 import no.nav.tilleggsstonader.kontrakter.felles.IdenterStønadstype
 import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
@@ -21,11 +22,16 @@ class ArenaClient(
 
     val statusUri = UriComponentsBuilder.fromUri(arenaUri).pathSegment("api", "status").toUriString()
 
+    val vedtakUri = UriComponentsBuilder.fromUri(arenaUri).pathSegment("api", "vedtak").toUriString()
+
     val statusHarSakerUri = UriComponentsBuilder.fromUri(arenaUri).pathSegment("api", "status", "har-saker").toUriString()
 
     val oppgaverUri = UriComponentsBuilder.fromUri(arenaUri).pathSegment("api", "oppgave").toUriString()
 
     fun hentStatus(request: IdenterStønadstype): ArenaStatusDto =
+        postForEntity(statusUri, request)
+
+    fun hentVedtak(request: IdenterRequest): ArenaSakOgVedtakDto =
         postForEntity(statusUri, request)
 
     fun harSaker(request: IdenterRequest): ArenaStatusHarSakerDto =
