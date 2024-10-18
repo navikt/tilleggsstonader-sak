613f16423a941f06b14e897d685b50f54fa8212d
Sigmund Berbom <sberbom@gmail.com>
2024-10-18 14:48:38 +0200

    Ta høyde for perioder i bergning av læremidler

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/SatsLæremidler.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/SatsLæremidler.kt
@@ -1,9 +1,8 @@
-package no.nav.tilleggsstonader.sak.vedtak.læremidler
+package no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning
 
-import java.time.YearMonth
 import no.nav.tilleggsstonader.kontrakter.felles.Periode
-
-private val MAX = YearMonth.of(2099, 12)
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.domain.Studienivå
+import java.time.YearMonth
 
 data class SatsLæremidler(
     override val fom: YearMonth,
@@ -15,19 +14,22 @@ data class SatsLæremidler(
     }
 }
 
+private val MAX = YearMonth.of(2099, 12)
+
 val satser: List<SatsLæremidler> = listOf(
     SatsLæremidler(
         fom = YearMonth.of(2024, 1),
         tom = MAX,
         beløp = mapOf(Studienivå.VIDEREGÅENDE to 438, Studienivå.HØYERE_UTDANNING to 875),
-    ), SatsLæremidler(
+    ),
+    SatsLæremidler(
         fom = YearMonth.of(2023, 1),
         tom = YearMonth.of(2023, 12),
         beløp = mapOf(Studienivå.VIDEREGÅENDE to 411, Studienivå.HØYERE_UTDANNING to 822),
-    )
+    ),
 )
 
-fun finnSats(måned: YearMonth, studienivå: Studienivå): Int {
+fun finnSatsForStudienivå(måned: YearMonth, studienivå: Studienivå): Int {
     val sats = satser.find { måned in it.fom..it.tom } ?: error("Finner ikke satser for $måned")
     return sats.beløp[studienivå] ?: error("Kan ikke håndtere satser for studienivå=$studienivå")
 }
