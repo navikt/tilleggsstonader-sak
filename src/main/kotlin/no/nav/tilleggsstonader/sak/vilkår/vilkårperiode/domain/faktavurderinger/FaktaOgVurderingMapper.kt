4e9a496516e7c7832882d901b8eba065b193c8a4
Johan Blomgren <blommish@users.noreply.github.com>
2024-11-07 22:55:10 +0100

    Fjerner mapper som ikke brukes

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/domain/faktavurderinger/FaktaOgVurderingMapper.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/domain/faktavurderinger/FaktaOgVurderingMapper.kt
@@ -5,41 +5,15 @@ import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.DelvilkårAktivitet
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.DelvilkårMålgruppe
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
-import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.Vilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.DelvilkårMålgruppeDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.LagreVilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.evaluering.EvalueringMålgruppe
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.evaluering.ResultatEvaluering
 
-fun mapFaktaOgVurdering(vilkårperiode: Vilkårperiode): FaktaOgVurdering = with(vilkårperiode) {
-    val delvilkår = vilkårOgFakta.delvilkår
-    return when (type) {
-        AktivitetType.TILTAK -> TiltakTilsynBarn(
-            fakta = FaktaAktivitetTilsynBarn(vilkårOgFakta.aktivitetsdager!!),
-            vurderinger = VurderingTiltakTilsynBarn(lønnet = (delvilkår as DelvilkårAktivitet).lønnet),
-        )
-
-        AktivitetType.UTDANNING -> UtdanningTilsynBarn(
-            fakta = FaktaAktivitetTilsynBarn(vilkårOgFakta.aktivitetsdager!!),
-        )
-
-        AktivitetType.REELL_ARBEIDSSØKER -> ReellArbeidsøkerTilsynBarn(
-            fakta = FaktaAktivitetTilsynBarn(vilkårOgFakta.aktivitetsdager!!),
-        )
-
-        AktivitetType.INGEN_AKTIVITET -> IngenAktivitetTilsynBarn
-
-        is MålgruppeType -> MålgruppeTilsynBarn(
-            type = MålgruppeTilsynBarnType.entries.single { it.vilkårperiodeType == type },
-            vurderinger = MålgruppeVurderinger(
-                medlemskap = (delvilkår as DelvilkårMålgruppe).medlemskap,
-                dekketAvAnnetRegelverk = delvilkår.dekketAvAnnetRegelverk,
-            ),
-        )
-    }
-}
-
-fun mapFaktaOgVurderingDto(vilkårperiode: LagreVilkårperiode, resultatEvaluering: ResultatEvaluering): FaktaOgVurdering {
+fun mapFaktaOgVurderingDto(
+    vilkårperiode: LagreVilkårperiode,
+    resultatEvaluering: ResultatEvaluering,
+): FaktaOgVurdering {
     return when (vilkårperiode.type) {
         AktivitetType.TILTAK -> {
             require(resultatEvaluering.delvilkår is DelvilkårAktivitet)
