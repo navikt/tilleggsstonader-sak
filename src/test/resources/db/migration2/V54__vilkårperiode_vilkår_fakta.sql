6a685480d2aa0d3cd1a21a144359e3ba5c6ef683
Johan Blomgren <blommish@users.noreply.github.com>
2024-11-07 19:40:13 +0100

    Oppdaterer db

--- src/test/resources/db/migration2/V54__vilkårperiode_vilkår_fakta.sql
+++ src/test/resources/db/migration2/V54__vilkårperiode_vilkår_fakta.sql
@@ -2,7 +2,18 @@
 ALTER TABLE vilkar_periode
     ADD COLUMN fakta_og_vurdering JSONB;
 
---UPDATE vilkar_periode SET fakta_og_vurdering = ;
+UPDATE vilkar_periode
+SET fakta_og_vurdering =
+        jsonb_build_object(
+                'vurderinger', delvilkar,
+                'fakta',
+                CASE
+                    WHEN aktivitetsdager IS NOT NULL THEN jsonb_build_object('aktivitetsdager', aktivitetsdager)
+                    ELSE '{}'::jsonb
+                    END
+        );
 
-ALTER TABLE vilkar_periode DROP COLUMN delvilkar;
-ALTER TABLE vilkar_periode ALTER COLUMN fakta_og_vurdering SET NOT NULL;
+ALTER TABLE vilkar_periode
+    DROP COLUMN delvilkar;
+ALTER TABLE vilkar_periode
+    ALTER COLUMN fakta_og_vurdering SET NOT NULL;
