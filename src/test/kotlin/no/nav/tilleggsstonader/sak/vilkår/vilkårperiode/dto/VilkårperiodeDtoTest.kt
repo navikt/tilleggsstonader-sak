a5ddd3a3d6d23c1f1911ee5d2ad4b00c991df026
Johan Blomgren <blommish@users.noreply.github.com>
2024-11-12 17:54:54 +0100

    Lagt tilbake test og sjekker at mappingen virker som forventet

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/dto/VilkårperiodeDtoTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/dto/VilkårperiodeDtoTest.kt
@@ -1,10 +1,17 @@
 package no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto
 
 import no.nav.tilleggsstonader.libs.utils.osloDateNow
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeExtensions.dekketAvAnnetRegelverk
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeExtensions.medlemskap
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.målgruppe
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.DelvilkårMålgruppe
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.DelvilkårVilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatDelvilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatVilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.SvarJaNei
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Nested
@@ -21,6 +28,36 @@ class VilkårperiodeDtoTest {
                 tom = osloDateNow().minusDays(1),
             ).tilDto()
         }.hasMessageContaining("Til-og-med før fra-og-med")
+    }
+
+    @Nested
+    inner class MappingAvVurdering {
+
+        @Test
+        fun `skal mappe vurdering fra vilkår`() {
+            val målgruppe = målgruppe(
+                type = MålgruppeType.NEDSATT_ARBEIDSEVNE,
+                begrunnelse = "begrunnelse",
+                delvilkår = DelvilkårMålgruppe(
+                    medlemskap = DelvilkårVilkårperiode.Vurdering(
+                        svar = SvarJaNei.JA,
+                        resultat = ResultatDelvilkårperiode.OPPFYLT,
+                    ),
+                    dekketAvAnnetRegelverk = DelvilkårVilkårperiode.Vurdering(
+                        svar = SvarJaNei.JA,
+                        resultat = ResultatDelvilkårperiode.IKKE_OPPFYLT,
+                    ),
+                ),
+            ).tilDto()
+
+            assertThat(målgruppe.medlemskap).isNotNull
+            assertThat(målgruppe.medlemskap?.svar).isEqualTo(SvarJaNei.JA)
+            assertThat(målgruppe.medlemskap?.resultat).isEqualTo(ResultatDelvilkårperiode.OPPFYLT)
+            assertThat(målgruppe.dekketAvAnnetRegelverk).isNotNull
+            assertThat(målgruppe.dekketAvAnnetRegelverk?.svar).isEqualTo(SvarJaNei.JA)
+            assertThat(målgruppe.dekketAvAnnetRegelverk?.resultat).isEqualTo(ResultatDelvilkårperiode.IKKE_OPPFYLT)
+        }
+
     }
 
     @Nested
