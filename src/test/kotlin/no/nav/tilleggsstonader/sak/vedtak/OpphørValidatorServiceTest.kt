63c2c1d2a14ae3e8afcc346004ccfb24adf16796
eirikvilpponen <eirik.vilpponen@nav.no>
2024-11-14 12:19:03 +0100

    test kjører grønt

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/OpphørValidatorServiceTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/OpphørValidatorServiceTest.kt
@@ -1,10 +1,16 @@
 package no.nav.tilleggsstonader.sak.vedtak
 
+import io.mockk.every
 import io.mockk.mockk
+import no.nav.tilleggsstonader.libs.utils.osloDateNow
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
+import no.nav.tilleggsstonader.sak.interntVedtak.Vilkårperiode
 import no.nav.tilleggsstonader.sak.util.saksbehandling
 import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.beregning.TilsynBarnBeregningService
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.domain.BeregningsresultatTilsynBarn
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.VilkårService
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.Vilkårperioder
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -15,7 +21,7 @@ class OpphørValidatorServiceTest {
     private val vilkårService = mockk<VilkårService>()
     private val tilsynBarnBeregningService = mockk<TilsynBarnBeregningService>()
 
-    val saksbehandling = saksbehandling()
+    val saksbehandling = saksbehandling(revurderFra = osloDateNow(), type = BehandlingType.REVURDERING)
 
     val opphørValidatorService = OpphørValidatorService(vilkårperiodeService, vilkårService, tilsynBarnBeregningService)
 
@@ -27,8 +33,12 @@ class OpphørValidatorServiceTest {
     fun tearDown() {
     }
 
+
     @Test
     fun validerValiderIngenOppfylteVilkårEllerVilkårperioderMedStatusNy() {
+        every { vilkårService.hentVilkår(saksbehandling.id) } returns emptyList()
+        every { vilkårperiodeService.hentVilkårperioder(saksbehandling.id) } returns Vilkårperioder(emptyList(), emptyList())
+        every { tilsynBarnBeregningService.beregn(saksbehandling) } returns BeregningsresultatTilsynBarn(emptyList())
 
         opphørValidatorService.validerOpphør(saksbehandling)
     }
