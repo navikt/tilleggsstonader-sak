5273f2033e4a5d355161c65b6ebcdcd21d47ac1b
Johan Blomgren <blommish@users.noreply.github.com>
2024-11-21 12:26:16 +0100

    Justerer JsonTypeInfo til å ikke legge på "@type" ved serialisering av json-dataen, men at "type" brukes, likt FaktaOgVurderingerJson

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/dto/VedtakTilsynBarnDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/dto/VedtakTilsynBarnDto.kt
@@ -4,9 +4,16 @@ import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY,
+    property = "type",
+    visible = true,
+)
 @JsonSubTypes(
     JsonSubTypes.Type(InnvilgelseTilsynBarnDto::class, name = "INNVILGELSE"),
     JsonSubTypes.Type(AvslagTilsynBarnDto::class, name = "AVSLAG"),
+    JsonSubTypes.Type(OpphørTilsynBarnDto::class, name = "OPPHØR"),
+    failOnRepeatedNames = true,
 )
 sealed class VedtakTilsynBarnDto(open val type: TypeVedtak)
