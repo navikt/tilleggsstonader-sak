a59535e2ff7e1c4be2b2fadd606c8cfcd59f5154
eirikvilpponen <eirik.vilpponen@nav.no>
2024-11-21 13:18:55 +0100

    Oppdaterer validering på Opphør av Vilkår og Vilkårsperioder.

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/OpphørValideringService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/OpphørValideringService.kt
@@ -9,6 +9,7 @@ import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.VilkårStatus
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkårsresultat
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.ResultatVilkårperiode
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.Vilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.Vilkårperioder
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.felles.Vilkårstatus
 import org.springframework.stereotype.Service
@@ -65,20 +66,28 @@ class OpphørValideringService(
         revurderFraDato: LocalDate,
     ) {
         vilkårperioder.målgrupper.forEach { vilkårperiode ->
-            brukerfeilHvis(vilkårperiode.status == Vilkårstatus.ENDRET && vilkårperiode.tom > revurderFraDato) {
+            brukerfeilHvis(vilkårperiode.erOppfyltOgEndret() && vilkårperiode.tom > revurderFraDato) {
                 "Opphør er et ugyldig vedtaksresultat fordi til og med dato for endret målgruppe er etter revurder fra dato"
             }
         }
         vilkårperioder.aktiviteter.forEach { vilkårperiode ->
-            brukerfeilHvis(vilkårperiode.status == Vilkårstatus.ENDRET && vilkårperiode.tom > revurderFraDato) {
+            brukerfeilHvis(vilkårperiode.erOppfyltOgEndret() && vilkårperiode.tom > revurderFraDato) {
                 "Opphør er et ugyldig vedtaksresultat fordi til og med dato for endret aktivitet er etter revurder fra dato"
             }
         }
         vilkår.forEach { enkeltVilkår ->
-            if (enkeltVilkår.status == VilkårStatus.ENDRET) {
+            if (enkeltVilkår.erOppfyltOgEndret()) {
                 val tom = enkeltVilkår.tom ?: error("Til og med dato er påkrevd for endret vilkår")
                 brukerfeilHvis(tom > revurderFraDato) { "Opphør er et ugyldig vedtaksresultat fordi til og med dato for endret vilkår er etter revurder fra dato" }
             }
         }
+    }
+
+    private fun Vilkårperiode.erOppfyltOgEndret(): Boolean {
+        return (status == Vilkårstatus.ENDRET) && (resultat == ResultatVilkårperiode.OPPFYLT)
+    }
+
+    private fun Vilkår.erOppfyltOgEndret(): Boolean {
+        return (status == VilkårStatus.ENDRET) && (resultat == Vilkårsresultat.OPPFYLT)
     }
 }
