7f1789f98fbe3fe06484e4ab65512490ab0c4439
Nibedita.Pahari <nibedita.pahari@nav.no>
2024-12-03 13:02:30 +0100

    Spotless check
    Co-authored-by: Runar Skagestad <runar.skagestad@nav.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/HarBehandlingUnderArbeidController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/HarBehandlingUnderArbeidController.kt
@@ -3,14 +3,14 @@ package no.nav.tilleggsstonader.sak.ekstern.stønad
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tilleggsstonader.kontrakter.felles.IdentStønadstype
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvisIkke
+import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.EksternApplikasjon
 import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.SikkerhetContext
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.EksternApplikasjon
-import org.springframework.http.HttpStatus
 
 @RestController
 @RequestMapping(
