e5e51e5bd33ce73a9bed80b242c87cf0a5962928
Nibedita.Pahari <nibedita.pahari@nav.no>
2024-12-03 12:36:51 +0100

    legge til tilgangs kontrol på endepunkt
    Co-authored-by: Runar Skagestad <runar.skagestad@nav.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/HarBehandlingUnderArbeidController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/HarBehandlingUnderArbeidController.kt
@@ -2,11 +2,15 @@ package no.nav.tilleggsstonader.sak.ekstern.stønad
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tilleggsstonader.kontrakter.felles.IdentStønadstype
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvisIkke
+import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.SikkerhetContext
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.EksternApplikasjon
+import org.springframework.http.HttpStatus
 
 @RestController
 @RequestMapping(
@@ -20,6 +24,9 @@ class HarBehandlingUnderArbeidController(
     @PostMapping()
     @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
     fun harBehandlingUnderArbeid(@RequestBody identStønadstype: IdentStønadstype): Boolean {
+        feilHvisIkke(SikkerhetContext.kallKommerFra(EksternApplikasjon.SOKNAD_API), HttpStatus.UNAUTHORIZED) {
+            "Kallet utføres ikke av en autorisert klient"
+        }
         return harBehandlingUnderArbeidService.harSøknadUnderBehandling(identStønadstype)
     }
 }
