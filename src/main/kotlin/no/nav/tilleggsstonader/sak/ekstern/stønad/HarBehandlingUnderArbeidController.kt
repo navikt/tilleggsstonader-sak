c0bdb4674070b8e94da3d61a645776cabef3248f
Nibeditanav <nibedita.pahari@nav.no>
2024-12-03 14:28:24 +0100

    Sjekk behandlingstatus fra sak (#507)
     Nytt endepunkt for å sjekke om  søker har behandling under arbeid
    
    ---------
    
    Co-authored-by: Runar Skagestad <runar.skagestad@nav.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/HarBehandlingUnderArbeidController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/HarBehandlingUnderArbeidController.kt
@@ -1,4 +1,4 @@
-package no.nav.tilleggsstonader.sak.migrering.routing
+package no.nav.tilleggsstonader.sak.ekstern.stønad
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tilleggsstonader.kontrakter.felles.IdentStønadstype
@@ -6,24 +6,27 @@ import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvisIkke
 import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.EksternApplikasjon
 import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.SikkerhetContext
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path = ["/api/ekstern/routing-soknad"])
-@ProtectedWithClaims(issuer = "azuread")
-class SøknadRoutingController(
-    private val søknadRoutingService: SøknadRoutingService,
+@RequestMapping(
+    path = ["/api/ekstern/har-behandling"],
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+)
+class HarBehandlingUnderArbeidController(
+    private val harBehandlingUnderArbeidService: HarBehandlingUnderArbeidService,
 ) {
-
     @PostMapping()
     @ProtectedWithClaims(issuer = "azuread", claimMap = ["roles=access_as_application"])
-    fun sjekkRoutingForPerson(@RequestBody request: IdentStønadstype): SøknadRoutingResponse {
+    fun harBehandlingUnderArbeid(@RequestBody identStønadstype: IdentStønadstype): Boolean {
         feilHvisIkke(SikkerhetContext.kallKommerFra(EksternApplikasjon.SOKNAD_API), HttpStatus.UNAUTHORIZED) {
             "Kallet utføres ikke av en autorisert klient"
         }
-        return søknadRoutingService.sjekkRoutingForPerson(request)
+        return harBehandlingUnderArbeidService.harSøknadUnderBehandling(identStønadstype)
     }
 }
