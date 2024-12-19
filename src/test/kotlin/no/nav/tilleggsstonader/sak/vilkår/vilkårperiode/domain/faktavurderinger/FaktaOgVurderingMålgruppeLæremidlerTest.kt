310446210d6330cf1269a7b0fa92d4b7923a79a2
Runar Skagestad <runar.skagestad@nav.no>
2024-12-19 12:58:44 +0100

    🚒 Renamer FaktaOgVurderingLæremidlerTest -> FaktaOgVurderingMålgruppeLæremidlerTest fordi den nå bare handler om målgrupper

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/domain/faktavurderinger/FaktaOgVurderingMålgruppeLæremidlerTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/domain/faktavurderinger/FaktaOgVurderingMålgruppeLæremidlerTest.kt
@@ -7,7 +7,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class FaktaOgVurderingLæremidlerTest {
+class FaktaOgVurderingMålgruppeLæremidlerTest {
 
     @Nested
     inner class UtledningAvResultatBasertPåType {
