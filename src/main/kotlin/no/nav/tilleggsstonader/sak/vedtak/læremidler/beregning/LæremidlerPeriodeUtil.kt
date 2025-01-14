6943633cc144b622bdb0e4d5a8225e75c7dccc93
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-14 16:08:45 +0100

    Metode for å kunne splitte opp flere vedtaksperioder i en og samme løpende måned - Skal kunne innvilge flere vedtaksperioder i en og samme måned - Skal kunne innvilge periode 2 som løper i første løpende måned og i neste

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerPeriodeUtil.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerPeriodeUtil.kt
@@ -2,10 +2,101 @@ package no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning
 
 import no.nav.tilleggsstonader.kontrakter.felles.Periode
 import no.nav.tilleggsstonader.kontrakter.felles.alleDatoer
+import no.nav.tilleggsstonader.kontrakter.felles.sisteDagIÅret
+import no.nav.tilleggsstonader.kontrakter.felles.splitPerÅr
+import no.nav.tilleggsstonader.sak.util.datoEllerNesteMandagHvisLørdagEllerSøndag
 import no.nav.tilleggsstonader.sak.util.lørdagEllerSøndag
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.domain.Vedtaksperiode
 import java.time.LocalDate
 
 object LæremidlerPeriodeUtil {
+
+    fun List<Vedtaksperiode>.grupperVedtaksperioderPerLøpendeMåned(): List<UtbetalingPeriode> = this
+        .sorted()
+        .delVedtaksperiodePerÅr()
+        .fold(listOf<UtbetalingPeriode>()) { acc, vedtaksperiode ->
+            if (acc.isEmpty()) {
+                val nyeUtbetalingsperioder = vedtaksperiode.delTilUtbetalingPerioder()
+                acc + nyeUtbetalingsperioder
+            } else {
+                val håndterNyUtbetalingsperiode = vedtaksperiode.håndterNyUtbetalingsperiode(acc)
+                acc + håndterNyUtbetalingsperiode
+            }
+        }
+        .toList()
+
+    /**
+     * Legger til periode som overlapper med forrige utbetalingsperiode
+     * Returnerer utbetalingsperioder som løper etter forrige utbetalingsperiode
+     */
+    private fun VedtaksperiodeDeltForÅr.håndterNyUtbetalingsperiode(
+        acc: List<UtbetalingPeriode>,
+    ): List<UtbetalingPeriode> {
+        val forrigeUtbetalingsperide = acc.last()
+        this.overlappendeDelMed(forrigeUtbetalingsperide)?.let {
+            forrigeUtbetalingsperide.medVedtaksperiode(it)
+        }
+        return this
+            .delEtterUtbetalingsperiode(forrigeUtbetalingsperide)
+            .delTilUtbetalingPerioder()
+    }
+
+    /**
+     * Splitter en vedtaksperiode i forrige utbetalingsperiode hvis de overlapper
+     */
+    private fun VedtaksperiodeDeltForÅr.overlappendeDelMed(utbetalingPeriode: UtbetalingPeriode): Vedtaksperiode? {
+        return if (this.fom <= utbetalingPeriode.tom) {
+            Vedtaksperiode(
+                fom = utbetalingPeriode.fom,
+                tom = minOf(utbetalingPeriode.tom, this.tom),
+            )
+        } else {
+            null
+        }
+    }
+
+    /**
+     * Splitter vedtaksperiode som løper etter forrige utbetalingsperiode til nye vedtaksperioder
+     */
+    private fun VedtaksperiodeDeltForÅr.delEtterUtbetalingsperiode(
+        utbetalingPeriode: UtbetalingPeriode,
+    ): VedtaksperiodeDeltForÅr =
+        this.copy(fom = maxOf(this.fom, utbetalingPeriode.tom.plusDays(1)))
+
+    /**
+     * tom settes til minOf tom og årets tom for å håndtere at den ikke går over 2 år
+     */
+    private fun VedtaksperiodeDeltForÅr.delTilUtbetalingPerioder(): List<UtbetalingPeriode> {
+        return this.splitPerLøpendeMåneder { fom, tom ->
+            UtbetalingPeriode(
+                fom = fom,
+                tom = minOf(fom.sisteDagenILøpendeMåned(), this.tom.sisteDagIÅret()),
+                utbetalingsdato = this.fom.datoEllerNesteMandagHvisLørdagEllerSøndag(),
+            ).medVedtaksperiode(Vedtaksperiode(fom = fom, tom = tom))
+        }
+    }
+
+    /**
+     * Deler vedtaksperiode i år, for å eks innvilge høst og vår i 2 ulike perioder
+     * Og der vårterminen får en ny sats
+     */
+    data class VedtaksperiodeDeltForÅr(
+        override val fom: LocalDate,
+        override val tom: LocalDate,
+    ) : Periode<LocalDate> {
+        init {
+            require(fom.year == tom.year) {
+                "Kan ikke være 2 ulike år (${fom.year}, ${tom.year}})"
+            }
+        }
+    }
+
+    fun List<Vedtaksperiode>.delVedtaksperiodePerÅr(): List<VedtaksperiodeDeltForÅr> = this
+        .flatMap {
+            it.splitPerÅr { fom, tom ->
+                VedtaksperiodeDeltForÅr(fom, tom)
+            }
+        }
 
     /**
      * Splitter en periode i løpende måneder. Løpende måned er fra dagens dato og en måned frem i tiden.
--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerPeriodeUtilTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerPeriodeUtilTest.kt
@@ -1,8 +1,10 @@
 package no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning
 
 import no.nav.tilleggsstonader.kontrakter.felles.Datoperiode
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning.LæremidlerPeriodeUtil.grupperVedtaksperioderPerLøpendeMåned
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning.LæremidlerPeriodeUtil.sisteDagenILøpendeMåned
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning.LæremidlerPeriodeUtil.splitPerLøpendeMåneder
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.domain.Vedtaksperiode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -90,6 +92,119 @@ class LæremidlerPeriodeUtilTest {
                 )
         }
 
+        @Test
+        fun `skal håndtere vedtaksperiode som løper over 2 år`() {
+            val vedtaksperioder = listOf(
+                Vedtaksperiode(LocalDate.of(2024, 12, 5), LocalDate.of(2025, 1, 4)),
+            )
+            val perioder = vedtaksperioder.grupperVedtaksperioderPerLøpendeMåned()
+            assertThat(perioder).hasSize(2)
+
+            with(perioder[0]) {
+                assertThat(fom).isEqualTo(LocalDate.of(2024, 12, 5))
+                assertThat(tom).isEqualTo(LocalDate.of(2024, 12, 31))
+                assertThat(this.vedtaksperioder)
+                    .containsExactly(Vedtaksperiode(LocalDate.of(2024, 12, 5), LocalDate.of(2024, 12, 31)))
+            }
+            with(perioder[1]) {
+                assertThat(fom).isEqualTo(LocalDate.of(2025, 1, 1))
+                assertThat(tom).isEqualTo(LocalDate.of(2025, 1, 31))
+                assertThat(this.vedtaksperioder)
+                    .containsExactly(Vedtaksperiode(LocalDate.of(2025, 1, 1), LocalDate.of(2025, 1, 4)))
+            }
+        }
+
+        @Nested
+        inner class FlereVedtaksperioderSammeMåned {
+
+            @Test
+            fun `skal håndtere en vedtaksperiode som løper innenfor en løpende måned`() {
+                val vedtaksperioder = listOf(
+                    Vedtaksperiode(LocalDate.of(2024, 1, 5), LocalDate.of(2024, 1, 15)),
+                )
+                val perioder = vedtaksperioder.grupperVedtaksperioderPerLøpendeMåned()
+                assertThat(perioder).hasSize(1)
+                with(perioder.single()) {
+                    assertThat(fom).isEqualTo(LocalDate.of(2024, 1, 5))
+                    assertThat(tom).isEqualTo(LocalDate.of(2024, 2, 4))
+                    assertThat(this.vedtaksperioder).hasSize(1)
+                }
+            }
+
+            @Test
+            fun `skal håndtere to vedtaksperiode som løper i ulike løpende måneder`() {
+                val vedtaksperioder = listOf(
+                    Vedtaksperiode(LocalDate.of(2024, 1, 5), LocalDate.of(2024, 1, 15)),
+                    Vedtaksperiode(LocalDate.of(2024, 2, 7), LocalDate.of(2024, 3, 2)),
+                )
+                val perioder = vedtaksperioder.grupperVedtaksperioderPerLøpendeMåned()
+                assertThat(perioder).hasSize(2)
+                with(perioder[0]) {
+                    assertThat(fom).isEqualTo(LocalDate.of(2024, 1, 5))
+                    assertThat(tom).isEqualTo(LocalDate.of(2024, 2, 4))
+                    assertThat(this.vedtaksperioder).hasSize(1)
+                }
+                with(perioder[1]) {
+                    assertThat(fom).isEqualTo(LocalDate.of(2024, 2, 7))
+                    assertThat(tom).isEqualTo(LocalDate.of(2024, 3, 6))
+                    assertThat(this.vedtaksperioder).hasSize(1)
+                }
+            }
+
+            @Test
+            fun `skal gruppere alle perioder som gjelder januar i januar`() {
+                val vedtaksperioder = listOf(
+                    Vedtaksperiode(FØRSTE_JAN_2024, LocalDate.of(2024, 1, 5)),
+                    Vedtaksperiode(LocalDate.of(2024, 1, 7), LocalDate.of(2024, 1, 7)),
+                )
+                val perioder = vedtaksperioder.grupperVedtaksperioderPerLøpendeMåned()
+                assertThat(perioder).hasSize(1)
+                with(perioder.single()) {
+                    assertThat(fom).isEqualTo(FØRSTE_JAN_2024)
+                    assertThat(tom).isEqualTo(SISTE_JAN_2024)
+                    assertThat(this.vedtaksperioder).hasSize(2)
+                }
+            }
+
+            @Test
+            fun `skal gruppere alle perioder som gjelder fra med 5 januar til 4 februar`() {
+                val vedtaksperioder = listOf(
+                    Vedtaksperiode(LocalDate.of(2024, 1, 5), LocalDate.of(2024, 1, 5)),
+                    Vedtaksperiode(LocalDate.of(2024, 2, 1), LocalDate.of(2024, 2, 4)),
+                )
+                val perioder = vedtaksperioder.grupperVedtaksperioderPerLøpendeMåned()
+                assertThat(perioder).hasSize(1)
+                with(perioder.single()) {
+                    assertThat(fom).isEqualTo(LocalDate.of(2024, 1, 5))
+                    assertThat(tom).isEqualTo(LocalDate.of(2024, 2, 4))
+                    assertThat(this.vedtaksperioder).hasSize(2)
+                }
+            }
+
+            @Test
+            fun `skal splitte periode 2 som løper over 2 vedtaksperioder`() {
+                val vedtaksperioder = listOf(
+                    Vedtaksperiode(LocalDate.of(2024, 1, 5), LocalDate.of(2024, 1, 5)),
+                    Vedtaksperiode(LocalDate.of(2024, 2, 1), LocalDate.of(2024, 2, 28)),
+                )
+                val perioder = vedtaksperioder.grupperVedtaksperioderPerLøpendeMåned()
+                assertThat(perioder).hasSize(2)
+                with(perioder[0]) {
+                    assertThat(fom).isEqualTo(LocalDate.of(2024, 1, 5))
+                    assertThat(tom).isEqualTo(LocalDate.of(2024, 2, 4))
+                    assertThat(this.utbetalingsdato).isEqualTo(LocalDate.of(2024, 1, 5))
+                    assertThat(this.vedtaksperioder).hasSize(2)
+                }
+                with(perioder[1]) {
+                    assertThat(fom).isEqualTo(LocalDate.of(2024, 2, 5))
+                    assertThat(tom).isEqualTo(LocalDate.of(2024, 3, 4))
+                    // Utbetalingsdato for periode 2 blir i neste måned fordi det blir en ny vedtaksperiode
+                    assertThat(this.utbetalingsdato).isEqualTo(LocalDate.of(2024, 2, 5))
+                    assertThat(this.vedtaksperioder).hasSize(1)
+                }
+            }
+        }
+
         @Nested
         inner class SammeMåned {
 
@@ -97,9 +212,7 @@ class LæremidlerPeriodeUtilTest {
             fun `periode som kun gjelder rød dag men ikke helg får innvilget`() {
                 val datoperiode = Datoperiode(FØRSTE_JAN_2024, FØRSTE_JAN_2024)
                 assertThat(datoperiode.splitPerLøpendeMåneder { fom, tom -> Datoperiode(fom = fom, tom = tom) })
-                    .containsExactly(
-                        datoperiode,
-                    )
+                    .containsExactly(datoperiode)
             }
 
             // Arena tar ikke med helgdager når man innvilger for lengre perioder, men tar med hvis man kun innvilger en kort periode
