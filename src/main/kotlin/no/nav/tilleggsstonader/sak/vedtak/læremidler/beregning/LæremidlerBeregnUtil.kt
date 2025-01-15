ededcf73661cdff6dc2d15882a72a5862fe4ce89
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-15 08:09:42 +0100

    Metode for å kunne håndtere og splitte opp flere vedtaksperioder i en og samme løpende måned
    - Skal kunne innvilge flere vedtaksperioder i en og samme måned
    - Skal kunne innvilge periode 2 som løper i første løpende måned og i neste

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerBeregnUtil.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerBeregnUtil.kt
@@ -1,8 +1,10 @@
 package no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning
 
 import no.nav.tilleggsstonader.kontrakter.felles.Periode
+import no.nav.tilleggsstonader.kontrakter.felles.sisteDagIÅret
 import no.nav.tilleggsstonader.kontrakter.felles.splitPerÅr
 import no.nav.tilleggsstonader.sak.util.datoEllerNesteMandagHvisLørdagEllerSøndag
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning.LæremidlerSplitPerLøpendeMånedUtil.sisteDagenILøpendeMåned
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning.LæremidlerSplitPerLøpendeMånedUtil.splitPerLøpendeMåneder
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.domain.Vedtaksperiode
 import java.time.LocalDate
@@ -25,4 +27,91 @@ object LæremidlerBeregnUtil {
                     )
                 }
             }
+
+    fun List<Vedtaksperiode>.grupperVedtaksperioderPerLøpendeMåned(): List<GrunnlagForUtbetalingPeriode> = this
+        .sorted()
+        .delVedtaksperiodePerÅr()
+        .fold(listOf<GrunnlagForUtbetalingPeriode>()) { acc, vedtaksperiode ->
+            if (acc.isEmpty()) {
+                val nyeUtbetalingsperioder = vedtaksperiode.delTilUtbetalingPerioder()
+                acc + nyeUtbetalingsperioder
+            } else {
+                val håndterNyUtbetalingsperiode = vedtaksperiode.håndterNyUtbetalingsperiode(acc)
+                acc + håndterNyUtbetalingsperiode
+            }
+        }
+        .toList()
+
+    /**
+     * Legger til periode som overlapper med forrige utbetalingsperiode
+     * Returnerer utbetalingsperioder som løper etter forrige utbetalingsperiode
+     */
+    private fun VedtaksperiodeDeltForÅr.håndterNyUtbetalingsperiode(
+        acc: List<GrunnlagForUtbetalingPeriode>,
+    ): List<GrunnlagForUtbetalingPeriode> {
+        val forrigeUtbetalingsperide = acc.last()
+        this.overlappendeDelMed(forrigeUtbetalingsperide)?.let {
+            forrigeUtbetalingsperide.medVedtaksperiode(it)
+        }
+        return this
+            .delEtterUtbetalingsperiode(forrigeUtbetalingsperide)
+            .delTilUtbetalingPerioder()
+    }
+
+    /**
+     * Splitter en vedtaksperiode i forrige utbetalingsperiode hvis de overlapper
+     */
+    private fun VedtaksperiodeDeltForÅr.overlappendeDelMed(utbetalingPeriode: GrunnlagForUtbetalingPeriode): Vedtaksperiode? {
+        return if (this.fom <= utbetalingPeriode.tom) {
+            Vedtaksperiode(
+                fom = utbetalingPeriode.fom,
+                tom = minOf(utbetalingPeriode.tom, this.tom),
+            )
+        } else {
+            null
+        }
+    }
+
+    /**
+     * Splitter vedtaksperiode som løper etter forrige utbetalingsperiode til nye vedtaksperioder
+     */
+    private fun VedtaksperiodeDeltForÅr.delEtterUtbetalingsperiode(
+        utbetalingPeriode: GrunnlagForUtbetalingPeriode,
+    ): VedtaksperiodeDeltForÅr =
+        this.copy(fom = maxOf(this.fom, utbetalingPeriode.tom.plusDays(1)))
+
+    /**
+     * tom settes til minOf tom og årets tom for å håndtere at den ikke går over 2 år
+     */
+    private fun VedtaksperiodeDeltForÅr.delTilUtbetalingPerioder(): List<GrunnlagForUtbetalingPeriode> {
+        return this.splitPerLøpendeMåneder { fom, tom ->
+            GrunnlagForUtbetalingPeriode(
+                fom = fom,
+                tom = minOf(fom.sisteDagenILøpendeMåned(), this.tom.sisteDagIÅret()),
+                utbetalingsdato = this.fom.datoEllerNesteMandagHvisLørdagEllerSøndag(),
+            ).medVedtaksperiode(Vedtaksperiode(fom = fom, tom = tom))
+        }
+    }
+
+    /**
+     * Deler vedtaksperiode i år, for å eks innvilge høst og vår i 2 ulike perioder
+     * Og der vårterminen får en ny sats
+     */
+    data class VedtaksperiodeDeltForÅr(
+        override val fom: LocalDate,
+        override val tom: LocalDate,
+    ) : Periode<LocalDate> {
+        init {
+            require(fom.year == tom.year) {
+                "Kan ikke være 2 ulike år (${fom.year}, ${tom.year}})"
+            }
+        }
+    }
+
+    fun List<Vedtaksperiode>.delVedtaksperiodePerÅr(): List<VedtaksperiodeDeltForÅr> = this
+        .flatMap {
+            it.splitPerÅr { fom, tom ->
+                VedtaksperiodeDeltForÅr(fom, tom)
+            }
+        }
 }
--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/UtbetalingPeriode.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/UtbetalingPeriode.kt
@@ -6,10 +6,20 @@ import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
 import no.nav.tilleggsstonader.sak.util.formatertPeriodeNorskFormat
 import no.nav.tilleggsstonader.sak.vedtak.domain.StønadsperiodeBeregningsgrunnlag
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.domain.Studienivå
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.domain.Vedtaksperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
 import java.time.LocalDate
 
+/**
+ * Utbetalingperiode for løpende måned
+ * Eks 5jan - 4feb
+ *
+ * @param utbetalingsdato utbetalingsdato for når en utbetalingsperiode skal utbetales.
+ * Eks hvis man innvilger jan-juni så skal man utbetale hele beløpet for fom i første utbetalingsperioden,
+ * dvs 5 jan i tidligere eksemplet
+ *
+ */
 data class UtbetalingPeriode(
     override val fom: LocalDate,
     override val tom: LocalDate,
@@ -29,8 +39,27 @@ data class GrunnlagForUtbetalingPeriode(
     override val tom: LocalDate,
     val utbetalingsdato: LocalDate,
 ) : Periode<LocalDate> {
+
+    /**
+     * backing property for vedtaksperioder.
+     * Inneholder de vedtaksperioder som er innvilget innenfor en UtbetalingPeriode
+     * Implementert som private backing property for å ikke kunne legge til perioder direkte til listen uten å validere den
+     */
+    private val _vedtaksperioder: MutableList<Vedtaksperiode> = mutableListOf()
+
+    val vedtaksperioder: List<Vedtaksperiode> get() = _vedtaksperioder
+
     init {
         validatePeriode()
+        _vedtaksperioder.forEach { this.inneholder(it) }
+    }
+
+    fun medVedtaksperiode(vedtaksperiode: Vedtaksperiode): GrunnlagForUtbetalingPeriode {
+        require(inneholder(vedtaksperiode)) {
+            "Vedtaksperiode(${vedtaksperiode.formatertPeriodeNorskFormat()}) kan ikke gå utenfor utbetalingsperiode(${this.formatertPeriodeNorskFormat()})"
+        }
+        _vedtaksperioder.add(vedtaksperiode)
+        return this
     }
 
     fun tilUtbetalingPeriode(
@@ -80,9 +109,4 @@ data class GrunnlagForUtbetalingPeriode(
 
         return relevanteStønadsperioderForPeriode.single()
     }
-
-    private class MålgruppeOgAktivitet(
-        val målgruppe: MålgruppeType,
-        val aktivitet: AktivitetLæremidlerBeregningGrunnlag,
-    )
 }
