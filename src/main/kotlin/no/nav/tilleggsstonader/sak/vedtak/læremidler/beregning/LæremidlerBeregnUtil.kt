3b1b338df1f02e2c745933ca9012a4bd357dc041
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-15 09:10:16 +0100

    Fjerner metode som er erstattet av grupperVedtaksperioderPerLøpendeMåned som håndterer flere vedtaksperioder innenfor en måned

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerBeregnUtil.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerBeregnUtil.kt
@@ -11,23 +11,6 @@ import java.time.LocalDate
 
 object LæremidlerBeregnUtil {
 
-    /**
-     * Splitter vedtaksperiode per år. Sånn at man får en periode for høstterminen og en for vårterminen
-     * Dette for å kunne lage en periode for våren som ikke utbetales direkte, men når satsen for det nye året er satt.
-     * Eks 2024-08-15 - 2025-06-20 blir 2024-08-15 - 2024-12-31 og 2025-01-01 - 2025-06-20
-     */
-    fun Periode<LocalDate>.delTilUtbetalingsPerioder(): List<GrunnlagForUtbetalingPeriode> =
-        splitPerÅr { fom, tom -> Vedtaksperiode(fom, tom) }
-            .flatMap { periode ->
-                periode.splitPerLøpendeMåneder { fom, tom ->
-                    GrunnlagForUtbetalingPeriode(
-                        fom = fom,
-                        tom = tom,
-                        utbetalingsdato = periode.fom.datoEllerNesteMandagHvisLørdagEllerSøndag(),
-                    )
-                }
-            }
-
     fun List<Vedtaksperiode>.grupperVedtaksperioderPerLøpendeMåned(): List<GrunnlagForUtbetalingPeriode> = this
         .sorted()
         .delVedtaksperiodePerÅr()
