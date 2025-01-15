ccca7d723d06616bbdd5bf2ee2d2a603c1df1136
Runar Skagestad <runar.skagestad@nav.no>
2025-01-15 11:19:08 +0100

    🗃️️ Lager tabell for revidert versjon av vedtaksstatistikk
    Co-authored-by: Sigmund Berbom <sigmund.bernhard.berbom@nav.no>

---
+++ src/main/resources/db/migration/V60__vedtaksstatistikk_v2.sql
@@ -0,0 +1,23 @@
+CREATE TABLE vedtaksstatistikk_v2
+(
+    id                     UUID PRIMARY KEY,
+    fagsak_id              UUID      NOT NULL,
+    behandling_id          UUID      NOT NULL,
+    ekstern_fagsak_id      VARCHAR   NOT NULL,
+    ekstern_behandling_id  VARCHAR   NOT NULL,
+    relatert_behandling_id VARCHAR,
+    adressebeskyttelse     VARCHAR,
+    tidspunkt_vedtak       TIMESTAMP NOT NULL,
+    person                 VARCHAR   NOT NULL,
+    behandling_type        VARCHAR   NOT NULL,
+    behandling_arsak       VARCHAR   NOT NULL,
+    vedtak_resultat        VARCHAR   NOT NULL,
+    vedtaksperioder        JSON      NOT NULL,
+    utbetalinger           JSON      NOT NULL,
+    stonadstype            VARCHAR   NOT NULL,
+    krav_mottatt           DATE,
+    arsaker_avslag         JSON,
+    arsaker_opphor         JSON,
+    opprettet_tid          TIMESTAMP NOT NULL,
+    endretTid              TIMESTAMP NOT NULL
+);
