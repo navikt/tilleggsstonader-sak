9c48dbfaac597541c6be153bfe7b4b008c34bf76
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-15 06:42:33 +0100

    Flytter inn tester i nested inner class for bedre gruppering før andre tester legges inn

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerBeregningUtilTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/beregning/LæremidlerBeregningUtilTest.kt
@@ -2,24 +2,30 @@ package no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning
 
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.beregning.LæremidlerBeregningUtil.beregnBeløp
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class LæremidlerBeregningUtilTest {
-    @Test
-    fun `finn beløp for stuieprosent 100 prosent`() {
-        val beløp = beregnBeløp(875, 100)
-        assertThat(beløp).isEqualTo(875)
-    }
 
-    @Test
-    fun `finn beløp for stuieprosent 50 prosent`() {
-        val beløp = beregnBeløp(874, 50)
-        assertThat(beløp).isEqualTo(437)
-    }
+    @Nested
+    inner class BeregnBeløp {
 
-    @Test
-    fun `finn beløp for stuieprosent 50 runder opp prosent`() {
-        val beløp = beregnBeløp(875, 50)
-        assertThat(beløp).isEqualTo(438)
+        @Test
+        fun `finn beløp for stuieprosent 100 prosent`() {
+            val beløp = beregnBeløp(875, 100)
+            assertThat(beløp).isEqualTo(875)
+        }
+
+        @Test
+        fun `finn beløp for stuieprosent 50 prosent`() {
+            val beløp = beregnBeløp(874, 50)
+            assertThat(beløp).isEqualTo(437)
+        }
+
+        @Test
+        fun `finn beløp for stuieprosent 50 runder opp prosent`() {
+            val beløp = beregnBeløp(875, 50)
+            assertThat(beløp).isEqualTo(438)
+        }
     }
 }
