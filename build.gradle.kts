8f37ba5c4ce6d725410880854fa84625d0508e95
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-04 13:29:07 +0100

    Erstatter funksjon antallMånederBakITiden med felt på stønadstype

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.io.ByteArrayOutputStream
 val javaVersion = JavaLanguageVersion.of(21)
 val familieProsesseringVersion = "2.20241112093526_694e258"
 val tilleggsstønaderLibsVersion = "2025.01.21-10.26.933160d62458"
-val tilleggsstønaderKontrakterVersion = "2025.01.28-19.54.057d752b4db8"
+val tilleggsstønaderKontrakterVersion = "2025.02.04-13.24.4713c49b1c78"
 val tokenSupportVersion = "5.0.11"
 val wiremockVersion = "3.9.2"
 val mockkVersion = "1.13.12"
