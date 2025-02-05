e9c96b6ccca3719fec97e76ab62950bce7ca33bd
Sigmund Berbom <sberbom@gmail.com>
2025-02-05 10:38:43 +0100

    🏷️ Legg til VedtaksperiodeDto som skal være felles for alle stønader

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dto/VedtaksperiodeDto.kt
@@ -0,0 +1,40 @@
+package no.nav.tilleggsstonader.sak.vedtak.dto
+
+import no.nav.tilleggsstonader.kontrakter.felles.Periode
+import no.nav.tilleggsstonader.kontrakter.felles.splitPerMåned
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.beregning.PeriodeMedDager
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.beregning.TilsynBeregningUtil.antallDagerIPeriodeInklusiv
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.beregning.TilsynBeregningUtil.splitPerUke
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.beregning.Uke
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
+import java.time.LocalDate
+import java.time.YearMonth
+
+data class VedtaksperiodeDto(
+    override val fom: LocalDate,
+    override val tom: LocalDate,
+    val målgruppeType: MålgruppeType,
+    val aktivitetType: AktivitetType,
+) : Periode<LocalDate> {
+    /**
+     * Splitter en vedtaksperioder opp i uker (kun hverdager inkludert)
+     * Antall dager i uken er oppad begrenset til antall dager i vedtaksperioden som er innenfor uken
+     */
+    fun tilUke(): Map<Uke, PeriodeMedDager> =
+        this.splitPerUke { fom, tom ->
+            antallDagerIPeriodeInklusiv(fom, tom)
+        }
+}
+
+fun List<VedtaksperiodeDto>.tilÅrMåned(): Map<YearMonth, List<VedtaksperiodeDto>> =
+    this
+        .flatMap { vedtaksperiode ->
+            vedtaksperiode.splitPerMåned { måned, periode ->
+                periode.copy(
+                    fom = maxOf(periode.fom, måned.atDay(1)),
+                    tom = minOf(periode.tom, måned.atEndOfMonth()),
+                )
+            }
+        }.groupBy({ it.first }, { it.second })
+        .mapValues { it.value.sorted() }
