e7057665172ac207f500fd7f066ada277d915c42
Runar Skagestad <runar.skagestad@nav.no>
2025-02-07 11:50:17 +0100

    🚚 Flytt logikk for å opprette status til eget objekt, for enklere lesbarhet og testbarhet
    Co-authored-by: Nibedita Pehari <nibedita.pehari@nav.no>

---
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/VedtaksperiodeStatusMapperTest.kt
@@ -0,0 +1,12 @@
+// package no.nav.tilleggsstonader.sak.vedtak.læremidler
+//
+// import org.junit.jupiter.api.Assertions.*
+//
+// class VedtaksperiodeStatusMapperTest {
+//    @Test
+//    fun `test`() {
+//        VedtaksperiodeStatusMapper.settStatusPåVedtaksperioder(
+//            vedtaksperioder =
+//        )
+//    }
+// }
