47cfc2ba53389255578a181ef4a31dd93c56d016
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-11 14:42:39 +0100

    GjelderFraOgMed skal avkortes fra og med revurder-fra datoet for å vise riktig dato vedtaket gjelder fra i brevet

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/dto/InnvilgelseLæremidlerDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/dto/InnvilgelseLæremidlerDto.kt
@@ -3,6 +3,12 @@ package no.nav.tilleggsstonader.sak.vedtak.læremidler.dto
 import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
 import java.time.LocalDate
 
+/**
+ * @param vedtaksperioder viser alle vedtaksperioder, inkl vedtaksperioder før revurder-fra dato
+ * @param beregningsresultat viser beregningsresultatet fra og med revurder-fra-dato
+ * Beregningsresultatet kan være en del av en løpende måned. Eks 1jan-31jan med revurder-fra 15 gir 15jan-31jan
+ * @param gjelderFraOgMed gjelder fra og med revurder-fra-dato, brukes til brevet for å vise fra når vedtaket gjelder fra
+ */
 data class InnvilgelseLæremidlerResponse(
     val vedtaksperioder: List<VedtaksperiodeLæremidlerDto>,
     val beregningsresultat: BeregningsresultatLæremidlerDto,
