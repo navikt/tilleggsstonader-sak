b3e7b7060fc6b3c84fa7d1343186aef6b25da6d0
Runar Skagestad <runar.skagestad@nav.no>
2025-02-24 15:50:59 +0100

    🗃️ Lager admin-endepunkt for å sette vedtaksperioder på eksisterende tilsyn barn-vedtak
    Co-authored-by: Sigmund Berbom <sigmund.bernhard.berbom@nav.no>

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/barnetilsyn/VedtaksperiodeMigreringsController.kt
@@ -0,0 +1,102 @@
+package no.nav.tilleggsstonader.sak.vedtak.barnetilsyn
+
+import no.nav.security.token.support.core.api.Unprotected
+import no.nav.tilleggsstonader.kontrakter.felles.mergeSammenhengende
+import no.nav.tilleggsstonader.sak.vedtak.VedtakRepository
+import no.nav.tilleggsstonader.sak.vedtak.VedtakService
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.domain.BeregningsresultatForMåned
+import no.nav.tilleggsstonader.sak.vedtak.domain.GeneriskVedtak
+import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseTilsynBarn
+import no.nav.tilleggsstonader.sak.vedtak.domain.OpphørTilsynBarn
+import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/admin/vedtak/migrer")
+@Unprotected
+class VedtaksperiodeMigreringsController(
+    val vedtakservice: VedtakService,
+    val vedtakRepository: VedtakRepository,
+) {
+    @GetMapping()
+    fun migrer() {
+        val innvilgelseTilsynBarn = hentInnvilgelser()
+        val opphørTilsynBarn = hentOpphør()
+
+        innvilgelseTilsynBarn.forEach {
+            if (it.data.vedtaksperioder == null) {
+                val beregningsresultat = it.data.beregningsresultat
+                val vedtaksperioder = mapTilVedtaksperiode(beregningsresultat.perioder)
+
+                val nyttVedtak = it.copy(data = it.data.copy(vedtaksperioder = vedtaksperioder))
+
+                vedtakRepository.update(nyttVedtak)
+            }
+        }
+
+        opphørTilsynBarn.forEach {
+            if (it.data.vedtaksperioder == null) {
+                val beregningsresultat = it.data.beregningsresultat
+                val vedtaksperioder = mapTilVedtaksperiode(beregningsresultat.perioder)
+
+                val nyttVedtak = it.copy(data = it.data.copy(vedtaksperioder = vedtaksperioder))
+
+                vedtakRepository.update(nyttVedtak)
+            }
+        }
+    }
+
+    private fun hentInnvilgelser(): List<GeneriskVedtak<InnvilgelseTilsynBarn>> {
+        val innvilgelseTilsynBarn =
+            vedtakRepository
+                .findAll()
+                .filter { it.data is InnvilgelseTilsynBarn } as List<GeneriskVedtak<InnvilgelseTilsynBarn>>
+        return innvilgelseTilsynBarn
+    }
+
+    private fun hentOpphør(): List<GeneriskVedtak<OpphørTilsynBarn>> {
+        val innvilgelseTilsynBarn =
+            vedtakRepository
+                .findAll()
+                .filter { it.data is OpphørTilsynBarn } as List<GeneriskVedtak<OpphørTilsynBarn>>
+        return innvilgelseTilsynBarn
+    }
+}
+
+fun Vedtaksperiode.erLikOgPåfølgesAv(other: Vedtaksperiode): Boolean {
+    val erLik =
+        this.aktivitet == other.aktivitet &&
+            this.målgruppe == other.målgruppe
+    val påfølgesAv = this.tom.plusDays(1) == other.fom
+    return erLik && påfølgesAv
+}
+
+fun mapTilVedtaksperiode(beregningsresultat: List<BeregningsresultatForMåned>): List<Vedtaksperiode> =
+    beregningsresultat
+        .flatMap { tilVedtaksperioder(it) }
+        .sorted()
+        .mergeSammenhengende(
+            skalMerges = { s1, s2 -> s1.erLikOgPåfølgesAv(s2) },
+            merge = { s1: Vedtaksperiode, s2: Vedtaksperiode ->
+                s1.copy(
+                    fom = minOf(s1.fom, s2.fom),
+                    tom = maxOf(s1.tom, s2.tom),
+                )
+            },
+        )
+
+private fun tilVedtaksperioder(beregningsresultat: BeregningsresultatForMåned) =
+    beregningsresultat.grunnlag.stønadsperioderGrunnlag
+        .map { it.stønadsperiode }
+        .map { vedtaksperiode ->
+            Vedtaksperiode(
+                id = UUID.randomUUID(),
+                fom = vedtaksperiode.fom,
+                tom = vedtaksperiode.tom,
+                målgruppe = vedtaksperiode.målgruppe,
+                vedtaksperiode.aktivitet,
+            )
+        }
