40bc99aafe74681758b28eab2ba507308f4f1e69
eirikvilpponen <eirik.vilpponen@nav.no>
2025-02-27 10:58:10 +0100

    legger til ny unit test for AldersvilkårVurderingTest

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/AldersvilkårVurderingTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/vilkårperiode/AldersvilkårVurderingTest.kt
@@ -3,18 +3,29 @@ package no.nav.tilleggsstonader.sak.vilkår.vilkårperiode
 import no.nav.tilleggsstonader.sak.util.GrunnlagsdataUtil.grunnlagsdataDomain
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.AldersvilkårVurdering.vurderAldersvilkår
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.dummyVilkårperiodeMålgruppe
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.faktavurderinger.SvarJaNei
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class AldersvilkårVurderingTest {
     @Test
-    fun `vurdering skal være riktig`() {
+    fun `gyldig vurdering skal gi svar JA`() {
         val målgruppe = dummyVilkårperiodeMålgruppe()
         val grunnlagsdata = grunnlagsdataDomain()
 
         vurderAldersvilkår(målgruppe, grunnlagsdata).also {
             assertEquals(SvarJaNei.JA, it)
+        }
+    }
+
+    @Test
+    fun `OVERGANGSSTØNAD gi svar JA_IMPLISITT`() {
+        val målgruppe = dummyVilkårperiodeMålgruppe().copy(type = MålgruppeType.OVERGANGSSTØNAD)
+        val grunnlagsdata = grunnlagsdataDomain()
+
+        vurderAldersvilkår(målgruppe, grunnlagsdata).also {
+            assertEquals(SvarJaNei.JA_IMPLISITT, it)
         }
     }
 }
