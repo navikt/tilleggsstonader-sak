dc2cbfb06629f3d3cb699dbb0357bb4fe45dd705
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-28 12:01:46 +0100

    Legger til integrajsonstester for vedtak for læremidler

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/LæremidlerBeregnYtelseStegTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/læremidler/LæremidlerBeregnYtelseStegTest.kt
@@ -2,24 +2,38 @@ package no.nav.tilleggsstonader.sak.vedtak.læremidler
 
 import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
 import no.nav.tilleggsstonader.sak.IntegrationTest
+import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.Satstype
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.StatusIverksetting
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TilkjentYtelseRepository
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TypeAndel
+import no.nav.tilleggsstonader.sak.util.Applikasjonsversjon
 import no.nav.tilleggsstonader.sak.util.behandling
 import no.nav.tilleggsstonader.sak.util.fagsak
+import no.nav.tilleggsstonader.sak.util.saksbehandling
 import no.nav.tilleggsstonader.sak.util.stønadsperiode
+import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
 import no.nav.tilleggsstonader.sak.vedtak.VedtakRepository
+import no.nav.tilleggsstonader.sak.vedtak.domain.AvslagLæremidler
+import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseLæremidler
+import no.nav.tilleggsstonader.sak.vedtak.domain.OpphørLæremidler
+import no.nav.tilleggsstonader.sak.vedtak.domain.VedtakUtil.withTypeOrThrow
+import no.nav.tilleggsstonader.sak.vedtak.domain.ÅrsakAvslag
+import no.nav.tilleggsstonader.sak.vedtak.domain.ÅrsakOpphør
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.dto.AvslagLæremidlerDto
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.dto.InnvilgelseLæremidlerRequest
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.dto.OpphørLæremidlerRequest
 import no.nav.tilleggsstonader.sak.vedtak.læremidler.dto.VedtaksperiodeLæremidlerDto
 import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.domain.StønadsperiodeRepository
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.aktivitet
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.faktaOgVurderingAktivitetLæremidler
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.målgruppe
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.VilkårperiodeRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.LocalDate
@@ -39,11 +53,107 @@ class LæremidlerBeregnYtelseStegTest(
 ) : IntegrationTest() {
     val fagsak = fagsak(stønadstype = Stønadstype.LÆREMIDLER)
     val behandling = behandling(fagsak = fagsak)
+    val saksbehandling = saksbehandling(behandling = behandling, fagsak = fagsak)
+
+    final val fom = LocalDate.of(2025, 1, 1)
+    final val tom = LocalDate.of(2025, 4, 30)
+
+    val stønadsperiode = stønadsperiode(behandlingId = behandling.id, fom = fom, tom = tom)
+    val aktivitet =
+        aktivitet(behandling.id, fom = fom, tom = tom, faktaOgVurdering = faktaOgVurderingAktivitetLæremidler())
+    val målgruppe = målgruppe(behandling.id, fom = fom, tom = tom)
 
     @BeforeEach
     fun setUp() {
         testoppsettService.lagreFagsak(fagsak)
         testoppsettService.lagre(behandling, opprettGrunnlagsdata = false)
+    }
+
+    @Nested
+    inner class Innvilgelse {
+        @Test
+        fun `skal lagre vedtak`() {
+            val vedtaksperiode = VedtaksperiodeLæremidlerDto(id = UUID.randomUUID(), fom = fom, tom = tom)
+            val innvilgelse = InnvilgelseLæremidlerRequest(vedtaksperioder = listOf(vedtaksperiode))
+
+            stønadsperiodeRepository.insert(stønadsperiode)
+            vilkårperiodeRepository.insert(aktivitet)
+            vilkårperiodeRepository.insert(målgruppe)
+
+            steg.utførSteg(saksbehandling, innvilgelse)
+
+            val vedtak = repository.findByIdOrThrow(saksbehandling.id).withTypeOrThrow<InnvilgelseLæremidler>()
+            assertThat(vedtak.behandlingId).isEqualTo(saksbehandling.id)
+            assertThat(vedtak.type).isEqualTo(TypeVedtak.INNVILGELSE)
+            with(vedtak.data.vedtaksperioder.single()) {
+                assertThat(this.fom).isEqualTo(fom)
+                assertThat(this.tom).isEqualTo(tom)
+            }
+            assertThat(vedtak.gitVersjon).isEqualTo(Applikasjonsversjon.versjon)
+        }
+    }
+
+    @Nested
+    inner class Opphør {
+        @Test
+        fun `skal lagre vedtak`() {
+            val vedtaksperiode = VedtaksperiodeLæremidlerDto(id = UUID.randomUUID(), fom = fom, tom = tom)
+            val innvilgelse = InnvilgelseLæremidlerRequest(vedtaksperioder = listOf(vedtaksperiode))
+
+            vilkårperiodeRepository.insertAll(listOf(målgruppe, aktivitet))
+            stønadsperiodeRepository.insert(stønadsperiode)
+
+            steg.utførSteg(saksbehandling, innvilgelse)
+
+            testoppsettService.ferdigstillBehandling(behandling = behandling)
+            val behandlingForOpphør =
+                testoppsettService
+                    .opprettRevurdering(
+                        revurderFra = LocalDate.of(2025, 2, 1),
+                        forrigeBehandling = behandling,
+                        fagsak = fagsak,
+                    ).let { testoppsettService.hentSaksbehandling(it.id) }
+
+            val opphør =
+                OpphørLæremidlerRequest(
+                    årsakerOpphør = listOf(ÅrsakOpphør.ANNET),
+                    begrunnelse = "en begrunnelse",
+                )
+            steg.utførSteg(behandlingForOpphør, opphør)
+
+            val vedtak = repository.findByIdOrThrow(behandlingForOpphør.id).withTypeOrThrow<OpphørLæremidler>()
+            assertThat(vedtak.behandlingId).isEqualTo(behandlingForOpphør.id)
+            assertThat(vedtak.type).isEqualTo(TypeVedtak.OPPHØR)
+            with(vedtak.data.vedtaksperioder.single()) {
+                assertThat(this.fom).isEqualTo(fom)
+                assertThat(this.tom).isEqualTo(LocalDate.of(2025, 1, 31))
+            }
+            assertThat(vedtak.gitVersjon).isEqualTo(Applikasjonsversjon.versjon)
+        }
+    }
+
+    @Nested
+    inner class Avslag {
+        @Test
+        fun `skal lagre vedtak`() {
+            val request =
+                AvslagLæremidlerDto(
+                    årsakerAvslag = listOf(ÅrsakAvslag.ANNET),
+                    begrunnelse = "en begrunnelse",
+                )
+            steg.utførSteg(saksbehandling, request)
+
+            val vedtak = repository.findByIdOrThrow(saksbehandling.id).withTypeOrThrow<AvslagLæremidler>()
+            assertThat(vedtak.behandlingId).isEqualTo(saksbehandling.id)
+            assertThat(vedtak.type).isEqualTo(TypeVedtak.AVSLAG)
+            assertThat(vedtak.gitVersjon).isEqualTo(Applikasjonsversjon.versjon)
+            val forventetAvslag =
+                AvslagLæremidler(
+                    årsaker = request.årsakerAvslag,
+                    begrunnelse = request.begrunnelse,
+                )
+            assertThat(vedtak.data).isEqualTo(forventetAvslag)
+        }
     }
 
     // Ved ny sats må man oppdatere datoer her
