2b4b04f4b0f9844ed5fcb9b01e00f0ed621f6ff3
Sigmund Berbom <sberbom@gmail.com>
2025-03-05 15:02:35 +0100

    🗃️ Oppdater database migrering script for å unngå å endre fritekst i begrunnelse for opphør

--- src/main/resources/db/migration/V68__bruk_vedtaksperioder_i_beregningsgrunnlag_tilsyn_barn.sql
+++ src/main/resources/db/migration/V68__bruk_vedtaksperioder_i_beregningsgrunnlag_tilsyn_barn.sql
@@ -1,6 +1,10 @@
 UPDATE vedtak
-SET data = replace(
-        replace(data::text, '"stønadsperioderGrunnlag"', '"vedtaksperiodeGrunnlag"'),
-        '"stønadsperiode"', '"vedtaksperiode"'
-           )::jsonb
+SET data = jsonb_set(
+        data,
+        '{beregningsresultat}',
+        replace(
+                replace(data ->> 'beregningsresultat', '"stønadsperioderGrunnlag"', '"vedtaksperiodeGrunnlag"'),
+                '"stønadsperiode"', '"vedtaksperiode"'
+        )::jsonb
+           )
 WHERE data ->> 'type' IN ('INNVILGELSE_TILSYN_BARN', 'OPPHØR_TILSYN_BARN');
