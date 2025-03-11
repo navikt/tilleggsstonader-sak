b9e3068f94fa6e98e0c8ce338139b9a74bc310c0
Sigmund Berbom <sberbom@gmail.com>
2025-03-11 10:36:06 +0100

    🚩 Fjern `sak.kan-bruke-vedtaksperioder-tilsyn-barn` feature toggle

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/InngangsvilkårSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/InngangsvilkårSteg.kt
@@ -1,6 +1,6 @@
 package no.nav.tilleggsstonader.sak.vilkår
 
-import no.nav.tilleggsstonader.libs.unleash.UnleashService
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
@@ -8,7 +8,6 @@ import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
-import no.nav.tilleggsstonader.sak.infrastruktur.unleash.Toggle
 import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.StønadsperiodeService
 import org.springframework.stereotype.Service
 
@@ -16,10 +15,9 @@ import org.springframework.stereotype.Service
 class InngangsvilkårSteg(
     private val behandlingService: BehandlingService,
     private val stønadsperiodeService: StønadsperiodeService,
-    private val unleashService: UnleashService,
 ) : BehandlingSteg<Void?> {
     override fun validerSteg(saksbehandling: Saksbehandling) {
-        if (!unleashService.isEnabled(Toggle.KAN_BRUKE_VEDTAKSPERIODER_TILSYN_BARN)) {
+        if (saksbehandling.stønadstype == Stønadstype.LÆREMIDLER) {
             stønadsperiodeService.validerStønadsperioder(saksbehandling.id)
         }
         brukerfeilHvis(saksbehandling.type == BehandlingType.REVURDERING && saksbehandling.revurderFra == null) {
--- src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/InngangsvilkårStegTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vilkår/InngangsvilkårStegTest.kt
@@ -4,7 +4,6 @@ import io.mockk.every
 import io.mockk.justRun
 import io.mockk.mockk
 import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
-import no.nav.tilleggsstonader.libs.unleash.UnleashService
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
 import no.nav.tilleggsstonader.sak.util.behandling
@@ -19,13 +18,11 @@ import org.junit.jupiter.api.Test
 class InngangsvilkårStegTest {
     val behandlingService = mockk<BehandlingService>()
     val stønadsperiodeService = mockk<StønadsperiodeService>()
-    val unleashService = mockk<UnleashService>()
 
     val steg =
         InngangsvilkårSteg(
             behandlingService = behandlingService,
             stønadsperiodeService = stønadsperiodeService,
-            unleashService = unleashService,
         )
 
     @BeforeEach
