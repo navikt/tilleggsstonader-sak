b27444ee58edd205955a10b6f80ecca25314a5b4
Runar Skagestad <runar.skagestad@nav.no>
2025-03-11 09:50:14 +0100

    🚚 Flytt VedtaksstatistikkV2MigreringController. Den hadde lagt seg i en mappe utenfor Spring-contexten

--- src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/VedtaksstatistikkV2MigreringController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/VedtaksstatistikkV2MigreringController.kt
@@ -1,6 +1,8 @@
+package no.nav.tilleggsstonader.sak.statistikk.vedtak
+
 import no.nav.familie.prosessering.util.MDCConstants
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.tilleggsstonader.libs.log.SecureLogger.secureLogger
+import no.nav.tilleggsstonader.libs.log.SecureLogger
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandling.BehandlingUtil.sortertEtterVedtakstidspunkt
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingRepository
@@ -8,11 +10,10 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.infrastruktur.felles.TransactionHandler
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
-import no.nav.tilleggsstonader.sak.statistikk.vedtak.VedtaksstatistikkRepositoryV2
-import no.nav.tilleggsstonader.sak.statistikk.vedtak.VedtaksstatistikkService
 import org.jboss.logging.MDC
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -32,7 +33,7 @@ class VedtaksstatistikkV2MigreringController(
     val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping
-    fun migrering() {
+    fun migrering(): ResponseEntity<String> {
         logger.info("Starter migrasjon av aktivitet læremidler i vedtaksstatistikk v2")
 
         val callId = MDC.get(MDCConstants.MDC_CALL_ID)
@@ -43,16 +44,15 @@ class VedtaksstatistikkV2MigreringController(
                     migrerStatistikkData()
                 }
             } catch (e: Exception) {
-                secureLogger.error("Feilet jobb", e)
+                SecureLogger.secureLogger.error("Feilet jobb", e)
             } finally {
                 MDC.remove(MDCConstants.MDC_CALL_ID)
             }
         }
+        return ResponseEntity.ok("Migrering kjører!")
     }
 
     fun migrerStatistikkData() {
-        vedtaksstatistikkRepositoryV2.deleteAll()
-
         val behandinger =
             behandlingRepository
                 .findAll()
@@ -64,6 +64,8 @@ class VedtaksstatistikkV2MigreringController(
             .hentPersonKortBolk(
                 behandinger.map { behandlingService.hentAktivIdent(it.id) },
             )
+
+        vedtaksstatistikkRepositoryV2.deleteAll()
 
         behandinger
             .sortertEtterVedtakstidspunkt()
