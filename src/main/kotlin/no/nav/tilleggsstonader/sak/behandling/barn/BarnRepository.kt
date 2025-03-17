1c927d4dd618dc9cc885ffbfb6118435ae6a6bfa
Johan Blomgren <blommish@users.noreply.github.com>
2025-03-17 13:02:06 +0100

    Lagt til metode for å kunne hente ut barn koblet til en fagsakPerson for å kunne sjekke tilgang til fagsakpersonen i tilfelle det finnes et barn koblet til en person som har en annen foreldre som har strengere tilgang

--- src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/barn/BarnRepository.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/barn/BarnRepository.kt
@@ -2,8 +2,10 @@ package no.nav.tilleggsstonader.sak.behandling.barn
 
 import no.nav.tilleggsstonader.sak.felles.domain.BarnId
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import no.nav.tilleggsstonader.sak.felles.domain.FagsakPersonId
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.InsertUpdateRepository
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.RepositoryInterface
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -11,4 +13,15 @@ interface BarnRepository :
     RepositoryInterface<BehandlingBarn, BarnId>,
     InsertUpdateRepository<BehandlingBarn> {
     fun findByBehandlingId(behandlingId: BehandlingId): List<BehandlingBarn>
+
+    @Query(
+        """
+        SELECT bb.ident AS ident
+        FROM behandling_barn bb
+        JOIN behandling b ON bb.behandling_id = b.id
+        JOIN fagsak f ON b.fagsak_id = f.id
+        WHERE f.fagsak_person_id = :fagsakPersonId
+    """,
+    )
+    fun finnIdenterTilFagsakPersonId(fagsakPersonId: FagsakPersonId): Set<String>
 }
