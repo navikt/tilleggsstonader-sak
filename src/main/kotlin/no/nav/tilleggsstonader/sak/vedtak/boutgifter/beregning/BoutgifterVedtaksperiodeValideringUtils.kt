dc110d2603150d3937aae0663d51525bd5e2591c
Runar Skagestad <runar.skagestad@nav.no>
2025-03-20 11:04:17 +0100

    🦺 Valider vedtaksperioder for boutgifter

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/beregning/BoutgifterVedtaksperiodeValideringUtils.kt
@@ -0,0 +1,43 @@
+package no.nav.tilleggsstonader.sak.vedtak.boutgifter.beregning
+
+import no.nav.tilleggsstonader.kontrakter.felles.Datoperiode
+import no.nav.tilleggsstonader.kontrakter.felles.mergeSammenhengende
+import no.nav.tilleggsstonader.kontrakter.felles.overlapperEllerPåfølgesAv
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvisIkke
+import no.nav.tilleggsstonader.sak.vedtak.domain.TypeBoutgift
+import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
+
+object BoutgifterVedtaksperiodeValideringUtils {
+    fun validerUtgiftHeleVedtaksperioden(
+        vedtaksperioder: List<Vedtaksperiode>,
+        utgifter: Map<TypeBoutgift, List<UtgiftBeregningBoutgifter>>,
+    ) {
+        if (vedtaksperioder.isEmpty()) {
+            return
+        }
+        brukerfeilHvisIkke(
+            erUtgiftperiodeSomInneholderVedtaksperiode(
+                vedtaksperioder = vedtaksperioder,
+                utgifter = utgifter,
+            ),
+        ) {
+            "Kan ikke innvilge når det ikke finnes utgifter hele vedtaksperioden"
+        }
+    }
+
+    private fun erUtgiftperiodeSomInneholderVedtaksperiode(
+        vedtaksperioder: List<Vedtaksperiode>,
+        utgifter: Map<TypeBoutgift, List<UtgiftBeregningBoutgifter>>,
+    ): Boolean {
+        val sammenslåtteUtgiftPerioder =
+            utgifter.values
+                .flatMap {
+                    it.map { Datoperiode(fom = it.fom, tom = it.tom) }
+                }.sorted()
+                .mergeSammenhengende { p1, p2 -> p1.overlapperEllerPåfølgesAv(p2) }
+
+        return vedtaksperioder.any { vedtaksperiode ->
+            sammenslåtteUtgiftPerioder.any { utgiftPeriode -> utgiftPeriode.inneholder(vedtaksperiode) }
+        }
+    }
+}
---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/beregning/BoutgifterVedtaksperiodeValidingerService.kt
@@ -0,0 +1,78 @@
+package no.nav.tilleggsstonader.sak.vedtak.boutgifter.beregning
+
+import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
+import no.nav.tilleggsstonader.sak.opplysninger.grunnlag.GrunnlagsdataService
+import no.nav.tilleggsstonader.sak.vedtak.TypeVedtak
+import no.nav.tilleggsstonader.sak.vedtak.VedtakRepository
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksperiodeValideringUtils.validerAtVedtaksperioderIkkeOverlapperMedVilkårPeriodeUtenRett
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksperiodeValideringUtils.validerEnkeltperiode
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksperiodeValideringUtils.validerIngenEndringerFørRevurderFra
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksperiodeValideringUtils.validerIngenOverlappMellomVedtaksperioder
+import no.nav.tilleggsstonader.sak.vedtak.VedtaksperiodeValideringUtils.validerVedtaksperioderEksisterer
+import no.nav.tilleggsstonader.sak.vedtak.boutgifter.beregning.BoutgifterVedtaksperiodeValideringUtils.validerUtgiftHeleVedtaksperioden
+import no.nav.tilleggsstonader.sak.vedtak.domain.Avslag
+import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseTilsynBarn
+import no.nav.tilleggsstonader.sak.vedtak.domain.OpphørTilsynBarn
+import no.nav.tilleggsstonader.sak.vedtak.domain.TypeBoutgift
+import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
+import no.nav.tilleggsstonader.sak.vilkår.stønadsperiode.mergeSammenhengendeOppfylteVilkårperioder
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class BoutgifterVedtaksperiodeValidingerService(
+    private val vilkårperiodeService: VilkårperiodeService,
+    private val grunnlagsdataService: GrunnlagsdataService,
+    private val vedtakRepository: VedtakRepository,
+) {
+    fun validerVedtaksperioder(
+        vedtaksperioder: List<Vedtaksperiode>,
+        behandling: Saksbehandling,
+        utgifter: Map<TypeBoutgift, List<UtgiftBeregningBoutgifter>>,
+        typeVedtak: TypeVedtak,
+    ) {
+        if (typeVedtak != TypeVedtak.OPPHØR) {
+            validerVedtaksperioderEksisterer(vedtaksperioder)
+        }
+        validerIngenOverlappMellomVedtaksperioder(vedtaksperioder)
+        validerUtgiftHeleVedtaksperioden(vedtaksperioder, utgifter)
+
+        val vilkårperioder = vilkårperiodeService.hentVilkårperioder(behandling.id)
+        validerAtVedtaksperioderIkkeOverlapperMedVilkårPeriodeUtenRett(vilkårperioder, vedtaksperioder)
+
+        val målgrupper = vilkårperioder.målgrupper.mergeSammenhengendeOppfylteVilkårperioder()
+        val aktiviteter = vilkårperioder.aktiviteter.mergeSammenhengendeOppfylteVilkårperioder()
+
+        val fødselsdato =
+            grunnlagsdataService
+                .hentGrunnlagsdata(behandling.id)
+                .grunnlag.fødsel
+                ?.fødselsdatoEller1JanForFødselsår()
+
+        vedtaksperioder.forEach {
+            validerEnkeltperiode(
+                vedtaksperiode = it,
+                målgruppePerioderPerType = målgrupper,
+                aktivitetPerioderPerType = aktiviteter,
+                fødselsdato = fødselsdato,
+            )
+        }
+
+        validerIngenEndringerFørRevurderFra(
+            innsendteVedtaksperioder = vedtaksperioder,
+            vedtaksperioderForrigeBehandling = hentForrigeVedtaksperioder(behandling),
+            revurderFra = behandling.revurderFra,
+        )
+    }
+
+    private fun hentForrigeVedtaksperioder(behandling: Saksbehandling): List<Vedtaksperiode>? =
+        behandling.forrigeBehandlingId?.let {
+            when (val forrigeVedtak = vedtakRepository.findByIdOrNull(it)?.data) {
+                is InnvilgelseTilsynBarn -> forrigeVedtak.vedtaksperioder
+                is OpphørTilsynBarn -> forrigeVedtak.vedtaksperioder
+                is Avslag -> null
+                else -> error("Håndterer ikke ${forrigeVedtak?.javaClass?.simpleName}")
+            }
+        }
+}
