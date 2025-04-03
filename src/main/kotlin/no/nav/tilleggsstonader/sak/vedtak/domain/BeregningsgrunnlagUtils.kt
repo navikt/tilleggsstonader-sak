8cc2d74fec4a081b694584daaaaaaafcf46f2811
Johan Blomgren <blommish@users.noreply.github.com>
2025-04-03 14:07:40 +0200

    Rename kommentarer stønadsperiode -> vedtaksperiode

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/domain/BeregningsgrunnlagUtils.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/domain/BeregningsgrunnlagUtils.kt
@@ -3,15 +3,15 @@ package no.nav.tilleggsstonader.sak.vedtak.domain
 object BeregningsgrunnlagUtils {
     /**
      * Beregner antall dager per uke som kan brukes
-     * Hvis antall dager fra stønadsperiode er 1, så kan man maks bruke 1 dag fra aktiviteter
-     * Hvis antall dager fra stønadsperiode er 5, men aktiviteter kun har 2 dager så kan man kun bruke 2 dager
+     * Hvis antall dager fra vedtaksperiode er 1, så kan man maks bruke 1 dag fra aktiviteter
+     * Hvis antall dager fra vedtaksperiode er 5, men aktiviteter kun har 2 dager så kan man kun bruke 2 dager
      */
     fun beregnAntallAktivitetsdagerForUke(
         periodeMedDager: PeriodeMedDager,
         aktiviteter: List<PeriodeMedDager>,
     ): Int =
         aktiviteter.filter { it.overlapper(periodeMedDager) }.fold(0) { acc, aktivitet ->
-            // Tilgjengelige dager i uke i overlapp mellom stønadsperiode og aktivitet
+            // Tilgjengelige dager i uke i overlapp mellom vedtaksperiode og aktivitet
             val antallTilgjengeligeDager = minOf(periodeMedDager.antallDager, aktivitet.antallDager)
 
             trekkFraBrukteDager(periodeMedDager, aktivitet, antallTilgjengeligeDager)
@@ -20,17 +20,17 @@ object BeregningsgrunnlagUtils {
         }
 
     /**
-     * Skal ikke kunne bruke en dager fra aktivitet eller stønadsperiode flere ganger.
+     * Skal ikke kunne bruke en dager fra aktivitet eller vedtaksperiode flere ganger.
      * Trekker fra tilgjengelige dager fra antallDager
-     * Dersom stønadsperioden har 2 dager, og aktiviten 3 dager, så skal man kun totalt kunne bruke 2 dager
-     * Dersom stønadsperioden har 3 dager, og aktiviten 2 dager, så skal man kun totalt kunne bruke 2 dager
+     * Dersom vedtaksperiode har 2 dager, og aktiviten 3 dager, så skal man kun totalt kunne bruke 2 dager
+     * Dersom vedtaksperiode har 3 dager, og aktiviten 2 dager, så skal man kun totalt kunne bruke 2 dager
      */
     private fun trekkFraBrukteDager(
-        stønadsperiode: PeriodeMedDager,
+        vedtaksperiode: PeriodeMedDager,
         aktivitet: PeriodeMedDager,
         antallTilgjengeligeDager: Int,
     ) {
         aktivitet.antallDager -= antallTilgjengeligeDager
-        stønadsperiode.antallDager -= antallTilgjengeligeDager
+        vedtaksperiode.antallDager -= antallTilgjengeligeDager
     }
 }
