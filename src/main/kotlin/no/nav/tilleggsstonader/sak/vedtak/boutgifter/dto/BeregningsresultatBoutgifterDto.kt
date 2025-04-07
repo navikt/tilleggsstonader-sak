29ed72ca80626392278bc76115792b25a8cd3809
Runar Skagestad <runar.skagestad@nav.no>
2025-04-07 15:19:01 +0200

    🔥 Fjerner sammenslåing av `BeregningsgrunnlagBoutgifterDto`. Det skal vises som én rad per periode.

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/dto/BeregningsresultatBoutgifterDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/dto/BeregningsresultatBoutgifterDto.kt
@@ -1,8 +1,6 @@
 package no.nav.tilleggsstonader.sak.vedtak.boutgifter.dto
 
 import no.nav.tilleggsstonader.kontrakter.felles.Periode
-import no.nav.tilleggsstonader.kontrakter.felles.mergeSammenhengende
-import no.nav.tilleggsstonader.kontrakter.felles.påfølgesAv
 import no.nav.tilleggsstonader.sak.felles.domain.FaktiskMålgruppe
 import no.nav.tilleggsstonader.sak.vedtak.boutgifter.domain.BeregningsresultatBoutgifter
 import no.nav.tilleggsstonader.sak.vedtak.boutgifter.domain.BeregningsresultatForLøpendeMåned
@@ -16,50 +14,25 @@ data class BeregningsresultatBoutgifterDto(
 data class BeregningsresultatForPeriodeDto(
     override val fom: LocalDate,
     override val tom: LocalDate,
-    val antallMåneder: Int,
     val stønadsbeløp: Int,
     val utbetalingsdato: LocalDate,
     val målgruppe: FaktiskMålgruppe,
     val aktivitet: AktivitetType,
     val delAvTidligereUtbetaling: Boolean,
-) : Periode<LocalDate> {
-    fun slåSammen(nestePeriode: BeregningsresultatForPeriodeDto): BeregningsresultatForPeriodeDto =
-        this.copy(
-            tom = nestePeriode.tom,
-            stønadsbeløp = this.stønadsbeløp + nestePeriode.stønadsbeløp,
-            antallMåneder = this.antallMåneder + 1,
-        )
+) : Periode<LocalDate>
 
-    fun kanSlåsSammen(nestePeriode: BeregningsresultatForPeriodeDto): Boolean =
-        this.målgruppe == nestePeriode.målgruppe &&
-            this.aktivitet == nestePeriode.aktivitet &&
-            this.stønadsbeløp == nestePeriode.stønadsbeløp &&
-            this.utbetalingsdato == nestePeriode.utbetalingsdato &&
-            this.delAvTidligereUtbetaling == nestePeriode.delAvTidligereUtbetaling &&
-            this.påfølgesAv(nestePeriode)
-}
-
-fun BeregningsresultatBoutgifter.tilDto(revurderFra: LocalDate?): BeregningsresultatBoutgifterDto {
-    val perioderDto =
-        this
-            .filtrerFraOgMed(revurderFra)
-            .perioder
-            .map { it.tilDto() }
-    return BeregningsresultatBoutgifterDto(
+fun BeregningsresultatBoutgifter.tilDto(revurderFra: LocalDate?): BeregningsresultatBoutgifterDto =
+    BeregningsresultatBoutgifterDto(
         perioder =
-            perioderDto
-                .mergeSammenhengende(
-                    skalMerges = { v1, v2 -> v1.kanSlåsSammen(v2) },
-                    merge = { v1, v2 -> v1.slåSammen(v2) },
-                ),
+            filtrerFraOgMed(revurderFra)
+                .perioder
+                .map { it.tilDto() },
     )
-}
 
 fun BeregningsresultatForLøpendeMåned.tilDto(): BeregningsresultatForPeriodeDto =
     BeregningsresultatForPeriodeDto(
         fom = grunnlag.fom,
         tom = grunnlag.tom,
-        antallMåneder = 1,
         stønadsbeløp = stønadsbeløp,
         utbetalingsdato = grunnlag.utbetalingsdato,
         målgruppe = grunnlag.målgruppe,
