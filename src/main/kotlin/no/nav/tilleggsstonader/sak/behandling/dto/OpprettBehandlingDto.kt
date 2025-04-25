8dedc31496d1a6face411d78713d23d101f23c6d
Filip Larsen <filip.larsen@live.no>
2025-04-25 09:42:28 +0200

    Lagrer metadata om nye opplysninger ved opprettelse av ny behandling om årsak er NYE_OPPLYSNINGER
    Legger til nye felter i databasen

--- src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/dto/OpprettBehandlingDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/dto/OpprettBehandlingDto.kt
@@ -1,15 +1,32 @@
 package no.nav.tilleggsstonader.sak.behandling.dto
 
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingÅrsak
+import no.nav.tilleggsstonader.sak.behandling.domain.NyeOpplysningerEndring
+import no.nav.tilleggsstonader.sak.behandling.domain.NyeOpplysningerKilde
+import no.nav.tilleggsstonader.sak.behandling.domain.NyeOpplysningerMetadata
 import no.nav.tilleggsstonader.sak.felles.domain.FagsakId
 import java.time.LocalDate
 
 data class OpprettBehandlingDto(
     val fagsakId: FagsakId,
     val årsak: BehandlingÅrsak,
+    val nyeOpplysningerMetadata: NyeOpplysningerMetadataDto?,
     val valgteBarn: Set<String> = emptySet(),
     val kravMottatt: LocalDate?,
 )
+
+data class NyeOpplysningerMetadataDto(
+    val kilde: NyeOpplysningerKilde,
+    val endringer: List<NyeOpplysningerEndring>,
+    val beskrivelse: String?,
+) {
+    fun tilDomene() =
+        NyeOpplysningerMetadata(
+            kilde = kilde,
+            endringer = endringer,
+            beskrivelse = beskrivelse,
+        )
+}
 
 data class BarnTilRevurderingDto(
     val barn: List<Barn>,
