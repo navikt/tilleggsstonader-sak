c0d0f47ac90b936fdda7a3aac42eff4dc156a32f
Sigmund Berbom <sberbom@gmail.com>
2025-04-28 11:16:28 +0200

    ✨ Legg til logikk for å kunne beregne boutgifter i en revurdering

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/domain/BeregningsresultatBoutgifter.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/domain/BeregningsresultatBoutgifter.kt
@@ -42,11 +42,11 @@ data class BeregningsresultatForLøpendeMåned(
         tom: LocalDate,
     ): BeregningsresultatForLøpendeMåned = this.copy(grunnlag = this.grunnlag.copy(fom = fom, tom = tom))
 
-//    fun medKorrigertUtbetalingsdato(utbetalingsdato: LocalDate): BeregningsresultatForLøpendeMåned =
-//        this.copy(grunnlag = grunnlag.copy(utbetalingsdato = utbetalingsdato))
-//
-//    fun markerSomDelAvTidligereUtbetaling(delAvTidligereUtbetaling: Boolean) =
-//        this.copy(delAvTidligereUtbetaling = delAvTidligereUtbetaling)
+    fun medKorrigertUtbetalingsdato(utbetalingsdato: LocalDate): BeregningsresultatForLøpendeMåned =
+        this.copy(grunnlag = grunnlag.copy(utbetalingsdato = utbetalingsdato))
+
+    fun markerSomDelAvTidligereUtbetaling(delAvTidligereUtbetaling: Boolean) =
+        this.copy(delAvTidligereUtbetaling = delAvTidligereUtbetaling)
 
     fun summerUtgifter(): Int =
         grunnlag.utgifter.values
