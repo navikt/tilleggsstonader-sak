82abb09fa271d242f4997795a180e25ece890615
sarahjelle <sara.hjelle@bekk.no>
2025-04-30 10:58:33 +0200

    Kutt utgiftsperioder som er yearMonth fra starten av måneden for revurder fra, fordi validering feiler. Diskutert at det gir mer mening å vise utgiften fra 1. fordi de ikke har lov å legge inn en vilkår fra en annen dato

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/VilkårUtil.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/VilkårUtil.kt
@@ -5,7 +5,9 @@ import no.nav.tilleggsstonader.kontrakter.felles.Periode
 import no.nav.tilleggsstonader.kontrakter.felles.mergeSammenhengende
 import no.nav.tilleggsstonader.kontrakter.felles.overlapperEllerPåfølgesAv
 import no.nav.tilleggsstonader.kontrakter.periode.avkortPerioderFør
+import no.nav.tilleggsstonader.sak.util.tilFørsteDagIMåneden
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkår
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.VilkårType
 import java.time.LocalDate
 
 object VilkårUtil {
@@ -26,40 +28,50 @@ object VilkårUtil {
 
     fun List<Vilkår>.finnPerioderEtterRevurderFra(revurderFra: LocalDate?): List<Vilkår> =
         this
-            .map { VilkårHolder(it) }
-            .avkortPerioderFør(revurderFra)
-            .map { it.vilkår }
+            .map { VilkårHolder(it).avkortPerioderFør(revurderFraEllerFørsteIMåneden(it.type, revurderFra)) }
+            .mapNotNull { it?.vilkår }
 
-    /**
-     * Hjelpeklasse for å kunne bruke funksjonene som finnes på Periode<T>
-     */
-    private data class VilkårHolder(
-        val vilkår: Vilkår,
-    ) : Periode<LocalDate>,
-        KopierPeriode<VilkårHolder> {
-        override val fom: LocalDate
-            get() = vilkår.fom ?: error("Mangler tom")
-        override val tom: LocalDate
-            get() = vilkår.tom ?: error("Mangler tom")
+    private fun revurderFraEllerFørsteIMåneden(
+        vilkårType: VilkårType,
+        revurderFra: LocalDate?,
+    ): LocalDate? {
+        if (revurderFra == null || !vilkårType.kreverYearMonthDatoer()) {
+            return revurderFra
+        }
 
-        override fun medPeriode(
-            fom: LocalDate,
-            tom: LocalDate,
-        ): VilkårHolder = VilkårHolder(vilkår.copy(fom = fom, tom = tom))
-
-        fun slåSammen(other: VilkårHolder): VilkårHolder =
-            VilkårHolder(
-                vilkår.copy(
-                    fom = minOf(vilkår.fom!!, other.vilkår.fom!!),
-                    tom = maxOf(vilkår.tom!!, other.vilkår.tom!!),
-                ),
-            )
-
-        fun kanSlåsSammen(other: VilkårHolder): Boolean =
-            vilkår.type == other.vilkår.type &&
-                vilkår.resultat == other.vilkår.resultat &&
-                vilkår.utgift == other.vilkår.utgift &&
-                vilkår.barnId == other.vilkår.barnId &&
-                overlapperEllerPåfølgesAv(other)
+        return revurderFra.tilFørsteDagIMåneden()
     }
+}
+
+/**
+ * Hjelpeklasse for å kunne bruke funksjonene som finnes på Periode<T>
+ */
+private data class VilkårHolder(
+    val vilkår: Vilkår,
+) : Periode<LocalDate>,
+    KopierPeriode<VilkårHolder> {
+    override val fom: LocalDate
+        get() = vilkår.fom ?: error("Mangler tom")
+    override val tom: LocalDate
+        get() = vilkår.tom ?: error("Mangler tom")
+
+    override fun medPeriode(
+        fom: LocalDate,
+        tom: LocalDate,
+    ): VilkårHolder = VilkårHolder(vilkår.copy(fom = fom, tom = tom))
+
+    fun slåSammen(other: VilkårHolder): VilkårHolder =
+        VilkårHolder(
+            vilkår.copy(
+                fom = minOf(vilkår.fom!!, other.vilkår.fom!!),
+                tom = maxOf(vilkår.tom!!, other.vilkår.tom!!),
+            ),
+        )
+
+    fun kanSlåsSammen(other: VilkårHolder): Boolean =
+        vilkår.type == other.vilkår.type &&
+            vilkår.resultat == other.vilkår.resultat &&
+            vilkår.utgift == other.vilkår.utgift &&
+            vilkår.barnId == other.vilkår.barnId &&
+            overlapperEllerPåfølgesAv(other)
 }
