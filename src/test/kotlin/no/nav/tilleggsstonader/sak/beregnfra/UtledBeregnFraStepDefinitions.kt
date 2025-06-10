605b25e10f4791d8e06a94390405649925fdd365
sarahjelle <sara.hjelle@bekk.no>
2025-06-10 17:07:45 +0200

    Legg til flere tester av beregnFra

--- src/test/kotlin/no/nav/tilleggsstonader/sak/beregnfra/UtledBeregnFraStepDefinitions.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/beregnfra/UtledBeregnFraStepDefinitions.kt
@@ -14,10 +14,12 @@ import no.nav.tilleggsstonader.sak.cucumber.parseInt
 import no.nav.tilleggsstonader.sak.cucumber.parseValgfriBoolean
 import no.nav.tilleggsstonader.sak.cucumber.parseValgfriEnum
 import no.nav.tilleggsstonader.sak.cucumber.parseValgfriInt
+import no.nav.tilleggsstonader.sak.cucumber.parseValgfriString
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
 import no.nav.tilleggsstonader.sak.util.behandling
 import no.nav.tilleggsstonader.sak.util.vedtaksperiode
 import no.nav.tilleggsstonader.sak.util.vilkår
+import no.nav.tilleggsstonader.sak.vedtak.barnetilsyn.BarnIdTilTestIdHolder.barnIder
 import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.Vilkår
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.VilkårStatus
@@ -58,6 +60,7 @@ enum class VilkårNøkler(
 ) : Domenenøkkel {
     UTGIFT("Utgift"),
     ER_FREMTIDIG_UTGIFT("Er fremtidig utgift"),
+    BARN_ID("BarnId"),
 }
 
 enum class VedtaksperiodeNøkler(
@@ -152,12 +155,14 @@ class UtledBeregnFraStepDefinitions {
 
     private fun mapVilkår(dataTable: DataTable) =
         dataTable.mapRad { rad ->
+            val valgtBarnId = parseValgfriInt(VilkårNøkler.BARN_ID, rad)
             vilkår(
                 behandlingId = BehandlingId.random(),
                 fom = parseDato(DomenenøkkelFelles.FOM, rad),
                 tom = parseDato(DomenenøkkelFelles.TOM, rad),
                 resultat = parseEnum(BeregnFraFellesNøkler.RESULTAT, rad),
                 type = parseValgfriEnum<VilkårType>(BeregnFraFellesNøkler.TYPE, rad) ?: VilkårType.PASS_BARN,
+                barnId = barnIder[valgtBarnId],
                 status = parseValgfriEnum<VilkårStatus>(
                     BeregnFraFellesNøkler.STATUS,
                     rad,
@@ -229,267 +234,3 @@ class UtledBeregnFraStepDefinitions {
         }
     }
 }
-
-//    @Gitt("følgende aktiviteter")
-//    fun `følgende aktiviteter`(dataTable: DataTable) {
-//        vilkårperiodeRepository.insertAll(mapAktiviteter(behandlingId, dataTable))
-//    }
-//
-//    @Gitt("følgende målgrupper")
-//    fun `følgende målgrupper`(dataTable: DataTable) {
-//        vilkårperiodeRepository.insertAll(mapMålgrupper(behandlingId, dataTable))
-//    }
-//
-//
-//    @Når("beregner med revurderFra={}")
-//    fun `beregner med revurder fra`(revurderFraStr: String) {
-//        val revurderFra = parseDato(revurderFraStr)
-//        beregn(
-//            saksbehandling(
-//                id = behandlingId,
-//                type = BehandlingType.REVURDERING,
-//                revurderFra = revurderFra,
-//                forrigeIverksatteBehandlingId = BehandlingId.random(),
-//            ),
-//        )
-//    }
-//
-//    private fun beregn(behandling: Saksbehandling) {
-//        every { tilsynBarnUtgiftService.hentUtgifterTilBeregning(any()) } returns utgifter
-//        try {
-//            beregningsresultat = service.beregn(vedtaksperioder, behandling, TypeVedtak.INNVILGELSE)
-//        } catch (e: Exception) {
-//            exception = e
-//        }
-//    }
-//
-//    @Så("forvent følgende feil: {}")
-//    fun `forvent følgende feil`(forventetFeil: String) {
-//        assertThat(exception!!).hasMessageContaining(forventetFeil)
-//    }
-//
-//    @Så("forvent følgende dato for første endring")
-//    fun `forvent følgende beregningsresultat`(dataTable: DataTable) {
-//        if (exception != null) {
-//            logger.error("Feilet", exception)
-//        }
-//
-//        assertThat(exception).isNull()
-//        val forventetBeregningsresultat =
-//            dataTable.mapRad { rad ->
-//                ForventetBeregningsresultat(
-//                    dagsats = parseBigDecimal(BeregningNøkler.DAGSATS, rad),
-//                    månedsbeløp = parseValgfriInt(BeregningNøkler.MÅNEDSBELØP, rad),
-//                    grunnlag =
-//                        ForventetBeregningsgrunnlag(
-//                            måned = parseÅrMåned(BeregningNøkler.MÅNED, rad),
-//                            makssats = parseValgfriInt(BeregningNøkler.MAKSSATS, rad),
-//                            antallDagerTotal = parseValgfriInt(BeregningNøkler.ANTALL_DAGER, rad),
-//                            utgifterTotal = parseValgfriInt(BeregningNøkler.UTGIFT, rad),
-//                            antallBarn = parseValgfriInt(BeregningNøkler.ANTALL_BARN, rad),
-//                        ),
-//                )
-//            }
-//
-//        val perioder = beregningsresultat!!.perioder
-//
-//        perioder.forEachIndexed { index, resultat ->
-//            val forventetResultat = forventetBeregningsresultat[index]
-//            try {
-//                assertThat(resultat.grunnlag.måned)
-//                    .`as` { "måned" }
-//                    .isEqualTo(forventetResultat.grunnlag.måned)
-//
-//                assertThat(resultat.dagsats)
-//                    .`as` { "dagsats" }
-//                    .isEqualTo(forventetResultat.dagsats)
-//
-//                forventetResultat.månedsbeløp?.let {
-//                    assertThat(resultat.månedsbeløp)
-//                        .`as` { "totaltMånedsbeløp" }
-//                        .isEqualTo(it)
-//                }
-//
-//                forventetResultat.grunnlag.antallDagerTotal?.let {
-//                    assertThat(resultat.grunnlag.vedtaksperiodeGrunnlag.sumOf { it.antallDager })
-//                        .`as` { "antallDagerTotal" }
-//                        .isEqualTo(it)
-//                }
-//
-//                forventetResultat.grunnlag.utgifterTotal?.let {
-//                    assertThat(resultat.grunnlag.utgifterTotal)
-//                        .`as` { "utgifterTotal" }
-//                        .isEqualTo(it)
-//                }
-//
-//                forventetResultat.grunnlag.makssats?.let {
-//                    assertThat(resultat.grunnlag.makssats)
-//                        .`as` { "makssats" }
-//                        .isEqualTo(it)
-//                }
-//            } catch (e: Throwable) {
-//                val acutal = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultat)
-//                logger.error("Feilet validering av rad ${index + 1} $acutal")
-//                throw e
-//            }
-//        }
-//
-//        assertThat(perioder).hasSize(forventetBeregningsresultat.size)
-//    }
-//
-//    @Så("forvent følgende vedtaksperioder for: {}")
-//    fun `forvent følgende vedtaksperioder`(
-//        månedStr: String,
-//        dataTable: DataTable,
-//    ) {
-//        assertThat(exception).isNull()
-//        val måned = parseÅrMåned(månedStr)
-//        val forventeteVedtaksperioder = mapVedtaksperioder(dataTable)
-//
-//        val perioder =
-//            beregningsresultat!!
-//                .perioder
-//                .find { it.grunnlag.måned == måned }
-//                ?.grunnlag
-//                ?.vedtaksperiodeGrunnlag
-//                ?.map { it.vedtaksperiode }
-//                ?: error("Finner ikke beregningsresultat for $måned")
-//
-//        perioder.forEachIndexed { index, resultat ->
-//            val forventetResultat = forventeteVedtaksperioder[index]
-//            try {
-//                assertThat(resultat.fom).`as` { "fom" }.isEqualTo(forventetResultat.fom)
-//                assertThat(resultat.tom).`as` { "tom" }.isEqualTo(forventetResultat.tom)
-//            } catch (e: Throwable) {
-//                logger.error("Feilet validering av rad ${index + 1}")
-//                throw e
-//            }
-//        }
-//
-//        assertThat(perioder)
-//            .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
-//            .containsExactlyElementsOf(forventeteVedtaksperioder.tilVedtaksperiodeBeregning().sorted())
-//    }
-//
-//    @Så("forvent følgende vedtaksperiodeGrunnlag for: {}")
-//    fun `forvent følgende vedtaksperiodeGrunnlag`(
-//        månedStr: String,
-//        dataTable: DataTable,
-//    ) {
-//        assertThat(exception).isNull()
-//        val måned = parseÅrMåned(månedStr)
-//        val forventeteVedtaksperioder = parseForventedeVedtaksperioder(dataTable)
-//
-//        val perioder =
-//            beregningsresultat!!
-//                .perioder
-//                .find { it.grunnlag.måned == måned }
-//                ?.grunnlag
-//                ?.vedtaksperiodeGrunnlag
-//                ?: error("Finner ikke beregningsresultat for $måned")
-//
-//        perioder.forEachIndexed { index, resultat ->
-//            val forventetResultat = forventeteVedtaksperioder[index]
-//            try {
-//                assertThat(resultat.vedtaksperiode.fom).isEqualTo(forventetResultat.fom)
-//                assertThat(resultat.vedtaksperiode.tom).isEqualTo(forventetResultat.tom)
-//                assertThat(resultat.vedtaksperiode.målgruppe).isEqualTo(forventetResultat.målgruppe)
-//                assertThat(resultat.vedtaksperiode.aktivitet).isEqualTo(forventetResultat.aktivitet)
-//                assertThat(resultat.aktiviteter.size).isEqualTo(forventetResultat.antallAktiviteter)
-//                assertThat(resultat.antallDager).isEqualTo(forventetResultat.antallDager)
-//            } catch (e: Throwable) {
-//                val actual =
-//                    listOf(
-//                        resultat.vedtaksperiode.fom,
-//                        resultat.vedtaksperiode.tom,
-//                        resultat.vedtaksperiode.målgruppe,
-//                        resultat.vedtaksperiode.aktivitet,
-//                        resultat.aktiviteter.size,
-//                        resultat.antallDager,
-//                    ).joinToString(" | ")
-//                val expected =
-//                    listOf(
-//                        forventetResultat.fom,
-//                        forventetResultat.tom,
-//                        forventetResultat.målgruppe,
-//                        forventetResultat.aktivitet,
-//                        forventetResultat.antallAktiviteter,
-//                        forventetResultat.antallDager,
-//                    ).joinToString(" | ")
-//                logger.error(
-//                    "Feilet validering av rad ${index + 1}\n" +
-//                            "expected = $expected\n" +
-//                            "actual = $actual",
-//                )
-//                throw e
-//            }
-//        }
-//    }
-//
-//    @Så("forvent følgende beløpsperioder for: {}")
-//    fun `forvent følgende beløpsperioder`(
-//        månedStr: String,
-//        dataTable: DataTable,
-//    ) {
-//        assertThat(exception).isNull()
-//        val måned = parseÅrMåned(månedStr)
-//        val forventedeBeløpsperioder = parseForventedeBeløpsperioder(dataTable)
-//
-//        val beløpsperioder =
-//            beregningsresultat!!
-//                .perioder
-//                .find { it.grunnlag.måned == måned }
-//                ?.beløpsperioder
-//                ?: error("Finner ikke beregningsresultat for $måned")
-//
-//        verifiserAtListerErLike(beløpsperioder, forventedeBeløpsperioder)
-//    }
-//
-//    private fun parseForventedeVedtaksperioder(dataTable: DataTable): List<ForventedeVedtaksperioder> =
-//        dataTable.mapRad { rad ->
-//            ForventedeVedtaksperioder(
-//                fom = parseÅrMånedEllerDato(DomenenøkkelFelles.FOM, rad).datoEllerFørsteDagenIMåneden(),
-//                tom = parseÅrMånedEllerDato(DomenenøkkelFelles.TOM, rad).datoEllerSisteDagenIMåneden(),
-//                målgruppe = parseValgfriEnum<FaktiskMålgruppe>(BeregningNøkler.MÅLGRUPPE, rad)
-//                    ?: FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
-//                aktivitet =
-//                    parseValgfriEnum<AktivitetType>(BeregningNøkler.AKTIVITET, rad)
-//                        ?: AktivitetType.TILTAK,
-//                antallAktiviteter = parseInt(BeregningNøkler.ANTALL_AKTIVITETER, rad),
-//                antallDager = parseInt(BeregningNøkler.ANTALL_DAGER, rad),
-//            )
-//        }
-//
-//    private fun parseForventedeBeløpsperioder(dataTable: DataTable): List<Beløpsperiode> =
-//        dataTable.mapRad { rad ->
-//            Beløpsperiode(
-//                dato = parseÅrMånedEllerDato(BeregningNøkler.DATO, rad).datoEllerFørsteDagenIMåneden(),
-//                målgruppe = parseValgfriEnum<FaktiskMålgruppe>(BeregningNøkler.MÅLGRUPPE, rad)
-//                    ?: FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
-//                beløp = parseInt(BeregningNøkler.BELØP, rad),
-//            )
-//        }
-// }
-//
-// data class ForventetBeregningsresultat(
-//    val dagsats: BigDecimal,
-//    val månedsbeløp: Int?,
-//    val grunnlag: ForventetBeregningsgrunnlag,
-// )
-//
-// data class ForventetBeregningsgrunnlag(
-//    val måned: YearMonth,
-//    val makssats: Int?,
-//    val antallDagerTotal: Int?,
-//    val utgifterTotal: Int?,
-//    val antallBarn: Int?,
-// )
-//
-// data class ForventedeVedtaksperioder(
-//    val fom: LocalDate,
-//    val tom: LocalDate,
-//    val målgruppe: FaktiskMålgruppe,
-//    val aktivitet: AktivitetType,
-//    val antallAktiviteter: Int?,
-//    val antallDager: Int?,
-// )
--- src/test/resources/no/nav/tilleggsstonader/sak/beregnFra/endring_vilkår.feature
+++ src/test/resources/no/nav/tilleggsstonader/sak/beregnFra/endring_vilkår.feature
@@ -138,3 +138,81 @@ Egenskap: Utled beregn fra endring av vilkår
     Når utleder beregnFraDato
 
     Så forvent følgende dato for tidligste endring: 01.03.2024
+
+  Regel: Endring på vilkår for ett barn skal kun sammenlignes med vilkår for det aktuelle barnet
+    Scenario: Forlengelse av vilkår for ett barn
+      Gitt følgende vilkår i forrige behandling - beregnFra
+        | Fom        | Tom        | Type      | Resultat | BarnId | Status |
+        | 01.03.2024 | 31.03.2024 | PASS_BARN | OPPFYLT  | 1      | NY     |
+        | 01.03.2024 | 30.04.2024 | PASS_BARN | OPPFYLT  | 2      | NY     |
+
+      Gitt følgende vilkår i revurdering - beregnFra
+        | Fom        | Tom        | Type      | Resultat | BarnId | Status  |
+        | 01.03.2024 | 31.03.2024 | PASS_BARN | OPPFYLT  | 1      | UENDRET |
+        | 01.03.2024 | 31.05.2024 | PASS_BARN | OPPFYLT  | 2      | ENDRET  |
+
+      Når utleder beregnFraDato
+
+      Så forvent følgende dato for tidligste endring: 01.05.2024
+
+      # To barn ender i en revurdering med like perioder, men ulike utgifter.
+    # Skal håndtere at endringen er en utvidelse og ikke en endring av fakta på annet barn
+    Scenario: Forlengelse av vilkår for ett barn slik at periode blir likt som eksisterende barn
+      Gitt følgende vilkår i forrige behandling - beregnFra
+        | Fom        | Tom        | Type      | Resultat | BarnId | Utgift | Status |
+        | 01.03.2024 | 30.04.2024 | PASS_BARN | OPPFYLT  | 1      | 1500   | NY     |
+        | 01.03.2024 | 31.03.2024 | PASS_BARN | OPPFYLT  | 2      | 1000   | NY     |
+
+      Gitt følgende vilkår i revurdering - beregnFra
+        | Fom        | Tom        | Type      | Resultat | BarnId | Utgift | Status  |
+        | 01.03.2024 | 30.04.2024 | PASS_BARN | OPPFYLT  | 1      | 1500   | UENDRET |
+        | 01.03.2024 | 30.04.2024 | PASS_BARN | OPPFYLT  | 2      | 1000   | ENDRET  |
+
+      Når utleder beregnFraDato
+
+      Så forvent følgende dato for tidligste endring: 01.04.2024
+
+    Scenario: Et barn slettes og samme periode legges inn for nytt barn
+      # Feks at utgift ble feilregistrert på barn 1
+      Gitt følgende vilkår i forrige behandling - beregnFra
+        | Fom        | Tom        | Type      | Resultat | BarnId | Utgift | Status |
+        | 01.03.2024 | 31.03.2024 | PASS_BARN | OPPFYLT  | 1      | 1000   | NY     |
+
+      Gitt følgende vilkår i revurdering - beregnFra
+        | Fom        | Tom        | Type      | Resultat | BarnId | Utgift | Status  |
+        | 01.03.2024 | 31.03.2024 | PASS_BARN | OPPFYLT  | 2      | 1000   | NY      |
+        | 01.03.2024 | 31.03.2024 | PASS_BARN | OPPFYLT  | 1      | 1000   | SLETTET |
+
+      Når utleder beregnFraDato
+
+      Så forvent følgende dato for tidligste endring: 01.03.2024
+
+  Regel: Endring på vilkår skal kun sjekkes opp mot andre vilkår av samme type
+    # Usikker på hva som bør testes her...
+    Scenario: Nytt vilkår av annen type er lagt inn etter eksisterende
+      Gitt følgende vilkår i forrige behandling - beregnFra
+        | Fom        | Tom        | Type                      | Resultat | Status |
+        | 01.03.2024 | 31.03.2024 | LØPENDE_UTGIFTER_EN_BOLIG | OPPFYLT  | NY     |
+
+      Gitt følgende vilkår i revurdering - beregnFra
+        | Fom        | Tom        | Type                      | Resultat | Status  |
+        | 01.03.2024 | 31.03.2024 | LØPENDE_UTGIFTER_EN_BOLIG | OPPFYLT  | UENDRET |
+        | 03.03.2024 | 04.03.2024 | UTGIFTER_OVERNATTING      | OPPFYLT  | NY      |
+
+      Når utleder beregnFraDato
+
+      Så forvent følgende dato for tidligste endring: 03.03.2024
+
+    Scenario: Nytt vilkår av annen type er lagt inn etter eksisterende
+      Gitt følgende vilkår i forrige behandling - beregnFra
+        | Fom        | Tom        | Type                      | Resultat | Status |
+        | 01.03.2024 | 31.03.2024 | LØPENDE_UTGIFTER_EN_BOLIG | OPPFYLT  | NY     |
+
+      Gitt følgende vilkår i revurdering - beregnFra
+        | Fom        | Tom        | Type                      | Resultat | Status  |
+        | 24.02.2024 | 25.02.2024 | UTGIFTER_OVERNATTING      | OPPFYLT  | NY      |
+        | 01.03.2024 | 31.03.2024 | LØPENDE_UTGIFTER_EN_BOLIG | OPPFYLT  | UENDRET |
+
+      Når utleder beregnFraDato
+
+      Så forvent følgende dato for tidligste endring: 24.02.2024
