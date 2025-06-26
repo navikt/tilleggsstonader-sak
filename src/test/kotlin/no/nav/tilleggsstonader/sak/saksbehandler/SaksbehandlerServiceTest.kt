7081ae1f1aa7740e4b0077c767f9463e16901d5a
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-06-26 14:59:38 +0200

    Legger til flere tester

--- src/test/kotlin/no/nav/tilleggsstonader/sak/saksbehandler/SaksbehandlerServiceTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/saksbehandler/SaksbehandlerServiceTest.kt
@@ -6,12 +6,14 @@ import io.mockk.mockkObject
 import no.nav.tilleggsstonader.kontrakter.felles.Saksbehandler
 import no.nav.tilleggsstonader.kontrakter.oppgave.Oppgavetype
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingRepository
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
 import no.nav.tilleggsstonader.sak.infrastruktur.sikkerhet.SikkerhetContext
 import no.nav.tilleggsstonader.sak.opplysninger.oppgave.OppgaveDomain
 import no.nav.tilleggsstonader.sak.opplysninger.oppgave.OppgaveRepository
+import no.nav.tilleggsstonader.sak.opplysninger.oppgave.dto.SaksbehandlerDto
 import no.nav.tilleggsstonader.sak.opplysninger.oppgave.dto.SaksbehandlerRolle
 import no.nav.tilleggsstonader.sak.opplysninger.saksbehandler.SaksbehandlerClient
 import no.nav.tilleggsstonader.sak.opplysninger.saksbehandler.SaksbehandlerService
@@ -19,6 +21,7 @@ import no.nav.tilleggsstonader.sak.util.BehandlingOppsettUtil.iverksattFørstega
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
 import java.util.UUID
 
 internal class SaksbehandlerServiceTest {
@@ -34,13 +37,13 @@ internal class SaksbehandlerServiceTest {
     fun setUp() {
         mockkObject(SikkerhetContext)
         every { SikkerhetContext.hentSaksbehandler() } returns innloggetSaksbehandler
-
         service = SaksbehandlerService(oppgaveRepository, behandlingRepository, saksbehandlerClient)
     }
 
     @Test
     fun `skal returnere INNLOGGET_SAKSBEHANDLER hvis oppgaven er tilordnet innlogget saksbehandler`() {
-        val behandling = iverksattFørstegangsbehandling.copy(steg = StegType.INNGANGSVILKÅR)
+        val behandling =
+            iverksattFørstegangsbehandling.copy(steg = StegType.INNGANGSVILKÅR, status = BehandlingStatus.UTREDES)
         val oppgave =
             OppgaveDomain(
                 id = UUID.randomUUID(),
@@ -67,7 +70,8 @@ internal class SaksbehandlerServiceTest {
 
     @Test
     fun `skal returnere OPPGAVE_FINNES_IKKE_SANNSYNLIGVIS_INNLOGGET_SAKSBEHANDLER hvis oppgave mangler og behandling er i visse steg`() {
-        val behandling = iverksattFørstegangsbehandling.copy(steg = StegType.INNGANGSVILKÅR)
+        val behandling =
+            iverksattFørstegangsbehandling.copy(steg = StegType.INNGANGSVILKÅR, status = BehandlingStatus.UTREDES)
         val saksbehandlerInfo = Saksbehandler(UUID.randomUUID(), "Z123456", "Test", "Testesen", "TSO")
 
         every { behandlingRepository.findByIdOrThrow(behandlingId) } returns behandling
@@ -84,16 +88,73 @@ internal class SaksbehandlerServiceTest {
     }
 
     @Test
-    fun `skal returnere OPPGAVE_FINNES_IKKE hvis behandling er i feil steg og ingen oppgave finnes`() {
-        val behandling = iverksattFørstegangsbehandling.copy(steg = StegType.FERDIGSTILLE_BEHANDLING)
+    fun `skal returnere OPPGAVE_FINNES_IKKE hvis oppgaven er null`() {
+        val behandling =
+            iverksattFørstegangsbehandling.copy(
+                steg = StegType.FERDIGSTILLE_BEHANDLING, // Et steg der det ikke skal finnes oppgave
+                status = BehandlingStatus.UTREDES,
+            )
 
-        every { behandlingRepository.findByIdOrThrow(behandlingId) } returns behandling
+        every { behandlingRepository.findByIdOrNull(behandlingId) } returns behandling
         every {
-            oppgaveRepository.findByBehandlingIdAndErFerdigstiltIsFalseAndTypeIn(any(), any())
+            oppgaveRepository.findByBehandlingIdAndErFerdigstiltIsFalseAndTypeIn(behandling.id, any())
         } returns null
 
         val resultat = service.finnSaksbehandler(behandlingId)
 
         assertThat(resultat?.rolle).isEqualTo(SaksbehandlerRolle.OPPGAVE_FINNES_IKKE)
+    }
+
+    @Test
+    fun `skal returnere null hvis behandlingen er ferdigstilt`() {
+        val ferdigstiltBehandling =
+            iverksattFørstegangsbehandling.copy(
+                steg = StegType.FERDIGSTILLE_BEHANDLING,
+                status = BehandlingStatus.FERDIGSTILT,
+            )
+
+        every { behandlingRepository.findByIdOrThrow(behandlingId) } returns ferdigstiltBehandling
+
+        every { oppgaveRepository.findByBehandlingIdAndErFerdigstiltIsFalseAndTypeIn(any(), emptySet()) } returns null
+
+        val resultat = service.finnSaksbehandler(behandlingId)
+
+        assertThat(resultat).isEqualTo(
+            SaksbehandlerDto(
+                fornavn = null,
+                etternavn = null,
+                rolle = SaksbehandlerRolle.OPPGAVE_FINNES_IKKE,
+            ),
+        )
+    }
+
+    @Test
+    fun `skal returnere ANNEN_SAKSBEHANDLER når innlogget ikke er tilordnet oppgave`() {
+        val innloggetSaksbehandlerSomIkkeEierBehandling = "Z654321"
+        val behandling =
+            iverksattFørstegangsbehandling.copy(steg = StegType.INNGANGSVILKÅR, status = BehandlingStatus.UTREDES)
+        val oppgave =
+            OppgaveDomain(
+                id = UUID.randomUUID(),
+                behandlingId = behandlingId,
+                gsakOppgaveId = 1L,
+                type = Oppgavetype.BehandleSak,
+                erFerdigstilt = false,
+                tilordnetSaksbehandler = innloggetSaksbehandlerSomIkkeEierBehandling,
+            )
+
+        val saksbehandlerInfo =
+            Saksbehandler(UUID.randomUUID(), innloggetSaksbehandlerSomIkkeEierBehandling, "Test", "Testesen", "TSO")
+
+        every { behandlingRepository.findByIdOrThrow(behandlingId) } returns behandling
+        every { oppgaveRepository.findByBehandlingIdAndErFerdigstiltIsFalseAndTypeIn(any(), any()) } returns oppgave
+        every { saksbehandlerClient.hentSaksbehandlerInfo(innloggetSaksbehandlerSomIkkeEierBehandling) } returns saksbehandlerInfo
+
+        val resultat = service.finnSaksbehandler(behandlingId)
+
+        assertThat(oppgave.tilordnetSaksbehandler).isNotEqualTo(SikkerhetContext.hentSaksbehandler())
+        assertThat(resultat?.rolle).isEqualTo(SaksbehandlerRolle.ANNEN_SAKSBEHANDLER)
+        assertThat(resultat?.fornavn).isEqualTo("Test")
+        assertThat(resultat?.etternavn).isEqualTo("Testesen")
     }
 }
