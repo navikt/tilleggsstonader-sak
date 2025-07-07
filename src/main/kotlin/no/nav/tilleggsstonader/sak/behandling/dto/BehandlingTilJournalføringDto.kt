cdd11d58284bd0cc52c383843a3a51bcbf507723
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-07-07 13:19:22 +0200

    Legger til nytt endepunkt med ny dto for å minimere responsen til journalføring

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/behandling/dto/BehandlingTilJournalføringDto.kt
@@ -0,0 +1,13 @@
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
+import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import java.time.LocalDateTime
+
+data class BehandlingTilJournalføringDto(
+    val id: BehandlingId,
+    val type: BehandlingType,
+    val status: BehandlingStatus,
+    val resultat: BehandlingResultat,
+    val sistEndret: LocalDateTime,
+)
