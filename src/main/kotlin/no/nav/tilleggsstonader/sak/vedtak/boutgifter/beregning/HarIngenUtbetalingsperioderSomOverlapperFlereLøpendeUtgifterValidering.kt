c1a0c3b7953b2827146d6a7d88c4ecd0c1d21e37
Johan Blomgren <blommish@users.noreply.github.com>
2025-08-14 12:01:32 +0200

    Feilmelding om ingen utbetalingsperioder overlapper flere utgifter skal kun vise de perioder som er aktuelle med mer detaljer for å enklere skjønne hvilke det er feil på

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/beregning/HarIngenUtbetalingsperioderSomOverlapperFlereLøpendeUtgifterValidering.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/beregning/HarIngenUtbetalingsperioderSomOverlapperFlereLøpendeUtgifterValidering.kt
@@ -1,30 +1,56 @@
 package no.nav.tilleggsstonader.sak.vedtak.boutgifter.beregning
 
-import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
 import no.nav.tilleggsstonader.sak.util.formatertPeriodeNorskFormat
 import no.nav.tilleggsstonader.sak.vedtak.boutgifter.domain.BoutgifterPerUtgiftstype
 import no.nav.tilleggsstonader.sak.vedtak.domain.TypeBoutgift
-import kotlin.collections.plus
 
 fun List<UtbetalingPeriode>.validerIngenUtbetalingsperioderOverlapperFlereLøpendeUtgifter(
     utgifter: BoutgifterPerUtgiftstype,
 ): List<UtbetalingPeriode> {
     val fasteUtgifter =
-        (utgifter[TypeBoutgift.LØPENDE_UTGIFTER_EN_BOLIG] ?: emptyList()) +
-            (utgifter[TypeBoutgift.LØPENDE_UTGIFTER_TO_BOLIGER] ?: emptyList())
-
-    val detFinnesUtbetalingsperioderSomOverlapperFlereLøpendeUtgifter =
-        any { utbetalingsperiode ->
-            fasteUtgifter.count { utbetalingsperiode.overlapper(it) } > 1
+        listOf(
+            TypeBoutgift.LØPENDE_UTGIFTER_EN_BOLIG,
+            TypeBoutgift.LØPENDE_UTGIFTER_TO_BOLIGER,
+        ).flatMap { type ->
+            (utgifter[type] ?: emptyList()).map { type to it }
         }
 
-    feilHvis(detFinnesUtbetalingsperioderSomOverlapperFlereLøpendeUtgifter) {
-        """
-        Vi støtter foreløpig ikke at utbetalingsperioder overlapper mer enn én løpende utgift. 
-        Utbetalingsperioder for denne behandlingen er: ${map { it.formatertPeriodeNorskFormat() }}, 
-        mens utgiftsperiodene er: ${fasteUtgifter.map { it.formatertPeriodeNorskFormat() }}
-        """.trimIndent()
+    val utbetalingsperioderMedFlereUtgifter =
+        mapNotNull { utbetalingsperiode ->
+            val utgifterSomOverlapper = fasteUtgifter.filter { utgift -> utgift.second.overlapper(utbetalingsperiode) }
+            utgifterSomOverlapper
+                .takeIf { it.size > 1 }
+                ?.let { utbetalingsperiode to it }
+        }
+
+    brukerfeilHvis(utbetalingsperioderMedFlereUtgifter.isNotEmpty()) {
+        buildString {
+            appendLine("Vi støtter foreløpig ikke at utbetalingsperioder inneholder mer enn én løpende utgift.")
+            appendLine("Utbetalingsperioder som inneholder flere løpende utgifter:")
+            appendLine()
+            utbetalingsperioderMedFlereUtgifter.forEachIndexed { index, (utbetalingsperiode, utgifter) ->
+                appendLine("${utbetalingsperiode.formatertPeriodeNorskFormat()} inneholder utgiftene:")
+                utgifter.forEachIndexed { utgiftIndex, (type, utgift) ->
+                    append("  - ${utgift.formatertPeriodeNorskFormat()} - ${type.mapType()} (${utgift.utgift} kr)")
+                    if (utgiftIndex + 1 < utgifter.size) {
+                        appendLine()
+                    }
+                }
+                if (index + 1 < utbetalingsperioderMedFlereUtgifter.size) {
+                    appendLine()
+                    appendLine()
+                }
+            }
+        }
     }
 
     return this
 }
+
+private fun TypeBoutgift.mapType() =
+    when (this) {
+        TypeBoutgift.UTGIFTER_OVERNATTING -> error("Skal ikke mappe UTGIFTER_OVERNATTING her")
+        TypeBoutgift.LØPENDE_UTGIFTER_EN_BOLIG -> "en bolig"
+        TypeBoutgift.LØPENDE_UTGIFTER_TO_BOLIGER -> "to boliger"
+    }
--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/beregning/HarIngenUtbetalingsperioderSomOverlapperFlereLøpendeUtgifterValideringTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/beregning/HarIngenUtbetalingsperioderSomOverlapperFlereLøpendeUtgifterValideringTest.kt
@@ -9,27 +9,32 @@ import org.junit.jupiter.api.Test
 import java.time.YearMonth
 
 class HarIngenUtbetalingsperioderSomOverlapperFlereLøpendeUtgifterValideringTest {
-
     val jan = YearMonth.of(2025, 1)
     val feb = YearMonth.of(2025, 2)
     val mars = YearMonth.of(2025, 3)
     val april = YearMonth.of(2025, 3)
 
-    val utbetalingsperioder = listOf(
-        utbetalingsperiode(jan),
-        utbetalingsperiode(feb),
-        utbetalingsperiode(mars),
-        utbetalingsperiode(april),
-    )
-
-    val utgifter: BoutgifterPerUtgiftstype = mapOf(
-        TypeBoutgift.LØPENDE_UTGIFTER_EN_BOLIG to listOf(
-            utgift(jan), utgift(feb)
-        ),
-        TypeBoutgift.LØPENDE_UTGIFTER_TO_BOLIGER to listOf(
-            utgift(jan), utgift(feb)
+    val utbetalingsperioder =
+        listOf(
+            utbetalingsperiode(jan),
+            utbetalingsperiode(feb),
+            utbetalingsperiode(mars),
+            utbetalingsperiode(april),
         )
-    )
+
+    val utgifter: BoutgifterPerUtgiftstype =
+        mapOf(
+            TypeBoutgift.LØPENDE_UTGIFTER_EN_BOLIG to
+                listOf(
+                    utgift(jan),
+                    utgift(feb),
+                ),
+            TypeBoutgift.LØPENDE_UTGIFTER_TO_BOLIGER to
+                listOf(
+                    utgift(jan),
+                    utgift(feb),
+                ),
+        )
 
     @Test
     fun `verifiserer at feilmeldingen blir som forventet`() {
@@ -37,32 +42,41 @@ class HarIngenUtbetalingsperioderSomOverlapperFlereLøpendeUtgifterValideringTes
             utbetalingsperioder.validerIngenUtbetalingsperioderOverlapperFlereLøpendeUtgifter(utgifter)
         }.hasMessage(
             """
-            Vi støtter foreløpig ikke at utbetalingsperioder overlapper mer enn én løpende utgift. 
-            Utbetalingsperioder for denne behandlingen er: [01.01.2025–31.01.2025, 01.02.2025–28.02.2025, 01.03.2025–31.03.2025, 01.03.2025–31.03.2025], 
-            mens utgiftsperiodene er: [01.01.2025–31.01.2025, 01.02.2025–28.02.2025, 01.01.2025–31.01.2025, 01.02.2025–28.02.2025]
-            """.trimIndent()
+            Vi støtter foreløpig ikke at utbetalingsperioder inneholder mer enn én løpende utgift.
+            Utbetalingsperioder som inneholder flere løpende utgifter:
+            
+            01.01.2025–31.01.2025 inneholder utgiftene:
+              - 01.01.2025–31.01.2025 - en bolig (1000 kr)
+              - 01.01.2025–31.01.2025 - to boliger (1000 kr)
+            
+            01.02.2025–28.02.2025 inneholder utgiftene:
+              - 01.02.2025–28.02.2025 - en bolig (1000 kr)
+              - 01.02.2025–28.02.2025 - to boliger (1000 kr)
+            """.trimIndent(),
         )
     }
 
-    private fun utbetalingsperiode(month: YearMonth): UtbetalingPeriode = UtbetalingPeriode(
-        LøpendeMåned(
-            fom = month.atDay(1),
-            tom = month.atEndOfMonth(),
-        ).medVedtaksperiode(
-            VedtaksperiodeInnenforLøpendeMåned(
+    private fun utbetalingsperiode(month: YearMonth): UtbetalingPeriode =
+        UtbetalingPeriode(
+            LøpendeMåned(
                 fom = month.atDay(1),
                 tom = month.atEndOfMonth(),
-                målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
-                aktivitet = AktivitetType.TILTAK,
-            )
-        ),
-        skalAvkorte = false,
-    )
+            ).medVedtaksperiode(
+                VedtaksperiodeInnenforLøpendeMåned(
+                    fom = month.atDay(1),
+                    tom = month.atEndOfMonth(),
+                    målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
+                    aktivitet = AktivitetType.TILTAK,
+                ),
+            ),
+            skalAvkorte = false,
+        )
 
-    private fun utgift(month: YearMonth): UtgiftBeregningBoutgifter = UtgiftBeregningBoutgifter(
-        fom = month.atDay(1),
-        tom = month.atEndOfMonth(),
-        utgift = 1000,
-        skalFåDekketFaktiskeUtgifter = true,
-    )
+    private fun utgift(month: YearMonth): UtgiftBeregningBoutgifter =
+        UtgiftBeregningBoutgifter(
+            fom = month.atDay(1),
+            tom = month.atEndOfMonth(),
+            utgift = 1000,
+            skalFåDekketFaktiskeUtgifter = true,
+        )
 }
