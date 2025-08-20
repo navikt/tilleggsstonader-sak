5e97e310dc3b48371a6c739f5611584ec9fb4467
Andreas Strand <andreas.strand@nav.no>
2025-08-20 12:42:31 +0200

    Funksjon i BehandlingRepository.kt for å hente ut behandlinger som skal satsjusteres

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/satsjustering/FinnBehandlingerForSatsjusteringService.kt
@@ -0,0 +1,10 @@
+package no.nav.tilleggsstonader.sak.satsjustering
+
+import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TilkjentYtelseRepository
+
+class FinnBehandlingerForSatsjusteringService(
+    private val tilkjentYtelseRepository: TilkjentYtelseRepository,
+) {
+    fun sjekkBehandlingerForSatsjustering() {
+    }
+}
