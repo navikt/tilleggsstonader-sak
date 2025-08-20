c52d3019d38e827fb4f6d1ce1588b18a8e07f642
Johan Blomgren <blommish@users.noreply.github.com>
2025-08-20 14:47:54 +0200

    Kast RekjørSenereException hvis det finnes en behandling under arbeid

--- src/main/kotlin/no/nav/tilleggsstonader/sak/satsjustering/SatsjusteringService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/satsjustering/SatsjusteringService.kt
@@ -1,10 +1,12 @@
 package no.nav.tilleggsstonader.sak.satsjustering
 
+import no.nav.familie.prosessering.error.RekjørSenereException
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 
 @Service
 class SatsjusteringService(
@@ -18,7 +20,13 @@ class SatsjusteringService(
         val fagsakId = behandlingSomTrengerSatsjustering.fagsakId
         if (behandlingService.finnesIkkeFerdigstiltBehandling(fagsakId)) {
             logger.info("Finnes en ikke ferdigstilt behandling for fagsakId=$fagsakId, kan ikke kjøre satsjustering.")
-            return
+            /**
+             * TODO
+             * Hvis man kun skal ha en task per behandling, og at cron-job sjekker for at det ikke finnes en task for den behandlingId
+             * så kan man kaste RekjørSenereException her, og tasken er ansvarlig for å kjøre på nytt dagen etter.
+             */
+            throw RekjørSenereException("Fagsak=${fagsakId} har en ikke ferdigstilt behandling, kan ikke kjøre satsjustering.",
+                LocalDate.now().plusDays(1).atTime(7,0))
         }
         val sisteIverksatteBehandling = behandlingService.finnSisteIverksatteBehandling(fagsakId)
         if (sisteIverksatteBehandling?.id != behandlingId) {
