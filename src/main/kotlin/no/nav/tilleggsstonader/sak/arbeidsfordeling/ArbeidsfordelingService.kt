fdecb1f696583c0c4fd644ed393538e6a352743b
Sigmund Berbom <sberbom@gmail.com>
2025-08-21 10:28:10 +0200

    🐛 Legg alle behandle-sak oppgaver for daglig reise tsr på Oslo enhenten, ikke "vurder henvedelse"-saker

--- src/main/kotlin/no/nav/tilleggsstonader/sak/arbeidsfordeling/ArbeidsfordelingService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/arbeidsfordeling/ArbeidsfordelingService.kt
@@ -46,6 +46,12 @@ class ArbeidsfordelingService(
         ident: String,
         stønadstype: Stønadstype,
     ): Arbeidsfordelingsenhet? {
+        if (stønadstype == Stønadstype.DAGLIG_REISE_TSR) {
+            return Arbeidsfordelingsenhet(
+                enhetNr = "0387",
+                navn = "0387 - Nav tiltak Oslo",
+            )
+        }
         val kriterie = lagArbeidsfordelingKritierieForPerson(ident, stønadstype)
         val enheter = finnArbeidsfordelingsenhet(kriterie)
         return enheter.firstOrNull()
@@ -56,12 +62,6 @@ class ArbeidsfordelingService(
         stønadstype: Stønadstype,
         oppgavetype: Oppgavetype,
     ): Arbeidsfordelingsenhet? {
-        if (stønadstype == Stønadstype.DAGLIG_REISE_TSR) {
-            return Arbeidsfordelingsenhet(
-                enhetNr = "0387",
-                navn = "0387 - Nav tiltak Oslo",
-            )
-        }
         val arbeidsfordelingskriterie =
             lagArbeidsfordelingKritierieForPerson(
                 personIdent = ident,
