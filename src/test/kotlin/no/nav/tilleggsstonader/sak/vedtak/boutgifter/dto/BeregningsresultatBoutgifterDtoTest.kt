dfe9b0ce8730bbe7f8b665716407757a0628d8d3
Sigmund Berbom <sberbom@gmail.com>
2025-08-25 16:21:46 +0200

    🏷️ Legg til `inneholderUtgifterOvernatting` i `BeregningsresultatForPeriodeDto` for å kunne vise rett utbetalingplan for vedtak med både midlertidig og løpende utgifter

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/dto/BeregningsresultatBoutgifterDtoTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/boutgifter/dto/BeregningsresultatBoutgifterDtoTest.kt
@@ -7,6 +7,7 @@ import no.nav.tilleggsstonader.sak.vedtak.boutgifter.domain.BeregningsresultatFo
 import no.nav.tilleggsstonader.sak.vedtak.domain.TypeBoutgift
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -229,5 +230,132 @@ class BeregningsresultatBoutgifterDtoTest {
             beregningsresultatForLøpendeMåned.finnUtgifterMedAndelTilUtbetaling(revurderFra)
 
         assertEquals(forventetResultat, result)
+    }
+
+    @Nested
+    inner class BeregningsresultatForLøpendeMånedTilDto {
+        @Test
+        fun `skal vise om perioden inneholder utgift til overnatting`() {
+            val utgiftOvernatting =
+                listOf(
+                    lagUtgiftBeregningBoutgifter(
+                        fom = LocalDate.of(2023, 1, 1),
+                        tom = LocalDate.of(2023, 1, 5),
+                        utgift = 3000,
+                        skalFåDekketFaktiskeUtgifter = false,
+                    ),
+                )
+
+            val beregningsresultatForLøpendeMåned =
+                BeregningsresultatForLøpendeMåned(
+                    grunnlag =
+                        Beregningsgrunnlag(
+                            fom = LocalDate.of(2023, 1, 1),
+                            tom = LocalDate.of(2023, 1, 31),
+                            utgifter = mapOf(TypeBoutgift.UTGIFTER_OVERNATTING to utgiftOvernatting),
+                            makssats = 4953,
+                            makssatsBekreftet = true,
+                            målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
+                            aktivitet = AktivitetType.TILTAK,
+                        ),
+                    stønadsbeløp = 3000,
+                )
+
+            val forventetResultat =
+                BeregningsresultatForPeriodeDto(
+                    fom = LocalDate.of(2023, 1, 1),
+                    tom = LocalDate.of(2023, 1, 31),
+                    stønadsbeløp = 3000,
+                    utgifter =
+                        listOf(
+                            UtgiftBoutgifterMedAndelTilUtbetalingDto(
+                                fom = LocalDate.of(2023, 1, 1),
+                                tom = LocalDate.of(2023, 1, 5),
+                                utgift = 3000,
+                                tilUtbetaling = 3000,
+                                erFørRevurderFra = true,
+                                skalFåDekketFaktiskeUtgifter = false,
+                            ),
+                        ),
+                    sumUtgifter = 3000,
+                    målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
+                    aktivitet = AktivitetType.TILTAK,
+                    makssatsBekreftet = true,
+                    delAvTidligereUtbetaling = false,
+                    skalFåDekketFaktiskeUtgifter = false,
+                    inneholderUtgifterOvernatting = true,
+                )
+
+            val revurderFra = LocalDate.of(2023, 1, 10)
+
+            val result =
+                beregningsresultatForLøpendeMåned.tilDto(revurderFra)
+
+            assertEquals(forventetResultat, result)
+        }
+
+        @Test
+        fun `skal vise om perioden ikke inneholder utgift til overnatting`() {
+            val utgift =
+                listOf(
+                    lagUtgiftBeregningBoutgifter(
+                        fom = LocalDate.of(2023, 1, 1),
+                        tom = LocalDate.of(2023, 1, 31),
+                        utgift = 3000,
+                        skalFåDekketFaktiskeUtgifter = false,
+                    ),
+                )
+
+            val beregningsresultatForLøpendeMåned =
+                BeregningsresultatForLøpendeMåned(
+                    grunnlag =
+                        Beregningsgrunnlag(
+                            fom = LocalDate.of(2023, 1, 1),
+                            tom = LocalDate.of(2023, 1, 31),
+                            utgifter =
+                                mapOf(
+                                    TypeBoutgift.LØPENDE_UTGIFTER_EN_BOLIG to utgift,
+                                    TypeBoutgift.UTGIFTER_OVERNATTING to emptyList(),
+                                ),
+                            makssats = 4953,
+                            makssatsBekreftet = true,
+                            målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
+                            aktivitet = AktivitetType.TILTAK,
+                        ),
+                    stønadsbeløp = 3000,
+                )
+
+            val forventetResultat =
+                BeregningsresultatForPeriodeDto(
+                    fom = LocalDate.of(2023, 1, 1),
+                    tom = LocalDate.of(2023, 1, 31),
+                    stønadsbeløp = 3000,
+                    utgifter =
+                        listOf(
+                            UtgiftBoutgifterMedAndelTilUtbetalingDto(
+                                fom = LocalDate.of(2023, 1, 1),
+                                tom = LocalDate.of(2023, 1, 31),
+                                utgift = 3000,
+                                tilUtbetaling = 3000,
+                                erFørRevurderFra = false,
+                                skalFåDekketFaktiskeUtgifter = false,
+                            ),
+                        ),
+                    sumUtgifter = 3000,
+                    målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
+                    aktivitet = AktivitetType.TILTAK,
+                    makssatsBekreftet = true,
+                    delAvTidligereUtbetaling = false,
+                    skalFåDekketFaktiskeUtgifter = false,
+                    inneholderUtgifterOvernatting = false,
+                )
+
+            val revurderFra = LocalDate.of(2023, 1, 10)
+
+            val result =
+                beregningsresultatForLøpendeMåned.tilDto(revurderFra)
+
+            assertEquals(forventetResultat, result)
+        }
     }
 }
