c5470ec77ae2de9b9bfd543a2de17bde9d68fcc3
Andreas Strand <andreas.strand@nav.no>
2025-09-01 21:33:23 +0200

    Riktig stauts i flyway migrering

--- src/main/resources/db/migration/V94__oppgave_status.sql
+++ src/main/resources/db/migration/V94__oppgave_status.sql
@@ -1,5 +1,5 @@
 ALTER TABLE oppgave ADD COLUMN status VARCHAR(50);
-UPDATE oppgave SET status='OPPRETTET' WHERE er_ferdigstilt = false;
+UPDATE oppgave SET status='ÅPEN' WHERE er_ferdigstilt = false;
 UPDATE oppgave SET status='FERDIGSTILT' WHERE er_ferdigstilt = true;
 ALTER TABLE oppgave DROP COLUMN er_ferdigstilt;
 
