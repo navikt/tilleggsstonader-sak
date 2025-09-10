29de47138ad01c1db8c36e5ba1f2edb38360ecb8
Runar Skagestad <runar.skagestad@nav.no>
2025-09-10 13:52:38 +0200

    ♻️ Legg inn endepunkt for å foreslå vedtaksperioder under en path som ikke er stønadsspesifikk

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/VedtakController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/VedtakController.kt
@@ -1,10 +1,14 @@
 package no.nav.tilleggsstonader.sak.vedtak
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
 import no.nav.tilleggsstonader.sak.tilgang.AuditLoggerEvent
 import no.nav.tilleggsstonader.sak.tilgang.TilgangService
+import no.nav.tilleggsstonader.sak.vedtak.dto.LagretVedtaksperiodeDto
 import no.nav.tilleggsstonader.sak.vedtak.dto.SluttdatoForVedtakDto
+import no.nav.tilleggsstonader.sak.vedtak.dto.tilLagretVedtaksperiodeDto
+import no.nav.tilleggsstonader.sak.vedtak.forslag.ForeslåVedtaksperiodeService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController
 class VedtakController(
     private val tilgangService: TilgangService,
     private val vedtakService: VedtakService,
+    private val behandlingService: BehandlingService,
+    private val foreslåVedtaksperiodeService: ForeslåVedtaksperiodeService,
 ) {
     @GetMapping("{behandlingId}/sluttdato")
     fun hentSluttdatoForVedtakPåBehandling(
@@ -26,6 +32,25 @@ class VedtakController(
 
         return SluttdatoForVedtakDto(
             sluttdato = vedtakService.hentVedtak(behandlingId)?.vedtaksperioderHvisFinnes()?.maxOfOrNull { it.tom },
+        )
+    }
+
+    @GetMapping("{behandlingId}/foresla")
+    fun foreslåVedtaksperioder(
+        @PathVariable behandlingId: BehandlingId,
+    ): List<LagretVedtaksperiodeDto> {
+        tilgangService.settBehandlingsdetaljerForRequest(behandlingId)
+        tilgangService.validerTilgangTilBehandling(behandlingId, AuditLoggerEvent.ACCESS)
+        tilgangService.validerHarSaksbehandlerrolle()
+
+        val behandling = behandlingService.hentBehandling(behandlingId)
+        val forrigeVedtaksperioder =
+            behandling.forrigeIverksatteBehandlingId?.let {
+                vedtakService.hentVedtaksperioder(it)
+            }
+
+        return foreslåVedtaksperiodeService.foreslåPerioder(behandlingId).tilLagretVedtaksperiodeDto(
+            tidligereVedtaksperioder = forrigeVedtaksperioder,
         )
     }
 }
