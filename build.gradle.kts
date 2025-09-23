27b4d393b66413edda0fac8db95e04dfc51639bd
Andreas Strand <andreas.strand@nav.no>
2025-09-23 11:24:18 +0200

    org.cyclonedx.bom 2.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.spring") version "2.2.20"
 
-    id("org.cyclonedx.bom") version "2.4.0"
+    id("org.cyclonedx.bom") version "2.4.1"
 }
 
 repositories {
