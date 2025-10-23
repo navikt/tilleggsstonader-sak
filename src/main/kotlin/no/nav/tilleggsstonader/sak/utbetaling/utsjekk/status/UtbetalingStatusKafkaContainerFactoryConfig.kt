9034417d4c8bf10ef980117c2541a4f86b5e1d0a
Runar Skagestad <runar.skagestad@nav.no>
2025-10-23 13:41:52 +0200

    🩹 Skru av status-endepunktet i prod midlertidig. Det er noe mismatch med møklene vi får fra helved på eksisterende utbetalinger

--- src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/utsjekk/status/UtbetalingStatusKafkaContainerFactoryConfig.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/utsjekk/status/UtbetalingStatusKafkaContainerFactoryConfig.kt
@@ -16,7 +16,7 @@ import org.springframework.kafka.support.serializer.JsonDeserializer
 import java.time.Duration
 
 @Configuration
-@Profile("!local & !integrasjonstest")
+@Profile("!local & !integrasjonstestc & !prod")
 class UtbetalingStatusKafkaContainerFactoryConfig {
     @Bean
     fun utbetalingStatusListenerContainerFactory(
