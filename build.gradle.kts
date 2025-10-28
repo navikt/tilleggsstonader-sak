20d66a87dc9dc9280a8412468796faeb693a640c
Sigmund Berbom <sberbom@gmail.com>
2025-10-28 12:39:24 +0100

    ⬆️ Oppdater kontrakter versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.io.ByteArrayOutputStream
 val javaVersion = JavaLanguageVersion.of(21)
 val familieProsesseringVersion = "2.20250922094930_4bb329c"
 val tilleggsstønaderLibsVersion = "2025.09.11-09.26.d3123ecc47ce"
-val tilleggsstønaderKontrakterVersion = "2025.10.27-16.29.f2aaf9780904"
+val tilleggsstønaderKontrakterVersion = "2025.10.28-12.30.376dfee2911b"
 val avroVersion = "1.12.0"
 val confluentVersion = "8.0.1"
 val joarkHendelseVersion = "1.1.6"
