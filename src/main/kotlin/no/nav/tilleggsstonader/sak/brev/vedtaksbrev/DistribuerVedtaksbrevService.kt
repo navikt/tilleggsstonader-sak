fbf1605c75c042ffba501ff52ba5ff74619cc805
Andreas Strand <andreas.strand@nav.no>
2025-11-05 11:36:19 +0100

    Fjern henting av dødsbo fra pdl

--- src/main/kotlin/no/nav/tilleggsstonader/sak/brev/vedtaksbrev/DistribuerVedtaksbrevService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/brev/vedtaksbrev/DistribuerVedtaksbrevService.kt
@@ -1,17 +1,13 @@
 package no.nav.tilleggsstonader.sak.brev.vedtaksbrev
 
-import no.nav.tilleggsstonader.kontrakter.dokdist.AdresseType
 import no.nav.tilleggsstonader.kontrakter.dokdist.DistribuerJournalpostRequest
 import no.nav.tilleggsstonader.kontrakter.dokdist.Distribusjonstype
-import no.nav.tilleggsstonader.kontrakter.dokdist.ManuellAdresse
 import no.nav.tilleggsstonader.kontrakter.felles.Fagsystem
 import no.nav.tilleggsstonader.libs.http.client.ProblemDetailException
 import no.nav.tilleggsstonader.sak.brev.brevmottaker.BrevmottakerVedtaksbrevRepository
 import no.nav.tilleggsstonader.sak.brev.brevmottaker.domain.BrevmottakerVedtaksbrev
 import no.nav.tilleggsstonader.sak.infrastruktur.felles.TransactionHandler
 import no.nav.tilleggsstonader.sak.journalføring.JournalpostClient
-import no.nav.tilleggsstonader.sak.opplysninger.pdl.PersonService
-import no.nav.tilleggsstonader.sak.opplysninger.pdl.dto.KontaktinformasjonForDoedsbo
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -21,7 +17,6 @@ import org.springframework.web.client.HttpClientErrorException
 class DistribuerVedtaksbrevService(
     private val journalpostClient: JournalpostClient,
     private val brevmottakerVedtaksbrevRepository: BrevmottakerVedtaksbrevRepository,
-    private val personService: PersonService,
     private val transactionHandler: TransactionHandler,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
@@ -41,20 +36,15 @@ class DistribuerVedtaksbrevService(
         } catch (ex: ProblemDetailException) {
             logger.warn("Distribusjon av vedtaksbrev for journalpost ${mottaker.journalpostId} feilet: ${ex.message}")
             if (ex.responseException is HttpClientErrorException.Gone) {
-                logger.warn(
-                    "Kan ikke sende vedtaksbrev da personen er død og mangler adresse. Forsøker å sende til kontaktinformasjon for dødsboet.",
-                )
-                return distribuerVedtaksbrevTilKontaktpersonForDødsbo(dødMottaker = mottaker, opprinneligFeilmelding = ex.message)
+                logger.warn("Kan ikke sende vedtaksbrev da personen er død og mangler adresse.")
+                return ResultatBrevutsendelse.FeiletFordiMottakerErDødOgManglerAdresse("${ex.message}")
             } else {
                 throw ex
             }
         }
     }
 
-    private fun distribuerVedtaksbrevOgOppdaterMottakerHvisSuksess(
-        mottaker: BrevmottakerVedtaksbrev,
-        manuellAdresse: ManuellAdresse? = null,
-    ): ResultatBrevutsendelse {
+    private fun distribuerVedtaksbrevOgOppdaterMottakerHvisSuksess(mottaker: BrevmottakerVedtaksbrev): ResultatBrevutsendelse {
         val bestillingsId =
             journalpostClient.distribuerJournalpost(
                 DistribuerJournalpostRequest(
@@ -62,7 +52,6 @@ class DistribuerVedtaksbrevService(
                     bestillendeFagsystem = Fagsystem.TILLEGGSSTONADER,
                     dokumentProdApp = "TILLEGGSSTONADER-SAK",
                     distribusjonstype = Distribusjonstype.VEDTAK,
-                    adresse = manuellAdresse,
                 ),
             )
         mottaker.lagreDistribusjonGjennomført(bestillingsId)
@@ -70,38 +59,6 @@ class DistribuerVedtaksbrevService(
             "Distribuert vedtaksbrev (journalpost=${mottaker.journalpostId}) med bestillingId=$bestillingsId",
         )
         return ResultatBrevutsendelse.BrevDistribuert
-    }
-
-    private fun distribuerVedtaksbrevTilKontaktpersonForDødsbo(
-        dødMottaker: BrevmottakerVedtaksbrev,
-        opprinneligFeilmelding: String? = null,
-    ): ResultatBrevutsendelse {
-        val kontaktinformasjonDødsbo = personService.hentSøker(dødMottaker.mottaker.ident).kontaktinformasjonForDoedsbo.firstOrNull()
-
-        if (kontaktinformasjonDødsbo == null) {
-            logger.warn("Fant ikke kontaktinformasjon for dødsboet. Kan ikke sende ut vedtaksbrev.")
-            return ResultatBrevutsendelse.FeiletFordiMottakerErDødOgManglerAdresse(
-                "$opprinneligFeilmelding - fant heller ikke kontaktinformasjon for dødsboet",
-            )
-        }
-
-        return distribuerVedtaksbrevOgOppdaterMottakerHvisSuksess(
-            mottaker = dødMottaker,
-            manuellAdresse = kontaktinformasjonDødsbo.tilManuellAdresse(),
-        )
-    }
-
-    private fun KontaktinformasjonForDoedsbo.tilManuellAdresse(): ManuellAdresse {
-        val landkode = adresse.landkode ?: "NO"
-        val adresseType = if (landkode == "NO") AdresseType.norskPostadresse else AdresseType.utenlandskPostadresse
-        return ManuellAdresse(
-            adresseType = adresseType,
-            adresselinje1 = adresse.adresselinje1,
-            adresselinje2 = adresse.adresselinje2,
-            postnummer = adresse.postnummer,
-            poststed = adresse.poststedsnavn,
-            land = landkode,
-        )
     }
 
     private fun BrevmottakerVedtaksbrev.lagreDistribusjonGjennomført(bestillingId: String) {
