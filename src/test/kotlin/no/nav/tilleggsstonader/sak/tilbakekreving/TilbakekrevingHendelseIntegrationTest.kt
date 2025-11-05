ab83a8b262784ed22d0f913c9e83961156f03475
Andreas Strand <andreas.strand@nav.no>
2025-11-05 14:57:30 +0100

    Skriv om tester for tilbakekreving til integrasjonstester slik at det blir enklere å endre for nye tilbakekrevingshendelser

---
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/tilbakekreving/TilbakekrevingHendelseIntegrationTest.kt
@@ -0,0 +1,179 @@
+package no.nav.tilleggsstonader.sak.tilbakekreving
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.mockk.verify
+import no.nav.tilleggsstonader.kontrakter.felles.ObjectMapperProvider.objectMapper
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.libs.utils.dato.januar
+import no.nav.tilleggsstonader.libs.utils.dato.mai
+import no.nav.tilleggsstonader.sak.IntegrationTest
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingRepository
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingResultat
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
+import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
+import no.nav.tilleggsstonader.sak.fagsak.FagsakService
+import no.nav.tilleggsstonader.sak.fagsak.domain.Fagsak
+import no.nav.tilleggsstonader.sak.hendelser.ConsumerRecordUtil
+import no.nav.tilleggsstonader.sak.tilbakekreving.hendelse.TilbakekrevingFagsysteminfoBehov
+import no.nav.tilleggsstonader.sak.tilbakekreving.hendelse.TilbakekrevingFagsysteminfoSvar
+import no.nav.tilleggsstonader.sak.util.behandling
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.LæremidlerBeregnYtelseSteg
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.LæremidlerTestUtil.vedtaksperiodeDto
+import no.nav.tilleggsstonader.sak.vedtak.læremidler.dto.InnvilgelseLæremidlerRequest
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.aktivitet
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.faktaOgVurderingAktivitetLæremidler
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.faktaOgVurderingMålgruppeLæremidler
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeTestUtil.målgruppe
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.VilkårperiodeRepository
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.KafkaTemplate
+import java.time.LocalDateTime
+import java.util.UUID
+import kotlin.random.Random
+
+class TilbakekrevingHendelseIntegrationTest : IntegrationTest() {
+    @Autowired
+    lateinit var vilkårsperiodeRepository: VilkårperiodeRepository
+
+    @Autowired
+    lateinit var læremidlerBeregnYtelseSteg: LæremidlerBeregnYtelseSteg
+
+    @Autowired
+    lateinit var behandlingRepository: BehandlingRepository
+
+    @Autowired
+    lateinit var tilbakekrevingHendelseDelegate: TilbakekrevingHendelseDelegate
+
+    @Autowired
+    lateinit var fagsakService: FagsakService
+
+    @Autowired
+    lateinit var kafkaTemplate: KafkaTemplate<String, String>
+
+    lateinit var forrigeBehandling: Saksbehandling
+    lateinit var behandling: Saksbehandling
+    lateinit var fagsak: Fagsak
+
+    val publiserteHendelser = mutableListOf<ProducerRecord<String, String>>()
+
+    @BeforeEach
+    fun setUp() {
+        val forrigeBehandling =
+            testoppsettService.opprettBehandlingMedFagsak(
+                behandling =
+                    behandling(
+                        type = BehandlingType.FØRSTEGANGSBEHANDLING,
+                        resultat = BehandlingResultat.INNVILGET,
+                        vedtakstidspunkt = LocalDateTime.now().minusDays(2),
+                    ),
+                stønadstype = Stønadstype.LÆREMIDLER,
+            )
+        fagsak = testoppsettService.hentFagsak(forrigeBehandling.fagsakId)
+
+        val fom = 1 januar 2025
+        val tom = 31 mai 2025
+
+        vilkårsperiodeRepository.insertAll(
+            listOf(
+                målgruppe(
+                    behandlingId = forrigeBehandling.id,
+                    fom = fom,
+                    tom = tom,
+                    faktaOgVurdering = faktaOgVurderingMålgruppeLæremidler(),
+                ),
+                aktivitet(
+                    behandlingId = forrigeBehandling.id,
+                    fom = fom,
+                    tom = tom,
+                    faktaOgVurdering = faktaOgVurderingAktivitetLæremidler(),
+                ),
+            ),
+        )
+
+        læremidlerBeregnYtelseSteg.utførSteg(
+            saksbehandling = behandlingRepository.finnSaksbehandling(forrigeBehandling.id),
+            InnvilgelseLæremidlerRequest(
+                vedtaksperioder = listOf(vedtaksperiodeDto(fom = fom, tom = tom)),
+            ),
+        )
+
+        testoppsettService.ferdigstillBehandling(forrigeBehandling)
+
+        val behandling =
+            behandling(
+                fagsak = fagsak,
+                type = BehandlingType.REVURDERING,
+                resultat = BehandlingResultat.OPPHØRT,
+                vedtakstidspunkt = LocalDateTime.now().minusDays(2),
+                forrigeIverksatteBehandlingId = forrigeBehandling.id,
+            )
+        testoppsettService.lagre(behandling)
+        testoppsettService.lagOpphørVedtak(behandling)
+        testoppsettService.ferdigstillBehandling(behandling)
+
+        this.forrigeBehandling = testoppsettService.hentSaksbehandling(forrigeBehandling.id)
+        this.behandling = testoppsettService.hentSaksbehandling(behandling.id)
+    }
+
+    @Test
+    fun `mottar ukjent hendelsestype, gjør ingenting`() {
+        val payload = mapOf("hendelsestype" to "ukjent_type")
+        publiserTilbakekrevinghendelse(UUID.randomUUID().toString(), payload)
+
+        verify(exactly = 0) { kafkaTemplate.send(any<ProducerRecord<String, String>>()) }
+    }
+
+    @Nested
+    inner class FagsysteminfoBehov {
+        @Test
+        fun `mottar hendelsestype fagsysteminfo_behov med ukjent fagsystemId, gjør ingenting`() {
+            val tilbakekrevingFagsysteminfoBehov =
+                TilbakekrevingFagsysteminfoBehov(
+                    eksternFagsakId = Random.nextLong(10, 10000).toString(),
+                    kravgrunnlagReferanse = "10",
+                    hendelseOpprettet = LocalDateTime.now(),
+                    versjon = 1,
+                )
+
+            publiserTilbakekrevinghendelse(UUID.randomUUID().toString(), tilbakekrevingFagsysteminfoBehov)
+
+            verify(exactly = 0) { kafkaTemplate.send(any<ProducerRecord<String, String>>()) }
+        }
+
+        @Test
+        fun `mottar hendelsestype fagsysteminfo_behov, behandling har vedtak, publiserer andeler på topic`() {
+            val key = UUID.randomUUID().toString()
+            val payload =
+                TilbakekrevingFagsysteminfoBehov(
+                    eksternFagsakId = behandling.eksternFagsakId.toString(),
+                    kravgrunnlagReferanse = behandling.eksternId.toString(),
+                    hendelseOpprettet = LocalDateTime.now(),
+                    versjon = 1,
+                )
+
+            publiserTilbakekrevinghendelse(key, payload)
+
+            verify { kafkaTemplate.send(capture(publiserteHendelser)) }
+            assertThat(publiserteHendelser).hasSize(1)
+            val publisertHendelse = publiserteHendelser.single()
+            assertThat(publisertHendelse.key()).isEqualTo(key)
+            val tilbakekrevingFagsysteminfoSvar = objectMapper.readValue<TilbakekrevingFagsysteminfoSvar>(publisertHendelse.value())
+            assertThat(tilbakekrevingFagsysteminfoSvar.utvidPerioder).isNotEmpty
+            println(tilbakekrevingFagsysteminfoSvar)
+        }
+    }
+
+    private fun publiserTilbakekrevinghendelse(
+        key: String,
+        payload: Any,
+    ) {
+        tilbakekrevingHendelseDelegate.håndter(
+            ConsumerRecordUtil.lagConsumerRecord(key, objectMapper.writeValueAsString(payload)),
+        )
+    }
+}
