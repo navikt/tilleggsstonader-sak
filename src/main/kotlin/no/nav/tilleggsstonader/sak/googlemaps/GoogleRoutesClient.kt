0428b06100030f28dfa9887318cf2fdeb53d921b
Sigmund Berbom <sberbom@gmail.com>
2025-11-26 15:30:10 +0100

    ♻️ Ta i bruk `onStatus` fra `RestClient` for enklere kode

--- src/main/kotlin/no/nav/tilleggsstonader/sak/googlemaps/GoogleRoutesClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/googlemaps/GoogleRoutesClient.kt
@@ -1,10 +1,13 @@
 package no.nav.tilleggsstonader.sak.googlemaps
 
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeil
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.feil
 import no.nav.tilleggsstonader.sak.opplysninger.pdl.logger
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpRequest
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.client.ClientHttpResponse
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
 import org.springframework.web.client.bodyWithType
@@ -21,25 +24,28 @@ class GoogleRoutesClient(
     private val uri = UriComponentsBuilder.fromUri(baseUrl).encode().toUriString()
 
     fun hentRuter(request: RuteRequest): RuteResponse? =
-        try {
-            restClient
-                .post()
-                .uri(uri)
-                .headers { headers ->
-                    headers.apply {
-                        add("X-Goog-Api-Key", apiKey)
-                        add("X-Goog-FieldMask", "*")
-                        add("Content-Type", "application/json")
-                    }
-                }.bodyWithType(request)
-                .retrieve()
-                .body<RuteResponse>()
-        } catch (e: HttpClientErrorException.NotFound) {
-            if (e.message?.contains("Address not found") == true) {
-                logger.warn(e.message)
-                brukerfeil("Kunne ikke finne adressen")
-            } else {
-                throw e
-            }
+        restClient
+            .post()
+            .uri(uri)
+            .headers { headers ->
+                headers.apply {
+                    add("X-Goog-Api-Key", apiKey)
+                    add("X-Goog-FieldMask", "*")
+                    add("Content-Type", "application/json")
+                }
+            }.bodyWithType(request)
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError, clientErrorHandler)
+            .body<RuteResponse>()
+
+    private val clientErrorHandler: (HttpRequest, ClientHttpResponse) -> Unit = { _, response ->
+        val body = String(response.body.readAllBytes())
+        if (body.contains("Address not found")) {
+            logger.warn(body)
+            brukerfeil("Kunne ikke finne adressen")
+        } else {
+            logger.error(body)
+            feil("Kunne ikke finne ruteforslag")
         }
+    }
 }
