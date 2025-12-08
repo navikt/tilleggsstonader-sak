e8ea4a04b08a83c5df0116b9015857eef876f865
Filip Larsen <filip.christoffer.larsen@nav.no>
2025-12-08 15:41:16 +0100

    Flytter sortering av reiser inn i BeregningsresultatOffentligTransport

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/domain/BeregningsresultatDagligReise.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/domain/BeregningsresultatDagligReise.kt
@@ -3,17 +3,4 @@ package no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain
 data class BeregningsresultatDagligReise(
     val offentligTransport: BeregningsresultatOffentligTransport?,
     // val privatBil: BeregningsresultatPrivatBil?,
-) {
-    fun sorterReiser(): BeregningsresultatDagligReise {
-        val sortedOffentligTransport =
-            offentligTransport?.copy(
-                reiser =
-                    offentligTransport.reiser
-                        .sortedBy { reise -> reise.perioder.minOf { it.grunnlag.fom } }
-                        .map { reise ->
-                            reise.copy(perioder = reise.perioder.sortedBy { it.grunnlag.fom })
-                        },
-            )
-        return this.copy(offentligTransport = sortedOffentligTransport)
-    }
-}
+)
