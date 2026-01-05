bf05484e12403cbd190ffb7ebe99885dde0065af
Andreas Strand <andreas.strand@nav.no>
2026-01-05 14:45:53 +0100

    Legger til endepunkt for å kunne migrere over utbetalinger fra v2-endepunkt hos helved til å bruke kafka-grensesnittet

--- src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/iverksetting/IverksettClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/iverksetting/IverksettClient.kt
@@ -31,6 +31,13 @@ class IverksettClient(
             .encode()
             .toUriString()
 
+    private val migreringUrl =
+        UriComponentsBuilder
+            .fromUri(uri)
+            .pathSegment("api", "iverksetting", "v2", "migrate")
+            .encode()
+            .toUriString()
+
     fun iverksett(dto: IverksettDto) {
         try {
             postForEntityNullable<Void>(iverksettingUrl, dto)
@@ -41,6 +48,10 @@ class IverksettClient(
             }
             throw e
         }
+    }
+
+    fun migrer(dto: MigrerUtbetalingDto) {
+        postForEntityNullable<Void>(migreringUrl, dto)
     }
 
     fun hentStatus(
