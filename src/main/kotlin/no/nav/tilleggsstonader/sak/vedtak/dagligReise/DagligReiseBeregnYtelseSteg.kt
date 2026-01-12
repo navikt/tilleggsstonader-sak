b82f34ff4df495880313af28aa5c31aa7f5d1f9d
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-01-12 10:59:52 +0100

    Lagrer ikke andeler, men hopper over steg dersom det kun er rammevedtak privat bil

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/DagligReiseBeregnYtelseSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/DagligReiseBeregnYtelseSteg.kt
@@ -4,18 +4,13 @@ import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
 import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
+import no.nav.tilleggsstonader.sak.infrastruktur.exception.feilHvis
 import no.nav.tilleggsstonader.sak.utbetaling.simulering.SimuleringService
 import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.TilkjentYtelseService
-import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.AndelTilkjentYtelse
-import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.Satstype
-import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TypeAndel
-import no.nav.tilleggsstonader.sak.util.datoEllerNesteMandagHvisLørdagEllerSøndag
 import no.nav.tilleggsstonader.sak.vedtak.VedtakRepository
-import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.BeregningsresultatPrivatBil
 import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseDagligReise
 import no.nav.tilleggsstonader.sak.vedtak.domain.VedtakUtil.withTypeOrThrow
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 
 @Service
 class DagligReiseBeregnYtelseSteg(
@@ -42,39 +37,15 @@ class DagligReiseBeregnYtelseSteg(
         val vedtak =
             vedtakRepository.findByIdOrThrow(saksbehandling.id).withTypeOrThrow<InnvilgelseDagligReise>()
 
-        val beregningsresultat = vedtak.data.beregningsresultat
+        val beregningsresultatOffentligTransport = vedtak.data.beregningsresultat?.offentligTransport
 
-        val andeler =
-            listOfNotNull(
-                beregningsresultat?.privatBil?.mapTilAndelTilkjentYtelse(saksbehandling),
-                beregningsresultat?.offentligTransport?.mapTilAndelTilkjentYtelse(saksbehandling),
-            ).flatten()
-
-        if (andeler.isNotEmpty()) {
-            tilkjentYtelseService.lagreTilkjentYtelse(
-                behandlingId = saksbehandling.id,
-                andeler = andeler,
-            )
+        feilHvis(beregningsresultatOffentligTransport == null) {
+            "Foreløpig støttes kun beregning av offentlig transport."
         }
-    }
 
-    private fun BeregningsresultatPrivatBil.mapTilAndelTilkjentYtelse(saksbehandling: Saksbehandling): List<AndelTilkjentYtelse> {
-        // Mock implementation for testing
-        return reiser.flatMap { reise ->
-            reise.uker.map { (grunnlag, stønadsbeløp) ->
-                // TODO - skal stønadsbeløp være 0 siden rammen er et 0-vedtak?
-                // TODO - hvilke datoer skal settes?
-                AndelTilkjentYtelse(
-                    beløp = 0,
-                    fom = LocalDate.now(),
-                    tom = LocalDate.now(),
-                    satstype = Satstype.DAG,
-                    type = TypeAndel.DAGLIG_REISE_AAP,
-                    kildeBehandlingId = saksbehandling.id,
-                    utbetalingsdato = LocalDate.now(),
-                    brukersNavKontor = "Mock",
-                )
-            }
-        }
+        tilkjentYtelseService.lagreTilkjentYtelse(
+            behandlingId = saksbehandling.id,
+            andeler = beregningsresultatOffentligTransport.mapTilAndelTilkjentYtelse(saksbehandling = saksbehandling),
+        )
     }
 }
--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/beregning/KjorelisteSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/beregning/KjorelisteSteg.kt
@@ -5,11 +5,16 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.findByIdOrThrow
+import no.nav.tilleggsstonader.sak.vedtak.VedtakRepository
+import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseEllerOpphørDagligReise
+import no.nav.tilleggsstonader.sak.vedtak.domain.VedtakUtil.withTypeOrThrow
 import org.springframework.stereotype.Service
 
 @Service
 class KjorelisteSteg(
     private val behandlingService: BehandlingService,
+    private val vedtakRepository: VedtakRepository,
 ) : BehandlingSteg<Void?> {
     override fun utførSteg(
         saksbehandling: Saksbehandling,
@@ -18,6 +23,24 @@ class KjorelisteSteg(
         if (saksbehandling.status != BehandlingStatus.UTREDES) {
             behandlingService.oppdaterStatusPåBehandling(saksbehandling.id, BehandlingStatus.UTREDES)
         }
+    }
+
+    override fun utførOgReturnerNesteSteg(
+        saksbehandling: Saksbehandling,
+        data: Void?,
+    ): StegType {
+        val beregningsResultatOffentligTransport =
+            vedtakRepository
+                .findByIdOrThrow(saksbehandling.id)
+                .withTypeOrThrow<InnvilgelseEllerOpphørDagligReise>()
+                .data
+                .beregningsresultat
+                ?.offentligTransport
+
+        if (beregningsResultatOffentligTransport == null) {
+            return StegType.SEND_TIL_BESLUTTER
+        }
+        return super.utførOgReturnerNesteSteg(saksbehandling, data)
     }
 
     override fun stegType(): StegType = StegType.KJØRELISTE
