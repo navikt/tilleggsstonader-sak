df3b5738c74d78d5e71a1c47139e753a6a5822eb
sarahjelle <sara.hjelle@bekk.no>
2026-01-22 18:28:58 +0100

    Legg inn dummy-resultat på privat bil for å få oppdatert filer i test som må oppdateres

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/domain/BeregningsresultatDagligReise.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/domain/BeregningsresultatDagligReise.kt
@@ -2,5 +2,11 @@ package no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain
 
 data class BeregningsresultatDagligReise(
     val offentligTransport: BeregningsresultatOffentligTransport?,
-    // val privatBil: BeregningsresultatPrivatBil?,
+    val privatBil: DummyBeregningsresultatPrivatBil? = null,
+)
+
+// TODO: Erstatt med ekte dataklasse
+// Kun lagt inn for å håndtere at privat bil må legges inn en del steder.
+data class DummyBeregningsresultatPrivatBil(
+    val resultat: Int,
 )
