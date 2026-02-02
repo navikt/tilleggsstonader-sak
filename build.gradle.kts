cb35754ec7755331a303f823d1203affd5a920d9
Runar Skagestad <runar.skagestad@nav.no>
2026-02-02 14:00:04 +0100

    ⬇️ Nedgrader til kotlin 2.2.21 ettersom codeql ikke støtter 2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,14 @@ version = "1.0.0"
 plugins {
     application
 
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "2.2.21"
     id("com.diffplug.spotless") version "8.0.0"
     id("com.github.ben-manes.versions") version "0.53.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
 
     id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.spring") version "2.3.0"
+    kotlin("plugin.spring") version "2.2.21"
 }
 
 repositories {
