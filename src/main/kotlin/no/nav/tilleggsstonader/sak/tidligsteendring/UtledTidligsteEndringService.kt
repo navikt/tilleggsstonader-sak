3435c8fe1c793924b20648cfd2aeb80965e8471b
Runar Skagestad <runar.skagestad@nav.no>
2026-02-03 13:17:22 +0100

    👔 Skal kunne beregne også dersom bruker får en ny aktivitet som er erstatter det forrige 1:1 (helt lik periode), og saksbehandler innvilger for å rydde opp

--- src/main/kotlin/no/nav/tilleggsstonader/sak/tidligsteendring/UtledTidligsteEndringService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/tidligsteendring/UtledTidligsteEndringService.kt
@@ -299,7 +299,8 @@ data class TidligsteEndringIBehandlingUtleder(
     ): Boolean =
         vilkårperiode.resultat != tidligereVilkårperiode.resultat ||
             vilkårperiode.type != tidligereVilkårperiode.type ||
-            vilkårperiode.faktaOgVurdering.fakta != tidligereVilkårperiode.faktaOgVurdering.fakta
+            vilkårperiode.faktaOgVurdering.fakta != tidligereVilkårperiode.faktaOgVurdering.fakta ||
+            vilkårperiode.kildeId != tidligereVilkårperiode.kildeId
 
     private fun erMålgruppeEndret(
         vilkårperiode: ForenkletMålgruppe,
