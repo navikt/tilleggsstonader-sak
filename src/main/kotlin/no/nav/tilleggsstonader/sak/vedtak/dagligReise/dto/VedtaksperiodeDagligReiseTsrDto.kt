d89d5a6d5d5cb496c8a38e413cf77095e1a739c8
Sigmund Berbom <sberbom@gmail.com>
2026-02-05 14:21:02 +0100

    🔥 Fjern interface som ikke lenger stemmer når målgruppe og aktivitet ikke må være på vedtaksperioder for tsr

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/dto/VedtaksperiodeDagligReiseTsrDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/dto/VedtaksperiodeDagligReiseTsrDto.kt
@@ -3,27 +3,23 @@ package no.nav.tilleggsstonader.sak.vedtak.dagligReise.dto
 import no.nav.tilleggsstonader.kontrakter.aktivitet.TypeAktivitet
 import no.nav.tilleggsstonader.sak.felles.domain.FaktiskMålgruppe
 import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
-import no.nav.tilleggsstonader.sak.vedtak.dto.VedtaksperiodeDtoInterface
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import java.time.LocalDate
 import java.util.UUID
 
 data class VedtaksperiodeDagligReiseTsrDto(
-    override val id: UUID = UUID.randomUUID(),
-    override val fom: LocalDate,
-    override val tom: LocalDate,
-    override val typeAktivitet: TypeAktivitet,
-) : VedtaksperiodeDtoInterface {
-    override val målgruppeType: FaktiskMålgruppe = FaktiskMålgruppe.ARBEIDSSØKER
-    override val aktivitetType: AktivitetType = AktivitetType.TILTAK
-
+    val id: UUID = UUID.randomUUID(),
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val typeAktivitet: TypeAktivitet,
+) {
     fun tilDomene() =
         Vedtaksperiode(
             id = id,
             fom = fom,
             tom = tom,
-            målgruppe = målgruppeType,
-            aktivitet = aktivitetType,
+            målgruppe = FaktiskMålgruppe.ARBEIDSSØKER,
+            aktivitet = AktivitetType.TILTAK,
             typeAktivitet = typeAktivitet,
         )
 }
--- src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/extensions/kall/VedtakKall.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/extensions/kall/VedtakKall.kt
@@ -1,10 +1,14 @@
 package no.nav.tilleggsstonader.sak.integrasjonstest.extensions.kall
 
+import no.nav.tilleggsstonader.kontrakter.felles.Enhet
 import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.kontrakter.felles.behandlendeEnhet
+import no.nav.tilleggsstonader.kontrakter.felles.gjelderDagligReise
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
 import no.nav.tilleggsstonader.sak.integrasjonstest.Testklient
 import no.nav.tilleggsstonader.sak.vedtak.dto.LagretVedtaksperiodeDto
 import no.nav.tilleggsstonader.sak.vedtak.dto.VedtakRequest
+import org.springframework.test.web.servlet.client.RestTestClient
 
 class VedtakKall(
     private val testklient: Testklient,
@@ -68,11 +72,34 @@ class VedtakKall(
             vedtakDto: VedtakRequest,
         ) = testklient.post("/api/vedtak/${stønadstype.tilPath()}/$behandlingId/$typeVedtakPath", vedtakDto)
 
+        fun lagreEnhetsspesifiktVedtak(
+            stønadstype: Stønadstype,
+            behandlingId: BehandlingId,
+            typeVedtakPath: String,
+            vedtakDto: VedtakRequest,
+            enhet: Enhet,
+        ): RestTestClient.ResponseSpec =
+            testklient.post(
+                "/api/vedtak/${stønadstype.tilPath()}/$behandlingId/${enhet.tilPath()}/$typeVedtakPath",
+                vedtakDto,
+            )
+
         fun lagreInnvilgelse(
             stønadstype: Stønadstype,
             behandlingId: BehandlingId,
             innvilgelseDto: VedtakRequest,
-        ) = lagreVedtak(stønadstype, behandlingId, "innvilgelse", innvilgelseDto)
+        ): RestTestClient.ResponseSpec {
+            if (stønadstype.gjelderDagligReise()) {
+                return lagreEnhetsspesifiktVedtak(
+                    stønadstype,
+                    behandlingId,
+                    "innvilgelse",
+                    innvilgelseDto,
+                    stønadstype.behandlendeEnhet(),
+                )
+            }
+            return lagreVedtak(stønadstype, behandlingId, "innvilgelse", innvilgelseDto)
+        }
 
         fun lagreOpphør(
             stønadstype: Stønadstype,
@@ -89,4 +116,12 @@ private fun Stønadstype.tilPath(): String =
         Stønadstype.BOUTGIFTER -> "boutgifter"
         Stønadstype.DAGLIG_REISE_TSO -> "daglig-reise"
         Stønadstype.DAGLIG_REISE_TSR -> "daglig-reise"
+    }
+
+private fun Enhet.tilPath(): String =
+    when (this) {
+        Enhet.NAV_ARBEID_OG_YTELSER_TILLEGGSSTØNAD -> "tso"
+        Enhet.NAV_TILTAK_OSLO -> "tsr"
+        Enhet.NAV_ARBEID_OG_YTELSER_EGNE_ANSATTE -> TODO()
+        Enhet.VIKAFOSSEN -> TODO()
     }
