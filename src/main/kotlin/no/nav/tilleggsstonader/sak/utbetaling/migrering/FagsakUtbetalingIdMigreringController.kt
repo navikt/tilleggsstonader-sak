1b8bf4dde39fec6b3b0c589461dabab47b87be08
Andreas Strand <andreas.strand@nav.no>
2026-02-06 15:37:13 +0100

    Gjeninnfører logikk for migrering av saker over på kafka, da saker hvor siste behandling opphører samtlige utbetalinger ikke har blitt migrert (#1055)

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/utbetaling/migrering/FagsakUtbetalingIdMigreringController.kt
@@ -0,0 +1,31 @@
+package no.nav.tilleggsstonader.sak.utbetaling.migrering
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.tilleggsstonader.sak.felles.domain.FagsakId
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.Executors
+
+@Tag(name = "Forvaltning")
+@RestController
+@RequestMapping("/api/forvaltning/migrer-utbetalinger")
+@ProtectedWithClaims(issuer = "azuread")
+class FagsakUtbetalingIdMigreringController(
+    private val fagsakUtbetalingIdMigreringService: FagsakUtbetalingIdMigreringService,
+) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @PostMapping("/{fagsakId}")
+    fun migrerFagsak(
+        @PathVariable fagsakId: FagsakId,
+    ) {
+        logger.info("Migrerer fagsak $fagsakId")
+        Executors.newVirtualThreadPerTaskExecutor().submit {
+            fagsakUtbetalingIdMigreringService.migrerForFagsak(fagsakId)
+        }
+    }
+}
