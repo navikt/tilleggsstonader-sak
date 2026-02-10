a8659b4f52815fb0e27c25275e4c5366196228a6
Andreas Strand <andreas.strand@nav.no>
2026-02-10 14:08:57 +0100

    Utvide kjoereliste dto (#1061)
    Utvider KjørelisteDto (midlertidig navn i påvente av bedre begrep) slik at den inneholder informasjon om hele rammevedtaket for en reise, aggregert med informasjon om innsendt kjøreliste og avklarte kjørte dager
    ---------
    
    Co-authored-by: Filip Larsen <filip.christoffer.larsen@nav.no>
    Co-authored-by: sarahjelle <sara.hjelle@bekk.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/kjøreliste/KjørelisteController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/kjøreliste/KjørelisteController.kt
@@ -4,12 +4,16 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tilleggsstonader.libs.utils.dato.ukenummer
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import no.nav.tilleggsstonader.sak.vedtak.VedtakService
+import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseEllerOpphørDagligReise
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.domain.ReiseId
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.UUID
 
 @RestController
 @RequestMapping(path = ["/api/kjoreliste"])
@@ -17,6 +21,7 @@ import java.time.LocalDate
 class KjørelisteController(
     private val behandlingService: BehandlingService,
     private val kjørelisteService: KjørelisteService,
+    private val vedtakService: VedtakService,
 ) {
     @GetMapping("{behandlingId}")
     fun hentKjørelisteForBehandling(
@@ -24,42 +29,130 @@ class KjørelisteController(
     ): List<KjørelisteDto> {
         val behandling = behandlingService.hentBehandling(behandlingId)
 
-        return kjørelisteService.hentForFagsakId(behandling.fagsakId).map { kjøreliste ->
+        val kjørelister = kjørelisteService.hentForFagsakId(behandling.fagsakId)
+        val reiserIRammevedtak =
+            vedtakService
+                .hentVedtak<InnvilgelseEllerOpphørDagligReise>(behandling.id)
+                ?.data
+                ?.rammevedtakPrivatBil
+                ?.reiser
+
+        reiserIRammevedtak?.map { reise ->
             KjørelisteDto(
-                reiseId = kjøreliste.data.reiseId,
+                reiseId = reise.reiseId,
                 uker =
-                    kjøreliste.data.reisedager
-                        .map { reisedag ->
-                            KjørelisteDagDto(
-                                dato = reisedag.dato,
-                                harKjørt = reisedag.harKjørt,
-                                parkeringsutgift = reisedag.parkeringsutgift,
-                            )
-                        }.groupBy {
-                            it.dato.ukenummer()
-                        }.map { (uke, reisedagerIUke) ->
-                            KjørelisteUkeDto(
-                                ukeNummer = uke,
-                                reisedager = reisedagerIUke,
-                            )
-                        },
+                    reise.uker.map { uke ->
+                        val kjørelisteForUke =
+                            kjørelister.firstOrNull {
+                                it.data.reiseId == reise.reiseId &&
+                                    it.inneholderUkenummer(uke.grunnlag.fom.ukenummer())
+                            }
+                        UkeDto(
+                            ukenummer = uke.grunnlag.fom.ukenummer(),
+                            fraDato = uke.grunnlag.fom,
+                            tilDato = uke.grunnlag.tom,
+                            // TODO: må utvides med flere statuser
+                            status = if (kjørelisteForUke == null) UkeStatus.IKKE_MOTTATT_KJØRELISTE else UkeStatus.AVVIK,
+                            avviksbegrunnelse = AvviksbegrunnelseUke.FLERE_REISEDAGER_ENN_I_RAMMEVEDTAK,
+                            avviksMelding = "Dette er egentlig ikke et avvik, bare en test",
+                            behandletDato = null,
+                            kjørelisteInnsendtDato = kjørelisteForUke?.datoMottatt?.toLocalDate(),
+                            kjørelisteId = kjørelisteForUke?.id,
+                            dag =
+                                (
+                                    0L..ChronoUnit.DAYS.between(
+                                        uke.grunnlag.fom,
+                                        uke.grunnlag.tom,
+                                    )
+                                ).map { uke.grunnlag.fom.plusDays(it) }
+                                    .map {
+                                        DagDto(
+                                            dato = it,
+                                            ukedag = it.dayOfWeek.name,
+                                            kjørelisteDag =
+                                                kjørelisteForUke
+                                                    ?.data
+                                                    ?.reisedager
+                                                    ?.firstOrNull { reisedag -> reisedag.dato == it }
+                                                    ?.let { reisedag ->
+                                                        KjørelisteDagDto(
+                                                            harKjørt = reisedag.harKjørt,
+                                                            parkeringsutgift = reisedag.parkeringsutgift,
+                                                        )
+                                                    },
+                                            avklartDag = null,
+                                        )
+                                    },
+                        )
+                    },
             )
         }
+
+        return emptyList()
     }
 }
 
 data class KjørelisteDto(
     val reiseId: ReiseId,
-    val uker: List<KjørelisteUkeDto>,
+    val uker: List<UkeDto>,
 )
 
-data class KjørelisteUkeDto(
-    val ukeNummer: Int,
-    val reisedager: List<KjørelisteDagDto>,
+data class UkeDto(
+    val ukenummer: Int,
+    val fraDato: LocalDate,
+    val tilDato: LocalDate,
+    val status: UkeStatus,
+    val avviksbegrunnelse: AvviksbegrunnelseUke?,
+    val avviksMelding: String?,
+    val behandletDato: LocalDate?,
+    val kjørelisteInnsendtDato: LocalDate?, // null hvis kjøreliste ikke er mottatt
+    val kjørelisteId: UUID?, // null hvis kjøreliste ikke er mottatt
+    val dag: List<DagDto>,
+)
+
+data class DagDto(
+    val dato: LocalDate,
+    val ukedag: String, // avklar om faktisk trenger, eller om frontend skal mappe ut fra dag
+    val kjørelisteDag: KjørelisteDagDto?,
+    val avklartDag: AvklartDag?,
 )
 
 data class KjørelisteDagDto(
-    val dato: LocalDate,
     val harKjørt: Boolean,
     val parkeringsutgift: Int?,
 )
+
+data class AvklartDag(
+    val resultat: UtfyltDagResultat,
+    val automatiskVurdering: UtfyltDagAutomatiskVurdering,
+    val avviksbegrunnelse: AvviksbegrunnelseDag,
+    val begrunnelse: String?, // må fylles ut om avvik?
+    val parkeringsutgift: Int?,
+)
+
+enum class UkeStatus {
+    OK_AUTOMATISK, // brukes hvis automatisk godkjent
+    OK_MANUELT, // brukes hvis saksbehandler godtar avvik
+    AVVIK, // parkeringsutgifter/for mange dager etc. saksbehandler må ta stilling til uka
+    KORRIGERT, // saksbehandler har endret innsendte dager
+    IKKE_MOTTATT_KJØRELISTE,
+}
+
+enum class UtfyltDagResultat {
+    UTBETALING,
+    IKKE_UTBETALING,
+}
+
+enum class UtfyltDagAutomatiskVurdering {
+    OK,
+    AVVIK,
+}
+
+enum class AvviksbegrunnelseDag {
+    FOR_HØY_PARKERINGSUTGIFT,
+    HELLIDAG_ELLER_HELG,
+}
+
+enum class AvviksbegrunnelseUke {
+    FLERE_REISEDAGER_ENN_I_RAMMEVEDTAK,
+}
