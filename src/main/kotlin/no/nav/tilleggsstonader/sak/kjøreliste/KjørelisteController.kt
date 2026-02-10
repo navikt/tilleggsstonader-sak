b0dda2962beaa8c959e35f1fc6a2afb5ae15b0d6
Andreas Strand <andreas.strand@nav.no>
2026-02-10 13:07:48 +0100

    Start på utvidelse av kjørelsitedto som ikke lenger er kjørelistedto
    Co-authored-by: Filip Larsen <filip.christoffer.larsen@nav.no>
    Co-authored-by: sarahjelle <sara.hjelle@bekk.no>

--- src/main/kotlin/no/nav/tilleggsstonader/sak/kjøreliste/KjørelisteController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/kjøreliste/KjørelisteController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
+import java.util.UUID
 
 @RestController
 @RequestMapping(path = ["/api/kjoreliste"])
@@ -27,7 +28,7 @@ class KjørelisteController(
         return kjørelisteService.hentForFagsakId(behandling.fagsakId).map { kjøreliste ->
             KjørelisteDto(
                 reiseId = kjøreliste.data.reiseId,
-                uker =
+                innsendteUker =
                     kjøreliste.data.reisedager
                         .map { reisedag ->
                             KjørelisteDagDto(
@@ -50,16 +51,65 @@ class KjørelisteController(
 
 data class KjørelisteDto(
     val reiseId: ReiseId,
-    val uker: List<KjørelisteUkeDto>,
+    val uker: List<UkeDto>,
 )
 
-data class KjørelisteUkeDto(
-    val ukeNummer: Int,
-    val reisedager: List<KjørelisteDagDto>,
+data class UkeDto(
+    val ukenummer: Int,
+    val fraDato: LocalDate,
+    val tilDato: LocalDate,
+    val status: UkeStatus,
+    val avviksbegrunnelse: AvviksbegrunnelseUke?,
+    val avviksMelding: String?,
+    val behandletDato: LocalDate?,
+    val kjørelisteInnsendtDato: LocalDate?, // null hvis kjøreliste ikke er mottatt
+    val kjørelisteId: UUID?, // null hvis kjøreliste ikke er mottatt
+    val dag: List<DagDto>
+)
+
+data class DagDto(
+    val dato: LocalDate,
+    val ukedag: String, // avklar om faktisk trenger, eller om frontend skal mappe ut fra dag
+    val kjørelisteDag: KjørelisteDagDto?,
+    val avklartDag: AvklartDag?
 )
 
 data class KjørelisteDagDto(
-    val dato: LocalDate,
     val harKjørt: Boolean,
     val parkeringsutgift: Int?,
 )
+
+data class AvklartDag(
+    val resultat: UtfyltDagResultat,
+    val automatiskVurdering: UtfyltDagAutomatiskVurdering,
+    val avviksbegrunnelse: AvviksbegrunnelseDag,
+    val begrunnelse: String?, // må fylles ut om avvik?
+    val parkeringsutgift: Int?,
+)
+
+enum class UkeStatus {
+    OK_AUTOMATISK, // brukes hvis automatisk godkjent
+    OK_MANUELT, // brukes hvis saksbehandler godtar avvik
+    AVVIK, // parkeringsutgifter/for mange dager etc. saksbehandler må ta stilling til uka
+    KORRIGERT, // saksbehandler har endret innsendte dager
+    IKKE_MOTTATT_KJØRELISTE,
+}
+
+enum class UtfyltDagResultat {
+    UTBETALING,
+    IKKE_UTBETALING
+}
+
+enum class UtfyltDagAutomatiskVurdering {
+    OK,
+    AVVIK
+}
+
+enum class AvviksbegrunnelseDag {
+    FOR_HØY_PARKERINGSUTGIFT,
+    HELLIDAG_ELLER_HELG,
+}
+
+enum class AvviksbegrunnelseUke {
+    FLERE_REISEDAGER_ENN_I_RAMMEVEDTAK
+}
