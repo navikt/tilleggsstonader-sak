8bc13e54111f78046dcd3f5b7263d11ad46b6af1
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-02-11 14:59:02 +0100

    Legger til repository for avklartKjørtUke
    Co-authored-by: Andreas Strand <Andreas.Strand@nav.no>
    
    Co-authored-by: sarahjelle <sara.hjelle@bekk.no>

---
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/kjøreliste/avklartedager/AvklartKjørtUkeRepository.kt
@@ -0,0 +1,16 @@
+package no.nav.tilleggsstonader.sak.kjøreliste.avklartedager
+
+import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.InsertUpdateRepository
+import no.nav.tilleggsstonader.sak.infrastruktur.database.repository.RepositoryInterface
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface AvklartKjørtUkeRepository :
+    RepositoryInterface<AvklartKjørtUke, UUID>,
+    InsertUpdateRepository<AvklartKjørtUke> {
+
+    fun findByBehandlingId(behandlingId: BehandlingId): List<AvklartKjørtUke>
+
+}
