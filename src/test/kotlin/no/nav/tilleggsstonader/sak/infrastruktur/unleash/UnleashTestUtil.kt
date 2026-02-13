81de35e25cc18d36e0509dbf7c175038d57e052e
Andreas Strand <andreas.strand@nav.no>
2026-02-13 08:47:32 +0100

    revert toggle

--- src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/unleash/UnleashTestUtil.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/infrastruktur/unleash/UnleashTestUtil.kt
@@ -25,7 +25,7 @@ fun resetMock(
     clearMocks(mockk)
     every { mockk.isEnabled(any()) } returns isEnabled
     every { mockk.isEnabled(any(), any<Boolean>()) } returns isEnabled
-    every { mockk.isEnabled(Toggle.KAN_BEHANDLE_PRIVAT_BIL) } returns true
+    every { mockk.isEnabled(Toggle.KAN_BEHANDLE_PRIVAT_BIL) } returns false
     justRun { mockk.destroy() }
 }
 
