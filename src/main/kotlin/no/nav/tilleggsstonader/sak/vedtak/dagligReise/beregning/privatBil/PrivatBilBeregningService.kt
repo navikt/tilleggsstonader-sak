e3cdf1d0922b322c6c5200197e48ed6e30601466
Andreas Strand <andreas.strand@nav.no>
2026-02-23 23:01:06 +0100

    Endrer rammevedtak for privat bil slik at ukene som inngår ikke listes ut og at satsene inngår i rammevedtaket og tar høyde for om satsen er bekreftet eller ikke

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/beregning/privatBil/PrivatBilBeregningService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/beregning/privatBil/PrivatBilBeregningService.kt
@@ -1,6 +1,7 @@
 package no.nav.tilleggsstonader.sak.vedtak.dagligReise.beregning.privatBil
 
 import no.nav.tilleggsstonader.kontrakter.felles.Datoperiode
+import no.nav.tilleggsstonader.kontrakter.felles.splitPerÅr
 import no.nav.tilleggsstonader.sak.vedtak.dagligReise.beregning.finnSnittMellomReiseOgVedtaksperioder
 import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.BeregningsgrunnlagForReiseMedPrivatBil
 import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.BeregningsgrunnlagForUke
@@ -8,6 +9,7 @@ import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.Ekstrakostnader
 import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.RammeForReiseMedPrivatBil
 import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.RammeForUke
 import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.RammevedtakPrivatBil
+import no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain.SatsForPeriodePrivatBil
 import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.domain.VilkårDagligReise
 import org.springframework.stereotype.Service
@@ -46,113 +48,56 @@ class PrivatBilBeregningService {
 
         val grunnlagForReise = lagBeregningsgrunnlagForReise(justertReise)
 
-        val periodeSplittetPåUker = justertReise.splitPerUkeMedHelg()
-
-        val uker =
-            periodeSplittetPåUker.mapNotNull {
-                beregnForUke(
-                    uke = it,
-                    grunnlagForReise = grunnlagForReise,
-                    vedtaksperioder = vedtaksperioder,
-                )
-            }
-
-        if (uker.isEmpty()) return null
-
         return RammeForReiseMedPrivatBil(
             reiseId = reise.reiseId,
             aktivitetsadresse = reise.aktivitetsadresse,
-            uker = uker,
             grunnlag = grunnlagForReise,
         )
     }
 
-    private fun beregnForUke(
-        uke: Datoperiode,
-        grunnlagForReise: BeregningsgrunnlagForReiseMedPrivatBil,
-        vedtaksperioder: List<Vedtaksperiode>,
-    ): RammeForUke? {
-        val grunnlagForUke =
-            lagBeregningsgrunnlagForUke(
-                uke = uke,
-                reisedagerPerUke = grunnlagForReise.reisedagerPerUke,
-                vedtaksperioder = vedtaksperioder,
-            ) ?: return null
-
-        val dagsatsUtenParkering =
-            beregnDagsatsUtenParkering(
-                kilometersats = grunnlagForUke.kilometersats,
-                grunnlagForReise = grunnlagForReise,
-            )
-
-        return RammeForUke(
-            grunnlag = grunnlagForUke,
-            dagsatsUtenParkering = dagsatsUtenParkering,
-            maksBeløpSomKanDekkesFørParkering =
-                beregnMaksbeløp(
-                    grunnlagForUke = grunnlagForUke,
-                    dagsatsUtenParkering = dagsatsUtenParkering,
-                ),
+    private fun lagBeregningsgrunnlagForReise(reise: ReiseMedPrivatBil): BeregningsgrunnlagForReiseMedPrivatBil {
+        val ekstrakostnader = Ekstrakostnader(
+            fergekostnadEnVei = reise.fergekostandEnVei,
+            bompengerEnVei = reise.bompengerEnVei,
         )
-    }
-
-    private fun beregnMaksbeløp(
-        grunnlagForUke: BeregningsgrunnlagForUke,
-        dagsatsUtenParkering: BigDecimal,
-    ) = dagsatsUtenParkering
-        .multiply(grunnlagForUke.maksAntallDagerSomKanDekkes.toBigDecimal())
-        .setScale(0, RoundingMode.HALF_UP)
-        .toBigInteger()
-
-    private fun lagBeregningsgrunnlagForReise(reise: ReiseMedPrivatBil): BeregningsgrunnlagForReiseMedPrivatBil =
-        BeregningsgrunnlagForReiseMedPrivatBil(
+        return BeregningsgrunnlagForReiseMedPrivatBil(
             fom = reise.fom,
             tom = reise.tom,
             reisedagerPerUke = reise.reisedagerPerUke,
             reiseavstandEnVei = reise.reiseavstandEnVei,
-            ekstrakostnader =
-                Ekstrakostnader(
-                    fergekostnadEnVei = reise.fergekostandEnVei,
-                    bompengerEnVei = reise.bompengerEnVei,
-                ),
-        )
-
-    private fun lagBeregningsgrunnlagForUke(
-        uke: Datoperiode,
-        reisedagerPerUke: Int,
-        vedtaksperioder: List<Vedtaksperiode>,
-    ): BeregningsgrunnlagForUke? {
-        val relevantVedtaksperiode = finnRelevantVedtaksperiodeForUke(uke, vedtaksperioder) ?: return null
-
-        val justertUkeMedAntallDager =
-            uke.finnAntallDagerIUkeInnenforVedtaksperiode(relevantVedtaksperiode) ?: return null
-
-        val (antallDager, antallDagerInkludererHelg) =
-            finnAntallDagerSomDekkes(
-                justertUkeMedAntallDager,
-                reisedagerPerUke,
-            )
-
-        return BeregningsgrunnlagForUke(
-            fom = justertUkeMedAntallDager.fom,
-            tom = justertUkeMedAntallDager.tom,
-            maksAntallDagerSomKanDekkes = antallDager,
-            antallDagerInkludererHelg = antallDagerInkludererHelg,
-            vedtaksperioder = listOf(relevantVedtaksperiode),
-            kilometersats = finnRelevantKilometerSats(periode = justertUkeMedAntallDager),
+            ekstrakostnader = ekstrakostnader,
+            satser = beregnSatserForReise(reise, ekstrakostnader)
         )
     }
 
+    private fun beregnSatserForReise(reise: ReiseMedPrivatBil, ekstrakostnader: Ekstrakostnader): List<SatsForPeriodePrivatBil> =
+        reise.splitPerÅr { fom, tom ->
+            val periode = Datoperiode(fom, tom)
+            val sats = finnRelevantKilometerSats(periode)
+            SatsForPeriodePrivatBil(
+                fom = fom,
+                tom = tom,
+                satsBekreftetVedVedtakstidspunkt = sats.bekreftet,
+                kilometersats = sats.beløp,
+                dagsatsUtenParkering = beregnDagsatsUtenParkering(
+                    reiseavstandEnVei = reise.reiseavstandEnVei,
+                    ekstrakostnader = ekstrakostnader,
+                    kilometersats = sats.beløp
+                )
+            )
+        }
+
     private fun beregnDagsatsUtenParkering(
-        grunnlagForReise: BeregningsgrunnlagForReiseMedPrivatBil,
+        reiseavstandEnVei: BigDecimal,
+        ekstrakostnader: Ekstrakostnader,
         kilometersats: BigDecimal,
     ): BigDecimal {
         val kostnadKjøring =
-            grunnlagForReise.reiseavstandEnVei
+            reiseavstandEnVei
                 .multiply(BigDecimal.valueOf(2))
                 .multiply(kilometersats)
 
-        val sumEkstrakostnader = grunnlagForReise.ekstrakostnader.beregnTotalEkstrakostnadForEnDag()
+        val sumEkstrakostnader = ekstrakostnader.beregnTotalEkstrakostnadForEnDag()
 
         return kostnadKjøring + sumEkstrakostnader
     }
--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/domain/RammevedtakPrivatBil.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/domain/RammevedtakPrivatBil.kt
@@ -1,6 +1,8 @@
 package no.nav.tilleggsstonader.sak.vedtak.dagligReise.domain
 
+import no.nav.tilleggsstonader.kontrakter.felles.Datoperiode
 import no.nav.tilleggsstonader.kontrakter.felles.Periode
+import no.nav.tilleggsstonader.kontrakter.felles.alleDatoer
 import no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.domain.ReiseId
 import java.math.BigDecimal
@@ -14,19 +16,9 @@ data class RammevedtakPrivatBil(
 data class RammeForReiseMedPrivatBil(
     val reiseId: ReiseId,
     val aktivitetsadresse: String?,
-    val uker: List<RammeForUke>,
     val grunnlag: BeregningsgrunnlagForReiseMedPrivatBil,
 )
 
-/**
- * dagsatsUtenParkering: hva brukeren kan få dekt per dag. Inkluderer bompenger og ferge, men ikke parkering.
- * maksBeløpSomKanDekkesFørParkering: maksimalt beløp bruker kan få dekt dersom hen kjører hver dag.
- */
-data class RammeForUke(
-    val grunnlag: BeregningsgrunnlagForUke,
-    val dagsatsUtenParkering: BigDecimal,
-    val maksBeløpSomKanDekkesFørParkering: BigInteger,
-)
 
 data class BeregningsgrunnlagForReiseMedPrivatBil(
     override val fom: LocalDate,
@@ -34,23 +26,7 @@ data class BeregningsgrunnlagForReiseMedPrivatBil(
     val reisedagerPerUke: Int,
     val reiseavstandEnVei: BigDecimal,
     val ekstrakostnader: Ekstrakostnader,
-) : Periode<LocalDate>
-
-/**
- * fom: Mandag eller begrenset av reiseperioden
- * tom: Søndag eller begrenset av reiseperioden
- * maksAntallDagerSomKanDekkes: Begrenses av antall reisedager og fom/tom
- * antallDagerInkludererHelg: Flagg for å vise om beregning har tatt med helg i maksAntall dager
- * vedtaksperioder: Er en liste, men beregning håndterer foreløpig ikke mer enn 1
- * kilometersats: Hvor mye som dekkes per kilometer, fastsatt i forskriften og endres årlig.
- */
-data class BeregningsgrunnlagForUke(
-    override val fom: LocalDate,
-    override val tom: LocalDate,
-    val maksAntallDagerSomKanDekkes: Int,
-    val antallDagerInkludererHelg: Boolean,
-    val vedtaksperioder: List<Vedtaksperiode>,
-    val kilometersats: BigDecimal,
+    val satser: List<SatsForPeriodePrivatBil>
 ) : Periode<LocalDate>
 
 // TODO: Finn ut om det finnes abbonnement på disse prisene og om det påvirker hvordan vi vil løse dette
@@ -67,3 +43,15 @@ data class Ekstrakostnader(
         return sum.toBigDecimal()
     }
 }
+
+/**
+ * dagsatsUtenParkering: hva brukeren kan få dekt per dag. Inkluderer bompenger og ferge, men ikke parkering.
+ * maksBeløpSomKanDekkesFørParkering: maksimalt beløp bruker kan få dekt dersom hen kjører hver dag.
+ */
+data class SatsForPeriodePrivatBil(
+    override val fom: LocalDate,
+    override val tom: LocalDate,
+    val satsBekreftetVedVedtakstidspunkt: Boolean,
+    val kilometersats: BigDecimal,
+    val dagsatsUtenParkering: BigDecimal,
+): Periode<LocalDate>
