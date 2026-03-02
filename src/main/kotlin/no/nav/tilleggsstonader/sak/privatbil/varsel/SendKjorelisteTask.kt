9fe90676617a4610e2a7a85e465582dd5cf9692e
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-03-02 09:52:45 +0100

    Genererer eventId og starter en ny task onCompletion

--- src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/varsel/SendKjorelisteTask.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/varsel/SendKjorelisteTask.kt
@@ -3,6 +3,7 @@ package no.nav.tilleggsstonader.sak.privatbil.varsel
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import no.nav.familie.prosessering.internal.TaskService
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.fagsak.FagsakService
 import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
@@ -22,6 +23,7 @@ class SendKjorelisteTask(
     private val notifikasjonsService: VarselDittNavKafkaProducer,
     private val behandlingService: BehandlingService,
     private val fagsakService: FagsakService,
+    private val taskService: TaskService,
 ) : AsyncTaskStep {
     override fun doTask(task: Task) {
         val behandling = behandlingService.hentBehandling(BehandlingId.fromString(task.payload))
@@ -29,10 +31,15 @@ class SendKjorelisteTask(
         val fnr = fagsak.hentAktivIdent()
 
         val melding = "Du har én eller flere kjørelister tilgjengelige for utfylling."
-        // TODO: finne en eventId som er en gyldig UUID og unik
-        val eventId = task.payload
+        notifikasjonsService.sendToKafka(fnr, melding, task.metadata.getProperty("eventId"))
+    }
 
-        notifikasjonsService.sendToKafka(fnr, melding, eventId)
+    override fun onCompletion(task: Task) {
+        taskService.save(
+            opprettTask(
+                BehandlingId(UUID.fromString(task.payload))
+            )
+        )
     }
 
     companion object {
@@ -41,6 +48,7 @@ class SendKjorelisteTask(
         fun opprettTask(behandlingId: BehandlingId): Task {
             Properties().apply {
                 setProperty("behandlingId", behandlingId.toString())
+                setProperty("eventId", UUID.randomUUID().toString())
             }
             return Task(TYPE, behandlingId.toString())
                 .medTriggerTid(LocalDate.now().finnMandagNesteUke().atTime(10, 0))
