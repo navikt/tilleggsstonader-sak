2189011c0719e211cc13fcf9e5356c8f42321e84
Nibedita.Pahari <nibedita.pahari@nav.no>
2026-03-09 14:31:52 +0100

    spotless apply

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/detaljerteVedtaksperioder/DetaljertVedtaksperiodeDagligReiseTsrTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/detaljerteVedtaksperioder/DetaljertVedtaksperiodeDagligReiseTsrTest.kt
@@ -1,65 +1,59 @@
-// package no.nav.tilleggsstonader.sak.vedtak.dagligReise.detaljerteVedtaksperioder
-//
-// import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
-// import no.nav.tilleggsstonader.libs.utils.dato.februar
-// import no.nav.tilleggsstonader.libs.utils.dato.januar
-// import no.nav.tilleggsstonader.sak.vedtak.dagligReise.beregning.offentligTransport.Billettype
-// import no.nav.tilleggsstonader.sak.vedtak.domain.TypeDagligReise
-//
-// class DetaljertVedtaksperiodeDagligReiseTsrTest {
-//    val førsteJan = 1 januar 2024
-//    val sisteJan = 31 januar 2024
-//    val førsteFeb = 1 februar 2024
-//    val sisteFeb = 29 februar 2024
-//
-//    val vedtaksperiodeJan =
-//        DetaljertVedtaksperiodeDagligReise(
-//            stønadstype = Stønadstype.DAGLIG_REISE_TSO,
-//            typeDagligReise = TypeDagligReise.OFFENTLIG_TRANSPORT,
-//            detaljertVedtaksperiode =
-//                listOf(
-//                    DetaljertBeregningsperioder(
-//                        fom = førsteJan,
-//                        tom = sisteJan,
-//                        prisEnkeltbillett = 30,
-//                        prisSyvdagersbillett = 150,
-//                        pris30dagersbillett = 500,
-//                        beløp = 300,
-//                        billettdetaljer = mapOf(Billettype.ENKELTBILLETT to 20),
-//                        antallReisedager = 20,
-//                        antallReisedagerPerUke = 3,
-//                    ),
-//                ),
-//        )
-//
-//    val vedtaksperiodeFeb =
-//        DetaljertVedtaksperiodeDagligReise(
-//            stønadstype = Stønadstype.DAGLIG_REISE_TSO,
-//            typeDagligReise = TypeDagligReise.OFFENTLIG_TRANSPORT,
-//            detaljertVedtaksperiode =
-//                listOf(
-//                    DetaljertBeregningsperioder(
-//                        fom = førsteFeb,
-//                        tom = sisteFeb,
-//                        prisEnkeltbillett = 30,
-//                        prisSyvdagersbillett = 150,
-//                        pris30dagersbillett = 500,
-//                        beløp = 300,
-//                        billettdetaljer = mapOf(Billettype.ENKELTBILLETT to 20),
-//                        antallReisedager = 20,
-//                        antallReisedagerPerUke = 3,
-//                    ),
-//                ),
-//        )
-//
-//    @Test
-//    fun `skal ikke slå sammen aktivteter med ulik typeAktivitet`() {
-//        val vedtaksperioder =
-//            listOf(
-//                vedtaksperiodeJan,
-//                vedtaksperiodeFeb,
-//            )
-//        val resultat = vedtaksperioder.sorterOgMergeSammenhengendeEllerOverlappende()
-//        assertThat(resultat).isEqualTo(vedtaksperioder)
-//    }
-// }
+package no.nav.tilleggsstonader.sak.vedtak.dagligReise.detaljerteVedtaksperioder
+
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.libs.utils.dato.februar
+import no.nav.tilleggsstonader.libs.utils.dato.januar
+import no.nav.tilleggsstonader.sak.vedtak.dagligReise.beregning.offentligTransport.Billettype
+import no.nav.tilleggsstonader.sak.vedtak.domain.TypeDagligReise
+import org.junit.Test
+
+class DetaljertVedtaksperiodeDagligReiseTsrTest {
+    val førsteJan = 1 januar 2024
+    val sisteJan = 31 januar 2024
+    val førsteFeb = 1 februar 2024
+    val sisteFeb = 29 februar 2024
+
+    val vedtaksperiodeJan =
+        DetaljertVedtaksperiodeDagligReise(
+            stønadstype = Stønadstype.DAGLIG_REISE_TSO,
+            typeDagligReise = TypeDagligReise.OFFENTLIG_TRANSPORT,
+            detaljertVedtaksperiode =
+                listOf(
+                    DetaljertBeregningsperioder(
+                        fom = førsteJan,
+                        tom = sisteJan,
+                        prisEnkeltbillett = 30,
+                        prisSyvdagersbillett = 150,
+                        pris30dagersbillett = 500,
+                        beløp = 300,
+                        billettdetaljer = mapOf(Billettype.ENKELTBILLETT to 20),
+                        antallReisedager = 20,
+                        antallReisedagerPerUke = 3,
+                    ),
+                ),
+        )
+
+    val vedtaksperiodeFeb =
+        DetaljertVedtaksperiodeDagligReise(
+            stønadstype = Stønadstype.DAGLIG_REISE_TSO,
+            typeDagligReise = TypeDagligReise.OFFENTLIG_TRANSPORT,
+            detaljertVedtaksperiode =
+                listOf(
+                    DetaljertBeregningsperioder(
+                        fom = førsteFeb,
+                        tom = sisteFeb,
+                        prisEnkeltbillett = 30,
+                        prisSyvdagersbillett = 150,
+                        pris30dagersbillett = 500,
+                        beløp = 300,
+                        billettdetaljer = mapOf(Billettype.ENKELTBILLETT to 20),
+                        antallReisedager = 20,
+                        antallReisedagerPerUke = 3,
+                    ),
+                ),
+        )
+
+    @Test
+    fun `skal ikke slå sammen aktivteter med ulik typeAktivitet`() {
+    }
+}
