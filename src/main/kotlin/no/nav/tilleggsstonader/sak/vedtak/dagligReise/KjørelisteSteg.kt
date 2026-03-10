91caeeeab9b4db262a5c16f3ff2487b4ac3b161e
Andreas Strand <andreas.strand@nav.no>
2026-03-10 23:00:34 +0100

    Implementerer KjørelisteSteg.kt, slik at det beregnes for privat bil men ikke noe annet på vedtaket

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/KjørelisteSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/KjørelisteSteg.kt
@@ -1,14 +1,25 @@
 package no.nav.tilleggsstonader.sak.vedtak.dagligReise
 
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.sak.arbeidsfordeling.ArbeidsfordelingService
 import no.nav.tilleggsstonader.sak.behandling.BehandlingService
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.privatbil.avklartedager.AvklartKjørelisteService
+import no.nav.tilleggsstonader.sak.vedtak.VedtakService
+import no.nav.tilleggsstonader.sak.vedtak.dagligReise.beregning.privatBil.PrivatBilBeregningsresultatService
+import no.nav.tilleggsstonader.sak.vedtak.domain.InnvilgelseDagligReise
 import org.springframework.stereotype.Service
 
 @Service
 class KjørelisteSteg(
     private val behandlingService: BehandlingService,
+    private val privatBilBeregningsresultatService: PrivatBilBeregningsresultatService,
+    private val vedtakService: VedtakService,
+    private val arbeidsfordelingService: ArbeidsfordelingService,
+    private val dagligReiseVedtakService: DagligReiseVedtakService,
+    private val avklartKjørelisteService: AvklartKjørelisteService,
 ) : BehandlingSteg<Void?> {
     override fun utførSteg(
         saksbehandling: Saksbehandling,
@@ -19,7 +30,28 @@ class KjørelisteSteg(
             behandlingSteg = saksbehandling.steg,
             behandlingStatus = saksbehandling.status,
         )
-        // TODO: Beregn kjøreliste og lagre ned resultat
+
+        val brukersNavKontor =
+            if (saksbehandling.stønadstype == Stønadstype.DAGLIG_REISE_TSR) {
+                arbeidsfordelingService.hentBrukersNavKontor(saksbehandling.ident, saksbehandling.stønadstype)
+            } else {
+                null
+            }
+
+        val eksisterendeRammevedtak =
+            vedtakService.hentVedtak<InnvilgelseDagligReise>(saksbehandling.id).data.rammevedtakPrivatBil
+                ?: error("Finner ikke rammevedtak for behandling ${saksbehandling.id}")
+        val beregningsresultatPrivatBil =
+            privatBilBeregningsresultatService.beregn(
+                rammevedtak = eksisterendeRammevedtak,
+                avklarteUkerForBehandling = avklartKjørelisteService.hentAvklarteUkerForBehandling(saksbehandling.id),
+                brukersNavKontor = brukersNavKontor,
+            )
+
+        dagligReiseVedtakService.oppdaterVedtakMedBeregningPrivatBil(
+            behandlingId = saksbehandling.id,
+            beregningsresultatPrivatBil = beregningsresultatPrivatBil,
+        )
     }
 
     override fun stegType(): StegType = StegType.KJØRELISTE
