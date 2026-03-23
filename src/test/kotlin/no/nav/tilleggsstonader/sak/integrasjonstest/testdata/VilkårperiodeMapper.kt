12d56161ded9dc0c26f16a37d8d1f47d195c0f31
Andreas Strand <andreas.strand@nav.no>
2026-03-23 10:51:51 +0100

    Litt hjelpefunksjoner i dsl-kode for å enklere kunne opprette læremidler-saker

--- src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/testdata/VilkårperiodeMapper.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/testdata/VilkårperiodeMapper.kt
@@ -6,13 +6,16 @@ import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.MålgruppeType
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.AktivitetDagligReiseTsoFaktaOgVurderingerDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.AktivitetDagligReiseTsrFaktaOgVurderingerDto
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.AktivitetLæremidlerFaktaOgVurderingerDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.FaktaOgSvarAktivitetDagligReiseTsoDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.FaktaOgSvarAktivitetDagligReiseTsrDto
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.FaktaOgSvarAktivitetLæremidlerDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.FaktaOgSvarDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.FaktaOgSvarMålgruppeDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.FaktaOgVurderingerDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.LagreVilkårperiode
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.MålgruppeFaktaOgVurderingerDto
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.MålgruppeLæremidlerFaktaOgVurderingerDto
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.dto.VilkårperiodeDto
 
 fun VilkårperiodeDto.tilLagreVilkårperiodeAktivitet(behandlingId: BehandlingId) =
@@ -53,6 +56,15 @@ private fun FaktaOgVurderingerDto.tilFaktaOgSvarDto(): FaktaOgSvarDto =
             )
         }
 
+        is AktivitetLæremidlerFaktaOgVurderingerDto -> {
+            FaktaOgSvarAktivitetLæremidlerDto(
+                svarHarUtgifter = this.harUtgifter?.svar,
+                studienivå = this.studienivå,
+                svarHarRettTilUtstyrsstipend = this.harRettTilUtstyrsstipend?.svar,
+                prosent = this.prosent,
+            )
+        }
+
         is MålgruppeFaktaOgVurderingerDto -> {
             FaktaOgSvarMålgruppeDto(
                 svarMedlemskap = this.medlemskap?.svar,
@@ -60,6 +72,12 @@ private fun FaktaOgVurderingerDto.tilFaktaOgSvarDto(): FaktaOgSvarDto =
                 svarMottarSykepengerForFulltidsstilling = this.mottarSykepengerForFulltidsstilling?.svar,
             )
         }
+
+        is MålgruppeLæremidlerFaktaOgVurderingerDto ->
+            FaktaOgSvarMålgruppeDto(
+                svarMedlemskap = this.medlemskap?.svar,
+                svarUtgifterDekketAvAnnetRegelverk = this.utgifterDekketAvAnnetRegelverk?.svar,
+            )
 
         else -> TODO()
     }
