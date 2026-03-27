af4d08aab2cafa66bfaea0965c8c56006d26bf48
Andreas Strand <andreas.strand@nav.no>
2026-03-27 10:53:54 +0100

    Kjører migrering for å sette korrekt global_id ut fra vilkar_periode sin forrige_vilkarperiode_id, slik at globalid blir riktig også for tidligere behandlinger

--- src/main/resources/db/migration/V122__vilkar_periode_global_id.sql
+++ src/main/resources/db/migration/V122__vilkar_periode_global_id.sql
@@ -1,3 +1,10 @@
 ALTER TABLE vilkar_periode ADD COLUMN global_id UUID;
-UPDATE vilkar_periode SET global_id = gen_random_uuid();
+
+WITH RECURSIVE chain AS (
+    SELECT id, gen_random_uuid() AS root_id FROM vilkar_periode WHERE forrige_vilkarperiode_id IS NULL
+    UNION ALL
+    SELECT vp.id, c.root_id FROM vilkar_periode vp JOIN chain c ON vp.forrige_vilkarperiode_id = c.id
+)
+UPDATE vilkar_periode SET global_id = (SELECT root_id FROM chain WHERE chain.id = vilkar_periode.id);
+
 ALTER TABLE vilkar_periode ALTER COLUMN global_id SET NOT NULL;
