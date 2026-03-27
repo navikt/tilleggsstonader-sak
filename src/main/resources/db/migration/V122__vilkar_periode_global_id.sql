5f1dfb6d91398297884f14fe707a79f6ff5d3d7c
Andreas Strand <andreas.strand@nav.no>
2026-03-27 22:40:00 +0100

    Prøver V122__vilkar_periode_global_id.sql med en litt optimalisert sql

--- src/main/resources/db/migration/V122__vilkar_periode_global_id.sql
+++ src/main/resources/db/migration/V122__vilkar_periode_global_id.sql
@@ -1,10 +1,16 @@
 ALTER TABLE vilkar_periode ADD COLUMN global_id UUID;
 
-WITH RECURSIVE chain AS (
-    SELECT id, gen_random_uuid() AS root_id FROM vilkar_periode WHERE forrige_vilkarperiode_id IS NULL
-    UNION ALL
-    SELECT vp.id, c.root_id FROM vilkar_periode vp JOIN chain c ON vp.forrige_vilkarperiode_id = c.id
-)
-UPDATE vilkar_periode SET global_id = (SELECT root_id FROM chain WHERE chain.id = vilkar_periode.id);
+WITH RECURSIVE chain AS (SELECT id, gen_random_uuid() AS root_id
+                         FROM vilkar_periode
+                         WHERE forrige_vilkarperiode_id IS NULL
+                         UNION ALL
+                         SELECT vp.id, c.root_id
+                         FROM vilkar_periode vp
+                                  JOIN chain c ON vp.forrige_vilkarperiode_id = c.id)
+UPDATE vilkar_periode vp
+SET global_id = c.root_id
+FROM chain c
+WHERE c.id = vp.id
+  AND vp.global_id IS NULL;
 
 ALTER TABLE vilkar_periode ALTER COLUMN global_id SET NOT NULL;
