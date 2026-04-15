7f35bb8802929b242ecab1b10800dc636c82f0de
sarahjelle <sara.hjelle@bekk.no>
2026-04-15 17:16:27 +0200

    Oppsummer rammevedtak og beregningsresultat slik at frontend kun trenger å mappe ut verdier

--- src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/GjennomførKjørelistebehandling.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/GjennomførKjørelistebehandling.kt
@@ -4,18 +4,30 @@ import no.nav.tilleggsstonader.sak.IntegrationTest
 import no.nav.tilleggsstonader.sak.behandling.domain.Behandling
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
+import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
 import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.tasks.kjørTasksKlareForProsesseringTilIngenTasksIgjen
 import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.tilordneÅpenBehandlingOppgaveForBehandling
 
-fun IntegrationTest.gjennomførKjørelisteBehandling(behandling: Behandling) {
+fun IntegrationTest.gjennomførKjørelisteBehandling(
+    behandling: Behandling,
+    tilSteg: StegType = StegType.BEHANDLING_FERDIGSTILT,
+) {
     require(behandling.type == BehandlingType.KJØRELISTE) {
         "Behandling er ikke en kjøreliste-behandling"
     }
     testoppsettService.oppdater(behandling.copy(status = BehandlingStatus.UTREDES))
     tilordneÅpenBehandlingOppgaveForBehandling(behandling.id)
+
+    if (tilSteg == StegType.KJØRELISTE) return
     gjennomførKjørelisteSteg(behandling.id)
+
+    if (tilSteg == StegType.BEREGNING) return
     gjennomførBeregningStegDagligReise(behandling.id)
+
+    if (tilSteg == StegType.SIMULERING) return
     gjennomførSimuleringSteg(behandling.id)
     kall.privatBil.fullførKjørelisteBehandling(behandling.id)
+
+    if (tilSteg in setOf(StegType.JOURNALFØR_OG_DISTRIBUER_VEDTAKSBREV, StegType.FERDIGSTILLE_BEHANDLING)) return
     kjørTasksKlareForProsesseringTilIngenTasksIgjen()
 }
