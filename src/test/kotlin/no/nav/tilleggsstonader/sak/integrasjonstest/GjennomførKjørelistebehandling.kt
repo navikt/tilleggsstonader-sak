2fffd988028370a7ee9d4bea1344c6a6a32cbf22
Andreas Strand <andreas.strand@nav.no>
2026-04-15 14:18:01 +0200

    Oppretter integrasjonstest som verifiserer at det produseres behandlingsstatistikk, vedtaksstatistikk og internt vedtak for daglig reise med privat bil, både ved rammevedtakbehandlingen og i kjørelistebehandlinger

--- src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/GjennomførKjørelistebehandling.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/integrasjonstest/GjennomførKjørelistebehandling.kt
@@ -2,7 +2,6 @@ package no.nav.tilleggsstonader.sak.integrasjonstest
 
 import no.nav.tilleggsstonader.sak.IntegrationTest
 import no.nav.tilleggsstonader.sak.behandling.domain.Behandling
-import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
 import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.tasks.kjørTasksKlareForProsesseringTilIngenTasksIgjen
 import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.tilordneÅpenBehandlingOppgaveForBehandling
@@ -11,8 +10,10 @@ fun IntegrationTest.gjennomførKjørelisteBehandling(behandling: Behandling) {
     require(behandling.type == BehandlingType.KJØRELISTE) {
         "Behandling er ikke en kjøreliste-behandling"
     }
-    testoppsettService.oppdater(behandling.copy(status = BehandlingStatus.UTREDES))
     tilordneÅpenBehandlingOppgaveForBehandling(behandling.id)
+    // For at behandling skal oppdatere status OPPRETTET -> UTREDES. Henter også personpplysninger som trengs for å produsere internt vedtak
+    // Vil alltid hentes fra frontend, så OK at vi gjør GET-kallet her selv uten at vi gjør noe med responsen
+    kall.behandling.hent(behandling.id)
     gjennomførKjørelisteSteg(behandling.id)
     gjennomførBeregningStegDagligReise(behandling.id)
     gjennomførSimuleringSteg(behandling.id)
---
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/dagligReise/DagligReisePrivatBilStatistikkIntegrationTest.kt
@@ -0,0 +1,107 @@
+package no.nav.tilleggsstonader.sak.vedtak.dagligReise
+
+import io.mockk.every
+import no.nav.tilleggsstonader.kontrakter.felles.Datoperiode
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+import no.nav.tilleggsstonader.kontrakter.saksstatistikk.BehandlingDVH
+import no.nav.tilleggsstonader.libs.utils.dato.april
+import no.nav.tilleggsstonader.sak.IntegrationTest
+import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
+import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import no.nav.tilleggsstonader.sak.infrastruktur.mocks.JournalpostClientMockConfig
+import no.nav.tilleggsstonader.sak.infrastruktur.mocks.KafkaFake
+import no.nav.tilleggsstonader.sak.infrastruktur.unleash.Toggle
+import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.finnPåTopic
+import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.verdiEllerFeil
+import no.nav.tilleggsstonader.sak.integrasjonstest.gjennomførKjørelisteBehandling
+import no.nav.tilleggsstonader.sak.integrasjonstest.opprettBehandlingOgGjennomførBehandlingsløp
+import no.nav.tilleggsstonader.sak.statistikk.vedtak.VedtaksstatistikkRepositoryV2
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.UUID
+
+// Bruker for å teste litt "side-effekter" som kommer ut av behandlingsløp. Behandlingsstatistikk, vedtaksstatistikk og internt vedtak
+class DagligReisePrivatBilStatistikkIntegrationTest : IntegrationTest() {
+    @Autowired
+    lateinit var vedtaksstatistikkRepositoryV2: VedtaksstatistikkRepositoryV2
+
+    @Test
+    fun `blir produsert vedtaksstatistikk for rammevedtakbehandling og kjørelistebehandling`() {
+        every { unleashService.isEnabled(Toggle.KAN_BEHANDLE_PRIVAT_BIL) } returns true
+
+        val fom = 6 april 2026
+        val tom = 26 april 2026
+
+        val behandlingContext =
+            opprettBehandlingOgGjennomførBehandlingsløp(
+                stønadstype = Stønadstype.DAGLIG_REISE_TSO,
+            ) {
+                aktivitet {
+                    opprett {
+                        aktivitetTiltakTso(fom, tom)
+                    }
+                }
+                målgruppe {
+                    opprett {
+                        målgruppeAAP(fom, tom)
+                    }
+                }
+                vilkår {
+                    opprett {
+                        privatBil(fom, tom)
+                    }
+                }
+
+                sendInnKjøreliste {
+                    periode = Datoperiode(fom, tom)
+                    kjørteDager = listOf(fom to 50)
+                }
+            }
+
+        verifiserVedtaksstatistikkFinnesForBehandling(behandlingContext.behandlingId)
+        verifiserFinnesBehandlingstatistikkForBehandling(behandlingContext.behandlingId)
+        verifiserHarBlittProdusertInterntVedtakForBehandling(behandlingContext.behandlingId)
+
+        val kjørelisteBehandling =
+            testoppsettService.hentBehandlinger(behandlingContext.fagsakId).single {
+                it.type ==
+                    BehandlingType.KJØRELISTE
+            }
+        gjennomførKjørelisteBehandling(kjørelisteBehandling)
+
+        verifiserVedtaksstatistikkFinnesForBehandling(kjørelisteBehandling.id)
+        verifiserFinnesBehandlingstatistikkForBehandling(kjørelisteBehandling.id)
+        verifiserHarBlittProdusertInterntVedtakForBehandling(kjørelisteBehandling.id)
+    }
+
+    private fun verifiserFinnesBehandlingstatistikkForBehandling(behandlingId: BehandlingId) {
+        val behandlingsstatiskkMeldinger =
+            KafkaFake
+                .sendteMeldinger()
+                .finnPåTopic(kafkaTopics.dvhBehandling)
+                .map { it.verdiEllerFeil<BehandlingDVH>() }
+                .filter { it.behandlingUuid == behandlingId.toString() }
+
+        assertThat(behandlingsstatiskkMeldinger).isNotEmpty
+    }
+
+    private fun verifiserVedtaksstatistikkFinnesForBehandling(behandlingId: BehandlingId) {
+        val res =
+            jdbcTemplate.query(
+                "select * from vedtaksstatistikk_v2 where behandling_id=:id",
+                mapOf("id" to behandlingId.id),
+            ) { rs, _ ->
+                rs.getObject("behandling_id") as UUID
+            }
+
+        assertThat(res).hasSize(1)
+    }
+
+    private fun verifiserHarBlittProdusertInterntVedtakForBehandling(behandlingId: BehandlingId) {
+        assertThat(
+            JournalpostClientMockConfig.opprettedeJournalposter
+                .filter { it.eksternReferanseId == "$behandlingId-blankett" }, // Settes i InterntVedtakTask ved opprettelse av journalpost
+        ).hasSize(1)
+    }
+}
