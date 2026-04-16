00b8130f1d923680d21f74eb9568ab1fb81e6bda
Andreas Strand <andreas.strand@nav.no>
2026-04-16 21:44:34 +0200

    Litt smartere update av ukenummer i avklart_kjort_uke

--- src/main/resources/db/migration/V126__uke_kolonne_avklart_kjort_uke.sql
+++ src/main/resources/db/migration/V126__uke_kolonne_avklart_kjort_uke.sql
@@ -4,4 +4,4 @@ ALTER TABLE avklart_kjort_uke
     RENAME COLUMN ukenummer TO uke;
 
 UPDATE avklart_kjort_uke
-SET uke = EXTRACT(YEAR FROM tom)::TEXT || '-' || uke;
+SET uke = to_char(tom, 'IYYY') || '-' || to_char(tom, 'IW');
