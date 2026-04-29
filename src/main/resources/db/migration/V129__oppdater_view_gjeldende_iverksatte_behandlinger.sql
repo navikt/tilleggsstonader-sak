bd2ff05a16bd259c4ecba92be7fe64a1513b5371
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-04-29 11:18:50 +0200

    Kjører wildacard på migrering script da endringen dekkes av forrige migrering

--- src/main/resources/db/migration/V129__oppdater_view_gjeldende_iverksatte_behandlinger.sql
+++ src/main/resources/db/migration/V129__oppdater_view_gjeldende_iverksatte_behandlinger.sql
@@ -6,28 +6,7 @@ FROM (
          SELECT f.stonadstype,
                 ROW_NUMBER() OVER (PARTITION BY b.fagsak_id ORDER BY b.vedtakstidspunkt DESC) rn,
                 f.fagsak_person_id,
-                b.id,
-                b.fagsak_id,
-                b.versjon,
-                b.opprettet_av,
-                b.opprettet_tid,
-                b.endret_av,
-                b.endret_tid,
-                b.type,
-                b.status,
-                b.steg,
-                b.kategori,
-                b.resultat,
-                b.forrige_iverksatte_behandling_id,
-                b.arsak,
-                b.krav_mottatt,
-                b.henlagt_arsak,
-                b.vedtakstidspunkt,
-                b.henlagt_begrunnelse,
-                b.nye_opplysninger_kilde,
-                b.nye_opplysninger_endringer,
-                b.nye_opplysninger_beskrivelse,
-                b.behandling_metode
+                b.*
          FROM behandling b
                   JOIN fagsak f ON b.fagsak_id = f.id
          WHERE b.resultat IN ('OPPHØRT', 'INNVILGET')
