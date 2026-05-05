a6a9f3efab34c56b52ddf24936dee864aa415f6f
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-05-05 08:40:14 +0200

    Legger til tester på avvik helg og parkeringsutgift

--- src/test/kotlin/no/nav/tilleggsstonader/sak/privatbil/AutomatiskKjørelisteBehandlingTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/privatbil/AutomatiskKjørelisteBehandlingTest.kt
@@ -17,6 +17,7 @@ import no.nav.tilleggsstonader.sak.vedtak.dagligReise.DagligReiseVedtakService
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.DagligReiseVilkårService
 import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.VilkårperiodeService
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.LocalDate
@@ -40,11 +41,14 @@ class AutomatiskKjørelisteBehandlingTest : CleanDatabaseIntegrationTest() {
     val fom: LocalDate = 1 januar 2026
     val tom: LocalDate = 31 januar 2026
 
-    @Test
-    fun `ta i mot kjøreliste uten avvik og opprett automatisk behandling`() {
+    @BeforeEach
+    fun setUp() {
         every { unleashService.isEnabled(Toggle.KAN_BEHANDLE_PRIVAT_BIL) } returns true
         every { unleashService.isEnabled(Toggle.KAN_AUTOMATISK_BEHANDLE_KJØRELISTE) } returns true
+    }
 
+    @Test
+    fun `ta i mot kjøreliste uten avvik og opprett automatisk behandling`() {
         val behandlingContext =
             opprettBehandlingOgGjennomførBehandlingsløp(
                 stønadstype = Stønadstype.DAGLIG_REISE_TSO,
@@ -67,5 +71,57 @@ class AutomatiskKjørelisteBehandlingTest : CleanDatabaseIntegrationTest() {
         assertThat(behandlinger).hasSize(2)
         assertThat(kjørelistebehandling.behandlingMetode).isEqualTo(BehandlingMetode.AUTOMATISK)
         assertThat(kjørelistebehandling.status).isEqualTo(BehandlingStatus.FERDIGSTILT)
+    }
+
+    @Test
+    fun `ta i mot kjøreliste med avvik på parkeringsutgifter og opprett manuell behandling`() {
+        val behandlingContext =
+            opprettBehandlingOgGjennomførBehandlingsløp(
+                stønadstype = Stønadstype.DAGLIG_REISE_TSO,
+            ) {
+                defaultDagligReisePrivatBilTsoTestdata(fom, tom)
+
+                sendInnKjøreliste {
+                    periode = Datoperiode(fom, 2 januar 2026)
+                    kjørteDager =
+                        listOf(
+                            KjørtDag(dato = 1 januar 2026, parkeringsutgift = 150),
+                            KjørtDag(dato = 2 januar 2026, parkeringsutgift = 150),
+                        )
+                }
+            }
+        val behandlinger = behandlingService.hentBehandlinger(behandlingContext.fagsakId)
+
+        val kjørelistebehandling = behandlinger.single { it.type == BehandlingType.KJØRELISTE }
+
+        assertThat(behandlinger).hasSize(2)
+        assertThat(kjørelistebehandling.behandlingMetode).isEqualTo(BehandlingMetode.MANUELL)
+        assertThat(kjørelistebehandling.status).isEqualTo(BehandlingStatus.OPPRETTET)
+    }
+
+    @Test
+    fun `ta i mot kjøreliste med avvik på helg og opprett manuell behandling`() {
+        val behandlingContext =
+            opprettBehandlingOgGjennomførBehandlingsløp(
+                stønadstype = Stønadstype.DAGLIG_REISE_TSO,
+            ) {
+                defaultDagligReisePrivatBilTsoTestdata(fom, tom)
+
+                sendInnKjøreliste {
+                    periode = Datoperiode(fom, 4 januar 2026)
+                    kjørteDager =
+                        listOf(
+                            KjørtDag(dato = 3 januar 2026, parkeringsutgift = 50),
+                            KjørtDag(dato = 4 januar 2026, parkeringsutgift = 50),
+                        )
+                }
+            }
+        val behandlinger = behandlingService.hentBehandlinger(behandlingContext.fagsakId)
+
+        val kjørelistebehandling = behandlinger.single { it.type == BehandlingType.KJØRELISTE }
+
+        assertThat(behandlinger).hasSize(2)
+        assertThat(kjørelistebehandling.behandlingMetode).isEqualTo(BehandlingMetode.MANUELL)
+        assertThat(kjørelistebehandling.status).isEqualTo(BehandlingStatus.OPPRETTET)
     }
 }
