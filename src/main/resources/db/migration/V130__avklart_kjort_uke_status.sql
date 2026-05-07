2440dc0f164ace8a1e29382b2b8dc4bb2b9e79e7
Sigmund Berbom <sberbom@gmail.com>
2026-05-07 10:02:07 +0200

    🗃️ Oppdater migreringsscript til å ta vare på tomme lister ved migrering

--- src/main/resources/db/migration/V130__avklart_kjort_uke_status.sql
+++ src/main/resources/db/migration/V130__avklart_kjort_uke_status.sql
@@ -8,13 +8,19 @@ UPDATE vedtak
 SET data = jsonb_set(
     data,
     '{beregningsresultat,privatBil,reiser}',
-    (SELECT jsonb_agg(
-        jsonb_set(reise, '{perioder}',
-            (SELECT jsonb_agg(periode || '{"fraTidligereVedtak": false}'::jsonb)
-             FROM jsonb_array_elements(reise -> 'perioder') AS periode)
+    COALESCE(
+        (SELECT jsonb_agg(
+            jsonb_set(reise, '{perioder}',
+                COALESCE(
+                    (SELECT jsonb_agg(periode || '{"fraTidligereVedtak": false}'::jsonb)
+                     FROM jsonb_array_elements(reise -> 'perioder') AS periode),
+                    '[]'::jsonb
+                )
+            )
         )
+        FROM jsonb_array_elements(data -> 'beregningsresultat' -> 'privatBil' -> 'reiser') AS reise),
+        '[]'::jsonb
     )
-    FROM jsonb_array_elements(data -> 'beregningsresultat' -> 'privatBil' -> 'reiser') AS reise)
 )
 WHERE data -> 'beregningsresultat' -> 'privatBil' IS NOT NULL
   AND jsonb_typeof(data -> 'beregningsresultat' -> 'privatBil') != 'null';
