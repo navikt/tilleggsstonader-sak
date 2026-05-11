6fe7be76b34537971c4e4dea35205275c927ed3c
Andreas Strand <andreas.strand@nav.no>
2026-05-11 21:20:24 +0200

    Legg på metrikker på mottatt kjøreliste for å kunne vise om en kjøreliste behandles automatisk, manuelt eller legges til i en eksisterende manuell behandling (#1205)

--- src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/BehandleMottattKjørelisteService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/BehandleMottattKjørelisteService.kt
@@ -1,5 +1,7 @@
 package no.nav.tilleggsstonader.sak.privatbil
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
 import no.nav.familie.prosessering.internal.TaskService
 import no.nav.tilleggsstonader.kontrakter.oppgave.OppgavePrioritet
 import no.nav.tilleggsstonader.libs.unleash.UnleashService
@@ -36,8 +38,19 @@ class BehandleMottattKjørelisteService(
     private val avklartKjørelisteService: AvklartKjørelisteService,
     private val taskService: TaskService,
     private val unleashService: UnleashService,
+    meterRegistry: MeterRegistry,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
+
+    init {
+        countersPerUtfall =
+            KjørelisteBehandlingUtfall.entries.associateWith { utfall ->
+                Counter
+                    .builder("kjoreliste_behandling_utfall_total")
+                    .tag("utfall", utfall.tag)
+                    .register(meterRegistry)
+            }
+    }
 
     @Transactional
     fun behandleMottattKjøreliste(kjøreliste: Kjøreliste) {
@@ -46,6 +59,7 @@ class BehandleMottattKjørelisteService(
         if (gjenbrukBehandling != null) {
             // Gjenbrukt behandling har allerede en oppgave-task fra da den ble opprettet.
             logger.info("Legger til kjøreliste=${kjøreliste.id} i behandling=${gjenbrukBehandling.id}")
+            countersPerUtfall.getValue(KjørelisteBehandlingUtfall.MANUELL_GJENBRUK).increment()
             avklartKjørelisteService.avklarUkerFraKjøreliste(
                 behandling = gjenbrukBehandling,
                 kjøreliste = kjøreliste,
@@ -65,6 +79,7 @@ class BehandleMottattKjørelisteService(
             logger.info(
                 "Ingen avvik funnet for behandling=${nyBehandling.id} og toggle er aktiv. Oppretter task for automatisk kjørelistebehandling",
             )
+            countersPerUtfall.getValue(KjørelisteBehandlingUtfall.AUTOMATISK).increment()
             behandlingService.oppdaterBehandlingMetode(
                 nyBehandling.id,
                 behandlingMetode = BehandlingMetode.AUTOMATISK,
@@ -72,6 +87,7 @@ class BehandleMottattKjørelisteService(
             taskService.save(AutomatiskKjørelisteBehandlingTask.opprettTask(nyBehandling.id))
         } else {
             logger.info("Oppretter manuell behandling for behandling=${nyBehandling.id}")
+            countersPerUtfall.getValue(KjørelisteBehandlingUtfall.MANUELL).increment()
             taskService.save(
                 OpprettOppgaveForOpprettetBehandlingTask.opprettTask(
                     OpprettOppgaveForOpprettetBehandlingTask.OpprettOppgaveTaskData(
@@ -145,5 +161,17 @@ class BehandleMottattKjørelisteService(
         } else {
             return false
         }
+    }
+
+    enum class KjørelisteBehandlingUtfall(
+        val tag: String,
+    ) {
+        AUTOMATISK("automatisk"),
+        MANUELL("manuell"),
+        MANUELL_GJENBRUK("manuell_gjenbruk"),
+    }
+
+    companion object {
+        private lateinit var countersPerUtfall: Map<KjørelisteBehandlingUtfall, Counter>
     }
 }
