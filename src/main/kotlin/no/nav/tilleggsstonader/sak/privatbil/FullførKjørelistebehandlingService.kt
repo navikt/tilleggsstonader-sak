018424a6187e8e9ad47284e96444d1da1066f641
Andreas Strand <andreas.strand@nav.no>
2026-05-26 14:24:15 +0200

    Oppretter brevmottakere for automatisk behandling i FullførKjørelistebehandlingSteg

--- src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/FullførKjørelistebehandlingService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/FullførKjørelistebehandlingService.kt
@@ -7,9 +7,7 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingStatus
 import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.FerdigstillBehandlingTask
-import no.nav.tilleggsstonader.sak.brev.brevmottaker.BrevmottakereService
 import no.nav.tilleggsstonader.sak.brev.kjørelistebrev.JournalførKjørelisteBehandlingBrevTask
-import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
 import no.nav.tilleggsstonader.sak.utbetaling.iverksetting.IverksettService
 import org.springframework.stereotype.Service
@@ -19,7 +17,6 @@ class FullførKjørelistebehandlingService(
     private val taskService: TaskService,
     private val iverksettService: IverksettService,
     private val behandlingService: BehandlingService,
-    private val brevmottakereService: BrevmottakereService,
 ) {
     fun fullførKjørelistebehandling(saksbehandling: Saksbehandling) {
         brukerfeilHvis(saksbehandling.status == BehandlingStatus.FERDIGSTILT) {
@@ -29,16 +26,10 @@ class FullførKjørelistebehandlingService(
             "Kan ikke fullføre behandling=${saksbehandling.id} fordi den ikke er en kjørelistebehandling."
         }
 
-        opprettTaskForSendingAvVedtaksbrev(saksbehandling.id)
+        taskService.save(JournalførKjørelisteBehandlingBrevTask.opprettTask(saksbehandling.id))
         behandlingService.oppdaterResultatPåBehandling(saksbehandling.id, BehandlingResultat.INNVILGET)
 
         taskService.save(FerdigstillBehandlingTask.opprettTask(saksbehandling))
         iverksettService.iverksettBehandlingFørsteGang(saksbehandling.id)
-    }
-
-    private fun opprettTaskForSendingAvVedtaksbrev(behandlingId: BehandlingId) {
-        // For å opprette brevmottakere for kjøreliste-brev da det ikke gjøres fra frontend eller ved automatisk behandling
-        brevmottakereService.hentEllerOpprettBrevmottakere(behandlingId)
-        taskService.save(JournalførKjørelisteBehandlingBrevTask.opprettTask(behandlingId))
     }
 }
--- src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/FullførKjørelistebehandlingSteg.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/privatbil/FullførKjørelistebehandlingSteg.kt
@@ -6,6 +6,7 @@ import no.nav.tilleggsstonader.sak.behandling.domain.BehandlingType
 import no.nav.tilleggsstonader.sak.behandling.domain.Saksbehandling
 import no.nav.tilleggsstonader.sak.behandlingsflyt.BehandlingSteg
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
+import no.nav.tilleggsstonader.sak.brev.brevmottaker.BrevmottakereService
 import no.nav.tilleggsstonader.sak.infrastruktur.exception.brukerfeilHvis
 import org.springframework.stereotype.Component
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component
 class FullførKjørelistebehandlingSteg(
     private val behandlingService: BehandlingService,
     private val fullførKjørelistebehandlingService: FullførKjørelistebehandlingService,
+    private val brevmottakereService: BrevmottakereService,
 ) : BehandlingSteg<Void?> {
     override fun utførSteg(
         saksbehandling: Saksbehandling,
@@ -25,6 +27,8 @@ class FullførKjørelistebehandlingSteg(
             "Kan ikke fullføre behandling=${saksbehandling.id} fordi den ikke er en kjørelistebehandling."
         }
 
+        // For å opprette brevmottakere for kjøreliste-brev da det ikke gjøres ved automatisk behandling
+        brevmottakereService.hentEllerOpprettBrevmottakere(saksbehandling.id)
         fullførKjørelistebehandlingService.fullførKjørelistebehandling(saksbehandling)
         behandlingService.oppdaterStatusPåBehandling(saksbehandling.id, BehandlingStatus.IVERKSETTER_VEDTAK)
     }
