2ca04b306e28b1c00e317f545f087f163955be8a
Sara Hjelle <46678893+sarahjelle@users.noreply.github.com>
2026-06-23 12:13:44 +0200

    Sletting av vilkår for privat bil forventer informasjon om aktivitet i mapping (#1284)
    * Sletting av vilkår for privat bil forventer informasjon om aktivitet i mapping
    
    * Lint

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/dagligReise/dto/SlettVilkårResultatDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/dagligReise/dto/SlettVilkårResultatDto.kt
@@ -1,9 +1,5 @@
 package no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.dto
 
-import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.SlettetVilkårResultat
-import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.VilkårDagligReiseDtoMapper.tilDto
-import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.dagligReise.VilkårDagligReiseMapper.mapTilVilkårDagligReise
-
 data class SlettVilkårRequestDto(
     val kommentar: String? = null,
 )
@@ -12,9 +8,3 @@ data class SlettVilkårResultatDto(
     val slettetPermanent: Boolean,
     val vilkår: VilkårDagligReiseDto,
 )
-
-fun SlettetVilkårResultat.tilDagligreiseDto() =
-    SlettVilkårResultatDto(
-        slettetPermanent = this.slettetPermanent,
-        vilkår = this.vilkår.mapTilVilkårDagligReise().tilDto(),
-    )
