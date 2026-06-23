eddcf5b52ef01f6fa691a4d6ec1c1e48212e848a
Andreas Strand <andreas.strand@nav.no>
2026-06-23 11:43:00 +0200

    not null på ny kolonne

--- src/main/resources/db/migration/V139__simulering_utbetalinger_pa_fagomrade.sql
+++ src/main/resources/db/migration/V139__simulering_utbetalinger_pa_fagomrade.sql
@@ -1,2 +1,4 @@
 ALTER TABLE simuleringsresultat
-    ADD COLUMN finnes_ikke_registrert_utbetalinger_fagomrade BOOLEAN DEFAULT FALSE;
+    ADD COLUMN finnes_ikke_registrert_utbetalinger_fagomrade BOOLEAN NOT NULL DEFAULT FALSE;
+
+ALTER TABLE simuleringsresultat ALTER COLUMN finnes_ikke_registrert_utbetalinger_fagomrade DROP DEFAULT;
