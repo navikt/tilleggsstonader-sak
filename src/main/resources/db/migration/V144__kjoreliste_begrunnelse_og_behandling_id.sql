f73a60c8112707cbe716b77096485d9e45c56802
sarahjelle <sara.hjelle@bekk.no>
2026-08-03 14:44:33 +0200

    Lag indeks på behandlingid for lettere oppslag

--- src/main/resources/db/migration/V144__kjoreliste_begrunnelse_og_behandling_id.sql
+++ src/main/resources/db/migration/V144__kjoreliste_begrunnelse_og_behandling_id.sql
@@ -4,3 +4,5 @@ ALTER TABLE kjoreliste
     ADD COLUMN manuelt_registrert BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE kjoreliste ALTER COLUMN manuelt_registrert DROP DEFAULT;
+
+CREATE INDEX idx_kjoreliste_behandling_id ON kjoreliste (behandling_id) WHERE behandling_id IS NOT NULL;
