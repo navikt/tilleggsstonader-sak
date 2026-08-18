984fb065b190a8a4607330565fda2cef7f98a779
Filip Larsen <filip.christoffer.larsen@nav.no>
2026-08-18 10:59:31 +0200

    Legger til nytt endepunkt så soknad-api kan hente manuelt registrerte kjørelister

--- src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/DagligReisePrivatBilController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/ekstern/stønad/DagligReisePrivatBilController.kt
@@ -5,8 +5,10 @@ import no.nav.tilleggsstonader.kontrakter.søknad.RammevedtakDto
 import no.nav.tilleggsstonader.libs.sikkerhet.EksternBrukerUtils
 import no.nav.tilleggsstonader.sak.fagsak.FagsakService
 import no.nav.tilleggsstonader.sak.privatbil.KjørelisteService
+import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.ReiseId
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -34,5 +36,27 @@ class DagligReisePrivatBilController(
         val kjøreliste = kjørelister.groupBy { it.data.reiseId }
 
         return rammevedtak.flatMap { it.tilDto(kjøreliste) }
+    }
+
+    @GetMapping("/kjoreliste/{reiseId}")
+    @ProtectedWithClaims(issuer = EksternBrukerUtils.ISSUER_TOKENX, claimMap = ["acr=Level4"])
+    fun hentManueltRegistrertKjørelisteForReise(
+        @PathVariable reiseId: String,
+    ): ManueltRegistrertKjørelisteEksternDto {
+        val ident = EksternBrukerUtils.hentFnrFraToken()
+        val reisedager =
+            fagsakService
+                .finnFagsaker(setOf(ident))
+                .flatMap { kjørelisteService.hentForFagsakId(it.id) }
+                .filter { it.data.reiseId == ReiseId.fromString(reiseId) && it.manueltLagretIBehandling != null }
+                .flatMap { it.data.reisedager }
+                .map {
+                    ManueltRegistrertKjørelisteDagEksternDto(
+                        dato = it.dato,
+                        harKjørt = it.harKjørt,
+                        parkeringsutgift = it.parkeringsutgift,
+                    )
+                }
+        return ManueltRegistrertKjørelisteEksternDto(reisedager = reisedager)
     }
 }
