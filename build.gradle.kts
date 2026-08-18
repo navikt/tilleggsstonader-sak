1b0452a6c0d48263d6e70f6f69326c0f2bc045d1
Andreas Strand <andreas.strand@nav.no>
2026-08-18 10:25:54 +0200

    Release-versjon av kontrakter

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 val javaVersion = JavaLanguageVersion.of(21)
 val familieProsesseringVersion = "2.20260713101404_6072da6"
 val tilleggsstønaderLibsVersion = "2026.08.07-12.32.dfeb3286fb52"
-val tilleggsstønaderKontrakterVersion = "1"
+val tilleggsstønaderKontrakterVersion = "2026.08.18-10.22.815df0ee133f"
 val avroVersion = "1.12.1"
 val confluentVersion = "8.0.1"
 val joarkHendelseVersion = "1.1.11"
