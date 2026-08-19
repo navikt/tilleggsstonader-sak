fbe709c67232f55d2aec46356ac38fbb05a549df
Nibedita.Pahari <nibedita.pahari@nav.no>
2026-08-19 13:33:36 +0200

    Lagt til avslag og opphør i TypeVedtakReiseTilSamling men kommentert ut nå

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/domain/VedtakReiseTilSamling.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vedtak/domain/VedtakReiseTilSamling.kt
@@ -8,6 +8,8 @@ enum class TypeVedtakReiseTilSamling(
     override val typeVedtak: TypeVedtak,
 ) : TypeVedtaksdata {
     INNVILGELSE_REISE_TIL_SAMLING(TypeVedtak.INNVILGELSE),
+    //  AVSLAG_REISE_TIL_SAMLING(TypeVedtak.AVSLAG),
+    //  OPPHØR_REISE_TIL_SAMLING(TypeVedtak.OPPHØR),
 }
 
 enum class TypeReiseTilSamling {
