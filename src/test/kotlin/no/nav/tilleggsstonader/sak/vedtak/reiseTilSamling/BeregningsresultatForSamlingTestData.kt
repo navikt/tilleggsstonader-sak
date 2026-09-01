cedbe0343588c3d711b3f685e18d2674b5804a11
Adnan Vrevic <adnan.vrevic@nav.no>
2026-09-01 08:47:11 +0200

    fikser merge conflict og test

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/reiseTilSamling/BeregningsresultatForSamlingTestData.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/reiseTilSamling/BeregningsresultatForSamlingTestData.kt
@@ -1,21 +1,27 @@
 package no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling
 
-import no.nav.tilleggsstonader.sak.felles.domain.VedtaksperiodeId
 import no.nav.tilleggsstonader.sak.felles.domain.FaktiskMålgruppe
-import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
-import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.domain.*
+import no.nav.tilleggsstonader.sak.felles.domain.VedtaksperiodeId
+import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.domain.BeregningsgrunnlagOffentligTransportForSamling
+import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.domain.BeregningsgrunnlagPrivatBilForSamling
+import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.domain.BeregningsresultatOffentligTransport
+import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.domain.BeregningsresultatPrivatBil
+import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.domain.VedtaksperiodeGrunnlag
 import no.nav.tilleggsstonader.sak.vilkår.stønadsvilkår.domain.ReiseId
+import no.nav.tilleggsstonader.sak.vilkår.vilkårperiode.domain.AktivitetType
 import java.math.BigDecimal
 import java.time.LocalDate
 
-fun lagVedtaksperiodeGrunnlag(fom: LocalDate, tom: LocalDate) =
-    VedtaksperiodeGrunnlag(
-        id = VedtaksperiodeId.random(),
-        fom = fom,
-        tom = tom,
-        målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
-        aktivitet = AktivitetType.TILTAK,
-    )
+fun lagVedtaksperiodeGrunnlag(
+    fom: LocalDate,
+    tom: LocalDate,
+) = VedtaksperiodeGrunnlag(
+    id = VedtaksperiodeId.random(),
+    fom = fom,
+    tom = tom,
+    målgruppe = FaktiskMålgruppe.NEDSATT_ARBEIDSEVNE,
+    aktivitet = AktivitetType.TILTAK,
+)
 
 fun lagBeregningsresultatForOffentligTransport(
     fom: LocalDate,
@@ -23,13 +29,14 @@ fun lagBeregningsresultatForOffentligTransport(
     reiseId: ReiseId = ReiseId.random(),
     beløp: BigDecimal = BigDecimal.ZERO,
 ): BeregningsresultatOffentligTransport {
-    val grunnlag = BeregningsgrunnlagOffentligTransportForSamling(
-        adresse = null,
-        fom = fom,
-        tom = tom,
-        vedtaksperioder = listOf(lagVedtaksperiodeGrunnlag(fom, tom)),
-        brukersNavKontor = null,
-    )
+    val grunnlag =
+        BeregningsgrunnlagOffentligTransportForSamling(
+            adresse = null,
+            fom = fom,
+            tom = tom,
+            vedtaksperioder = listOf(lagVedtaksperiodeGrunnlag(fom, tom)),
+            brukersNavKontor = null,
+        )
 
     return BeregningsresultatOffentligTransport(
         reiseId = reiseId,
@@ -44,19 +51,21 @@ fun lagBeregningsresultatForPrivatBil(
     reiseId: ReiseId = ReiseId.random(),
     beløp: BigDecimal = BigDecimal.ZERO,
 ): BeregningsresultatPrivatBil {
-    val grunnlag = BeregningsgrunnlagPrivatBilForSamling(
-        adresse = null,
-        fom = fom,
-        tom = tom,
-        sats = BigDecimal.ZERO,
-        totaltReiseavstand = BigDecimal.ZERO,
-        vedtaksperioder = listOf(lagVedtaksperiodeGrunnlag(fom, tom)),
-        brukersNavKontor = null,
-    )
+    val grunnlag =
+        BeregningsgrunnlagPrivatBilForSamling(
+            adresse = null,
+            fom = fom,
+            tom = tom,
+            sats = BigDecimal.ZERO,
+            totaltReiseavstand = BigDecimal.ZERO,
+            vedtaksperioder = listOf(lagVedtaksperiodeGrunnlag(fom, tom)),
+            brukersNavKontor = null,
+        )
 
     return BeregningsresultatPrivatBil(
         reiseId = reiseId,
         grunnlag = grunnlag,
         beløp = beløp,
+        aktivitetId = null,
     )
 }
