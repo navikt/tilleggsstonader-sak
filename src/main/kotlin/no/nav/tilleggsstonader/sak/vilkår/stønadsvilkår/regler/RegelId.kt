c713e3dc1f0cb0a94703653212edeaefc28c5abd
sarahjelle <sara.hjelle@bekk.no>
2026-09-03 14:34:10 +0200

    Bruk 'bruker' fremfor 'søker'

--- src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/regler/RegelId.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/vilkår/stønadsvilkår/regler/RegelId.kt
@@ -38,9 +38,9 @@ enum class RegelId(
     KAN_REISE_MED_TAXI("Kan søker benytte seg av taxi?"),
 
     // REISE TIL SAMLING
-    HAR_NØDVENDIGE_UTGIFTER_TIL_REISE_TIL_SAMLING("Har søker nødvendige utgifter til reise til samling?"),
+    HAR_NØDVENDIGE_UTGIFTER_TIL_REISE_TIL_SAMLING("Har bruker nødvendige utgifter til reise til samling?"),
     AVSTAND_OVER_TRETTI_KM("Er reiseavstanden over 30km?"),
     ER_SAMLING_OBLIGATORISK("Er samlingen obligatorisk?"),
-    KAN_REISE_MED_EGEN_BIL("Kan søker benytte privat bil?"),
-    DOKUMENTERTE_UTGIFTER("Har søker dokumenterte utgifter til reise?"),
+    KAN_REISE_MED_EGEN_BIL("Kan bruker benytte privat bil?"),
+    DOKUMENTERTE_UTGIFTER("Har bruker dokumenterte utgifter til reise?"),
 }
