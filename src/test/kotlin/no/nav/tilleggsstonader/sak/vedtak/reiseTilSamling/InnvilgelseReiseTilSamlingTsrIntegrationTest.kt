61bd7245255ec60b779b6609e3da671711364ef6
Sigmund Berbom <sberbom@gmail.com>
2026-09-23 13:43:27 +0200

    ♻️Ta i bruk KafkaFake for å følge samme pattern på intergrasjonstester som ellers i koden

--- src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/reiseTilSamling/InnvilgelseReiseTilSamlingTsrIntegrationTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/sak/vedtak/reiseTilSamling/InnvilgelseReiseTilSamlingTsrIntegrationTest.kt
@@ -6,25 +6,23 @@ import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
 import no.nav.tilleggsstonader.libs.utils.dato.januar
 import no.nav.tilleggsstonader.sak.IntegrationTest
 import no.nav.tilleggsstonader.sak.behandlingsflyt.StegType
-import no.nav.tilleggsstonader.sak.felles.domain.BehandlingId
+import no.nav.tilleggsstonader.sak.infrastruktur.mocks.KafkaFake
+import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.forventAntallMeldingerPåTopic
 import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.kall.expectOkWithBody
+import no.nav.tilleggsstonader.sak.integrasjonstest.extensions.verdiEllerFeil
 import no.nav.tilleggsstonader.sak.integrasjonstest.opprettBehandlingOgGjennomførBehandlingsløp
 import no.nav.tilleggsstonader.sak.opplysninger.ytelse.YtelsePerioderUtil.ytelsePerioderDtoTiltakspengerTpsak
-import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TilkjentYtelseRepository
-import no.nav.tilleggsstonader.sak.utbetaling.tilkjentytelse.domain.TypeAndel
+import no.nav.tilleggsstonader.sak.utbetaling.utsjekk.utbetaling.IverksettingDto
+import no.nav.tilleggsstonader.sak.utbetaling.utsjekk.utbetaling.StønadUtbetaling
 import no.nav.tilleggsstonader.sak.vedtak.reiseTilSamling.dto.InnvilgelseReiseTilSamlingResponse
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 
 /**
  * Tester innvilgelse av reise til samling for tema **TSR**.
  * Se [InnvilgelseReiseTilSamlingTsoIntegrationTest] for tilsvarende test for tema TSO.
  */
 class InnvilgelseReiseTilSamlingTsrIntegrationTest : IntegrationTest() {
-    @Autowired
-    lateinit var tilkjentYtelseRepository: TilkjentYtelseRepository
-
     @Test
     fun `kan innvilge reise til samling offentlig transport - TSR`() {
         val fomNay = 1 januar 2025
@@ -60,34 +58,38 @@ class InnvilgelseReiseTilSamlingTsrIntegrationTest : IntegrationTest() {
         // For at det skal opprettes sak med stønadstype REISE_TIL_SAMLING_TSR
         every { ytelseClient.hentYtelser(any()) } returns ytelsePerioderDtoTiltakspengerTpsak()
 
-        val behandlingContext =
-            opprettBehandlingOgGjennomførBehandlingsløp(
-                stønadstype = Stønadstype.REISE_TIL_SAMLING_TSR,
-            ) {
-                aktivitet {
-                    opprett {
-                        aktivitetTiltakTsrReiseTilSamling(fom, tom, tiltaksvariant = TypeAktivitet.GRUPPEAMO)
-                    }
-                }
-                målgruppe {
-                    opprett {
-                        målgruppeTiltakspenger(fom, tom)
-                    }
-                }
-                vilkår {
-                    opprett {
-                        offentligTransportReiseTilSamling(fom, tom, hentAktivitet = { it.single() })
-                    }
+        opprettBehandlingOgGjennomførBehandlingsløp(
+            stønadstype = Stønadstype.REISE_TIL_SAMLING_TSR,
+        ) {
+            aktivitet {
+                opprett {
+                    aktivitetTiltakTsrReiseTilSamling(fom, tom, tiltaksvariant = TypeAktivitet.GRUPPEAMO)
                 }
             }
+            målgruppe {
+                opprett {
+                    målgruppeTiltakspenger(fom, tom)
+                }
+            }
+            vilkår {
+                opprett {
+                    offentligTransportReiseTilSamling(fom, tom, hentAktivitet = { it.single() })
+                }
+            }
+        }
 
-        validerTypeAndelFraTiltaksvariant(behandlingContext.behandlingId)
+        validerTypeAndelFraTiltaksvariant()
     }
 
-    private fun validerTypeAndelFraTiltaksvariant(behandlingId: BehandlingId) {
-        val andeler = tilkjentYtelseRepository.findByBehandlingId(behandlingId)!!.andelerTilkjentYtelse
+    private fun validerTypeAndelFraTiltaksvariant() {
+        val utbetalingRecord =
+            KafkaFake
+                .sendteMeldinger()
+                .forventAntallMeldingerPåTopic(kafkaTopics.utbetaling, 1)
+                .map { it.verdiEllerFeil<IverksettingDto>() }
 
-        assertThat(andeler).isNotEmpty
-        assertThat(andeler).allMatch { it.type == TypeAndel.REISE_TIL_SAMLING_TILTAK_GRUPPE_AMO }
+        assertThat(
+            utbetalingRecord.flatMap { it.utbetalinger },
+        ).allMatch { it.stønad == StønadUtbetaling.REISE_TIL_SAMLING_TILTAK_GRUPPE_AMO }
     }
 }
