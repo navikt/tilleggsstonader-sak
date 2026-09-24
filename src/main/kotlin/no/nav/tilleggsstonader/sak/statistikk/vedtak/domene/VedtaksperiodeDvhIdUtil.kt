383501b6720795b6fd142fda412406d049dc8cd9
Andreas Strand <andreas.strand@nav.no>
2026-09-24 15:05:57 +0200

    Fjern noen todoer

--- src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/domene/VedtaksperiodeDvhIdUtil.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/sak/statistikk/vedtak/domene/VedtaksperiodeDvhIdUtil.kt
@@ -12,8 +12,6 @@ import java.time.LocalDate
 import java.util.UUID
 
 /**
- * TODO: skall - ikke tatt i bruk ennå.
- *
  * For BARNETILSYN og LÆREMIDLER finnes det ingen naturlig 1:1-kobling mellom en persistert
  * [no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode] og periodene som bygges opp av
  * beregningsresultatet (se [VedtaksperiodePassAvBarnMapper]/[VedtaksperiodeLæremidlerMapper] og
@@ -26,17 +24,8 @@ import java.util.UUID
  * Denne id-en er kun ment for kobling internt mellom [VedtaksperioderDvh] og [UtbetalingerDvh],
  * og representerer IKKE en persistert [no.nav.tilleggsstonader.sak.vedtak.domain.Vedtaksperiode].
  *
- * TODO: Når denne tas i bruk må [AndelTilVedtaksperiodeIdKobler] for BARNETILSYN og LÆREMIDLER
- *  oppdateres til å matche andeler mot de sammenslåtte periodene (med denne id-en) i stedet for
- *  mot `vedtak.vedtaksperioder`, slik at [UtbetalingerDvh.vedtaksperiodeIder] og [VedtaksperioderDvh.id]
- *  refererer til samme id-sett.
  */
 object VedtaksperiodeDvhIdUtil {
-    /**
-     * TODO: skall - verifiser at [periodeEgenskaper] inneholder alle feltene som er med på å
-     *  definere en unik periode for stønadstypen (fom, tom, målgruppe, aktivitet, studienivå/antallBarn osv.),
-     *  slik at to reelt ulike perioder aldri kan kollidere.
-     */
     internal fun genererDeterministiskId(
         behandlingId: BehandlingId,
         vararg periodeEgenskaper: Any?,
